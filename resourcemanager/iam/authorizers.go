b4f6f9ee8c439d9b18e86766af379a8e0bdc29e9
Azadeh Khojandi <azkhojan@microsoft.com>
2019-07-08 10:56:38 +1000

    remove copyrights

--- resourcemanager/iam/authorizers.go
+++ resourcemanager/iam/authorizers.go
@@ -1,8 +1,3 @@
-// Copyright (c) Microsoft and contributors.  All rights reserved.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE file in the root directory of this source tree.
-
 package iam
 
 import (
