2d4b48403ecd62a50c51b826e86485a2bcb75603
Azadeh Khojandi <azkhojan@microsoft.com>
2019-07-09 15:58:38 +1000

    add creating resourcegroup test

--- go.mod
+++ go.mod
@@ -27,9 +27,5 @@ require (
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
 	sigs.k8s.io/controller-runtime v0.2.0-beta.2
-	github.com/jstemmer/go-junit-report
-    github.com/axw/gocov/gocov
-    github.com/AlekSi/gocov-xml
-    gopkg.in/matm/v1/gocov-html
 
 )
---
+++ resourcemanager/resourcegroups/resourcegroup_test.go
@@ -0,0 +1,21 @@
+package resourcegroups
+
+import (
+	"context"
+	"testing"
+	helpers "Telstra.Dx.AzureOperator/helpers"
+	resourcemanagerconfig "Telstra.Dx.AzureOperator/resourcemanager/config"
+)
+
+func TestCreatingResouceGroup(t *testing.T) {
+	resourcemanagerconfig.LoadSettings()
+	resourcegroupName := "t-resourcegroup-" + helpers.RandomString(10)
+	resourcegroupLocation:= "westus"
+	var err error
+
+	_, err = CreateGroup(context.Background(), resourcegroupName, resourcegroupLocation)
+	if err != nil {
+		t.Errorf( "ERROR")
+	}
+
+}
