7169f4d1ddea7499d5413ed8f4382291f96f1a59
azadeh khojandi <azkhojan@microsoft.com>
2019-08-06 11:43:26 +1000

    [Do not merge] Eventhub (#30)
    * initial commit for Eventhub

---
+++ resourcemanager/config/config.go
@@ -0,0 +1,80 @@
+// package config manages loading configuration from environment and command-line params
+package config
+
+import (
+	"fmt"
+
+	"github.com/Azure/go-autorest/autorest/azure"
+)
+
+var (
+	// these are our *global* config settings, to be shared by all packages.
+	// each has corresponding public accessors below.
+	// if anything requires a `Set` accessor, that indicates it perhaps
+	// shouldn't be set here, because mutable vars shouldn't be global.
+	clientID               string
+	clientSecret           string
+	tenantID               string
+	subscriptionID         string
+	authorizationServerURL string
+	cloudName              string = "AzurePublicCloud"
+	useDeviceFlow          bool
+
+	userAgent   string
+	environment *azure.Environment
+)
+
+// ClientID is the OAuth client ID.
+func ClientID() string {
+	return clientID
+}
+
+// ClientSecret is the OAuth client secret.
+func ClientSecret() string {
+	return clientSecret
+}
+
+// TenantID is the AAD tenant to which this client belongs.
+func TenantID() string {
+	return tenantID
+}
+
+// SubscriptionID is a target subscription for Azure resources.
+func SubscriptionID() string {
+	return subscriptionID
+}
+
+// AuthorizationServerURL is the OAuth authorization server URL.
+// Q: Can this be gotten from the `azure.Environment` in `Environment()`?
+func AuthorizationServerURL() string {
+	return authorizationServerURL
+}
+
+// UseDeviceFlow() specifies if interactive auth should be used. Interactive
+// auth uses the OAuth Device Flow grant type.
+func UseDeviceFlow() bool {
+	return useDeviceFlow
+}
+
+// UserAgent() specifies a string to append to the agent identifier.
+func UserAgent() string {
+	if len(userAgent) > 0 {
+		return userAgent
+	}
+	return "AzureOperator-resourcemanager"
+}
+
+// Environment() returns an `azure.Environment{...}` for the current cloud.
+func Environment() *azure.Environment {
+	if environment != nil {
+		return environment
+	}
+	env, err := azure.EnvironmentFromName(cloudName)
+	if err != nil {
+		// TODO: move to initialization of var
+		panic(fmt.Sprintf(
+			"invalid cloud name '%s' specified, cannot continue\n", cloudName))
+	}
+	environment = &env
+	return environment
+}
---
+++ resourcemanager/eventhubs/namespace_test.go
@@ -0,0 +1,87 @@
+/*
+Copyright 2019 microsoft.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package eventhubs
+
+import (
+	"context"
+	"time"
+
+	resoucegroupsresourcemanager "github.com/Azure/azure-service-operator/resourcemanager/resourcegroups"
+
+	helpers "github.com/Azure/azure-service-operator/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Namespace", func() {
+
+	const timeout = time.Second * 240
+
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
+	// your API definition.
+	// Avoid adding tests for vanilla CRUD operations because they would
+	// test Kubernetes API server, which isn't the goal here.
+
+	Context("Create and Delete", func() {
+		It("should create and delete namespace in azure", func() {
+
+			resourceGroupName := "t-rg-dev-eh-" + helpers.RandomString(10)
+			resourcegroupLocation := "westus"
+			eventhubNamespaceName := "t-ns-dev-eh-" + helpers.RandomString(10)
+			namespaceLocation := "westus"
+			var err error
+
+			_, err = resoucegroupsresourcemanager.CreateGroup(context.Background(), resourceGroupName, resourcegroupLocation)
+			Expect(err).NotTo(HaveOccurred())
+
+			time.Sleep(30 * time.Second)
+
+			_, err = CreateNamespaceAndWait(context.Background(), resourceGroupName, eventhubNamespaceName, namespaceLocation)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() bool {
+				result, _ := GetNamespace(context.Background(), resourceGroupName, eventhubNamespaceName)
+				return result.Response.StatusCode == 200
+			}, timeout,
+			).Should(BeTrue())
+
+			_, err = DeleteNamespace(context.Background(), resourceGroupName, eventhubNamespaceName)
+			Expect(err).NotTo(HaveOccurred())
+
+			Eventually(func() bool {
+				result, _ := GetNamespace(context.Background(), resourceGroupName, eventhubNamespaceName)
+				return result.Response.StatusCode == 404
+			}, timeout,
+			).Should(BeTrue())
+
+			time.Sleep(30 * time.Second)
+
+			_, err = resoucegroupsresourcemanager.DeleteGroup(context.Background(), resourceGroupName)
+			Expect(err).NotTo(HaveOccurred())
+
+		})
+
+	})
+})
