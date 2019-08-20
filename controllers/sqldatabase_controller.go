d2d8e357f17bc4c2b2eb2a89433c4ec8dc408123
Erin Corson <frodopwns@gmail.com>
2019-08-20 15:45:37 -0600

    Scaffold Azure SQL resources and controllers/ add boilerplate (#109)
    * generic fields and status subresource
    
    * adding some boilerplate to the controllers and fields to the apis
    
    * more boilerplate for sql operators
    
    * fixes
    
    * missed some keyvault refs

--- controllers/sqldatabase_controller.go
+++ controllers/sqldatabase_controller.go
@@ -17,28 +17,43 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	helpers "github.com/Azure/azure-service-operator/pkg/helpers"
 	"github.com/go-logr/logr"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	azurev1 "github.com/Azure/azure-service-operator/api/v1"
 )
 
+const SQLDatabaseFinalizerName = "sqldatabase.finalizers.azure.com"
+
 // SqlDatabaseReconciler reconciles a SqlDatabase object
 type SqlDatabaseReconciler struct {
 	client.Client
-	Log logr.Logger
+	Log      logr.Logger
+	Recorder record.EventRecorder
 }
 
 // +kubebuilder:rbac:groups=azure.microsoft.com,resources=sqldatabases,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=azure.microsoft.com,resources=sqldatabases/status,verbs=get;update;patch
 
 func (r *SqlDatabaseReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("sqldatabase", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("sqldatabase", req.NamespacedName)
 
 	// your logic here
+	var instance azurev1.SqlDatabase
+
+	if err := r.Get(ctx, req.NamespacedName, &instance); err != nil {
+		log.Info("Unable to retrieve sql-database resource", "err", err.Error())
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -47,4 +62,51 @@ func (r *SqlDatabaseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&azurev1.SqlDatabase{}).
 		Complete(r)
+}
+
+func (r *SqlDatabaseReconciler) reconcileExternal(instance *azurev1.SqlDatabase) error {
+	// ctx := context.Background()
+	// location := instance.Spec.Location
+	// name := instance.ObjectMeta.Name
+	// groupName := instance.Spec.ResourceGroupName
+
+	// // write information back to instance
+	// instance.Status.Provisioning = true
+
+	// if err := r.Status().Update(ctx, instance); err != nil {
+	// 	r.Recorder.Event(instance, "Warning", "Failed", "Unable to update instance")
+	// }
+
+	//err = CreateResource()
+	// if err != nil {
+
+	// }
+
+	// instance.Status.Provisioning = false
+	// instance.Status.Provisioned = true
+
+	// if err = r.Status().Update(ctx, instance); err != nil {
+	// 	r.Recorder.Event(instance, "Warning", "Failed", "Unable to update instance")
+	// }
+
+	return nil
+}
+
+func (r *SqlDatabaseReconciler) deleteExternal(instance *azurev1.SqlDatabase) error {
+	// ctx := context.Background()
+	name := instance.ObjectMeta.Name
+	// groupName := instance.Spec.ResourceGroupName
+
+	r.Recorder.Event(instance, "Normal", "Deleted", name+" deleted")
+	return nil
+}
+
+func (r *SqlDatabaseReconciler) addFinalizer(instance *azurev1.SqlDatabase) error {
+	helpers.AddFinalizer(instance, SQLDatabaseFinalizerName)
+	err := r.Update(context.Background(), instance)
+	if err != nil {
+		return fmt.Errorf("failed to update finalizer: %v", err)
+	}
+	r.Recorder.Event(instance, "Normal", "Updated", fmt.Sprintf("finalizer %s added", SQLDatabaseFinalizerName))
+	return nil
 }
--- controllers/sqlserver_controller.go
+++ controllers/sqlserver_controller.go
@@ -17,28 +17,74 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	helpers "github.com/Azure/azure-service-operator/pkg/helpers"
 	"github.com/go-logr/logr"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	azurev1 "github.com/Azure/azure-service-operator/api/v1"
 )
 
+const SQLServerFinalizerName = "sqlserver.finalizers.azure.com"
+
 // SqlServerReconciler reconciles a SqlServer object
 type SqlServerReconciler struct {
 	client.Client
-	Log logr.Logger
+	Log      logr.Logger
+	Recorder record.EventRecorder
 }
 
 // +kubebuilder:rbac:groups=azure.microsoft.com,resources=sqlservers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=azure.microsoft.com,resources=sqlservers/status,verbs=get;update;patch
 
 func (r *SqlServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("sqlserver", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("sqlserver", req.NamespacedName)
 
 	// your logic here
+	var instance azurev1.SqlServer
+
+	if err := r.Get(ctx, req.NamespacedName, &instance); err != nil {
+		log.Info("Unable to retrieve sql-server resource", "err", err.Error())
+		// we'll ignore not-found errors, since they can't be fixed by an immediate
+		// requeue (we'll need to wait for a new notification), and we can get them
+		// on deleted requests.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if helpers.IsBeingDeleted(&instance) {
+		if helpers.HasFinalizer(&instance, SQLServerFinalizerName) {
+			if err := r.deleteExternal(&instance); err != nil {
+				log.Info("Delete SQL Server failed with ", err.Error())
+				return ctrl.Result{}, err
+			}
+
+			helpers.RemoveFinalizer(&instance, SQLServerFinalizerName)
+			if err := r.Update(context.Background(), &instance); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		return ctrl.Result{}, nil
+	}
+
+	if !helpers.HasFinalizer(&instance, SQLServerFinalizerName) {
+		if err := r.addFinalizer(&instance); err != nil {
+			log.Info("Adding sql-server finalizer failed with ", err.Error())
+			return ctrl.Result{}, err
+		}
+	}
+
+	if !instance.IsSubmitted() {
+		if err := r.reconcileExternal(&instance); err != nil {
+			return ctrl.Result{}, fmt.Errorf("error reconciling sql server in azure: %v", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	r.Recorder.Event(&instance, "Normal", "Provisioned", "sqlserver "+instance.ObjectMeta.Name+" provisioned ")
 
 	return ctrl.Result{}, nil
 }
@@ -47,4 +93,52 @@ func (r *SqlServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&azurev1.SqlServer{}).
 		Complete(r)
+}
+
+func (r *SqlServerReconciler) reconcileExternal(instance *azurev1.SqlServer) error {
+	// ctx := context.Background()
+	// location := instance.Spec.Location
+	// name := instance.ObjectMeta.Name
+	// groupName := instance.Spec.ResourceGroupName
+
+	// // write information back to instance
+	// instance.Status.Provisioning = true
+
+	// if err := r.Status().Update(ctx, instance); err != nil {
+	// 	r.Recorder.Event(instance, "Warning", "Failed", "Unable to update instance")
+	// }
+
+	//err = CreateResource()
+	// if err != nil {
+
+	// }
+
+	// instance.Status.Provisioning = false
+	// instance.Status.Provisioned = true
+
+	// if err = r.Status().Update(ctx, instance); err != nil {
+	// 	r.Recorder.Event(instance, "Warning", "Failed", "Unable to update instance")
+	// }
+
+	return nil
+}
+
+func (r *SqlServerReconciler) deleteExternal(instance *azurev1.SqlServer) error {
+	// ctx := context.Background()
+	name := instance.ObjectMeta.Name
+	// groupName := instance.Spec.ResourceGroupName
+	// delette resource
+
+	r.Recorder.Event(instance, "Normal", "Deleted", name+" deleted")
+	return nil
+}
+
+func (r *SqlServerReconciler) addFinalizer(instance *azurev1.SqlServer) error {
+	helpers.AddFinalizer(instance, SQLServerFinalizerName)
+	err := r.Update(context.Background(), instance)
+	if err != nil {
+		return fmt.Errorf("failed to update finalizer: %v", err)
+	}
+	r.Recorder.Event(instance, "Normal", "Updated", fmt.Sprintf("finalizer %s added", SQLServerFinalizerName))
+	return nil
 }
