daf424764a4fed37edbc18602d436152a2be3222
Janani Vasudevan <49576785+jananivMS@users.noreply.github.com>
2019-08-20 13:09:19 -0600

    Updates to KV controller from Ace (#80)
    * feat: implement keyvault controller
    
    * Ace's KV changes with updates
    
    * Added an event for the final successful provisioning
    
    * Updated changes based on the PR comments
    
    * removing unwanted file
    
    * making resource group name the one in the keyvault yaml

---
+++ pkg/resourcemanager/keyvaults/keyvaults.go
@@ -0,0 +1,72 @@
+/*
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package keyvaults
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/Azure/azure-sdk-for-go/services/keyvault/mgmt/2018-02-14/keyvault"
+	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
+	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
+	"github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/to"
+	uuid "github.com/satori/go.uuid"
+)
+
+func getVaultsClient() keyvault.VaultsClient {
+	vaultsClient := keyvault.NewVaultsClient(config.SubscriptionID())
+	a, err := iam.GetResourceManagementAuthorizer()
+	if err != nil {
+		log.Fatalf("failed to initialize authorizer: %v\n", err)
+	}
+	vaultsClient.Authorizer = a
+	vaultsClient.AddToUserAgent(config.UserAgent())
+	return vaultsClient
+}
+
+// CreateVault creates a new key vault
+func CreateVault(ctx context.Context, groupName string, vaultName string, location string) (keyvault.Vault, error) {
+	vaultsClient := getVaultsClient()
+	id, err := uuid.FromString(config.TenantID())
+	if err != nil {
+		return keyvault.Vault{}, err
+	}
+
+	params := keyvault.VaultCreateOrUpdateParameters{
+		Properties: &keyvault.VaultProperties{
+			TenantID:       &id,
+			AccessPolicies: &[]keyvault.AccessPolicyEntry{},
+			Sku: &keyvault.Sku{
+				Family: to.StringPtr("A"),
+				Name:   keyvault.Standard,
+			},
+		},
+		Location: to.StringPtr(location),
+	}
+
+	log.Println(fmt.Sprintf("creating keyvault '%s' in resource group '%s' and location: %v", vaultName, groupName, location))
+	future, err := vaultsClient.CreateOrUpdate(ctx, groupName, vaultName, params)
+
+	return future.Result(vaultsClient)
+}
+
+// DeleteVault removes the resource group named by env var
+func DeleteVault(ctx context.Context, groupName string, vaultName string) (result autorest.Response, err error) {
+	vaultsClient := getVaultsClient()
+	return vaultsClient.Delete(ctx, groupName, vaultName)
+}
