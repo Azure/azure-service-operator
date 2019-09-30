50a99bbe67be7dc0c35e6c2c1d8d42272d7206a2
William Mortl <32373900+WilliamMortlMicrosoft@users.noreply.github.com>
2019-09-30 15:55:32 -0600

    Added a new operator for SQL Firewall Rules, cleaned up the resource client (#239)
    * makefile
    
    * updated schema for api
    
    * controller works, doesnt get deleted by parent
    
    * fix for Mels change
    
    * parent deletes child... updated resourceclient
    
    * cleaned up the end to end test
    
    * erin feedback

--- main.go
+++ main.go
@@ -191,8 +191,10 @@ func main() {
 		os.Exit(1)
 	}
 	if err = (&controllers.SqlFirewallRuleReconciler{
-		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("SqlFirewallRule"),
+		Client:   mgr.GetClient(),
+		Log:      ctrl.Log.WithName("controllers").WithName("SqlFirewallRule"),
+		Recorder: mgr.GetEventRecorderFor("SqlFirewallRule-controller"),
+		Scheme:   mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "SqlFirewallRule")
 		os.Exit(1)
--- pkg/resourcemanager/sqlclient/sqlclient_godsk.go
+++ pkg/resourcemanager/sqlclient/sqlclient_godsk.go
@@ -7,7 +7,6 @@ package sqlclient
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/2015-05-01-preview/sql"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
@@ -119,43 +118,65 @@ func (sdk GoSDKClient) CreateOrUpdateDB(properties SQLDatabaseProperties) (sql.D
 			Location:           to.StringPtr(sdk.Location),
 			DatabaseProperties: &dbProp,
 		})
-
 }
 
-// CreateOrUpdateDB creates or updates a DB in Azure
-func (sdk GoSDKClient) GetDB(name string) (sql.Database, error) {
+// GetServer returns a SQL server
+func (sdk GoSDKClient) GetServer() (result sql.Server, err error) {
+	serversClient := getGoServersClient()
+
+	return serversClient.Get(
+		sdk.Ctx,
+		sdk.ResourceGroupName,
+		sdk.ServerName,
+	)
+}
+
+// GetDB retrieves a database
+func (sdk GoSDKClient) GetDB(databaseName string) (sql.Database, error) {
 	dbClient := getGoDbClient()
 
 	return dbClient.Get(
 		sdk.Ctx,
 		sdk.ResourceGroupName,
 		sdk.ServerName,
-		name,
+		databaseName,
 		"serviceTierAdvisors, transparentDataEncryption",
+	)
+}
+
+// GetSQLFirewallRule returns a firewall rule
+func (sdk GoSDKClient) GetSQLFirewallRule(ruleName string) (result sql.FirewallRule, err error) {
+	firewallClient := getGoFirewallClient()
+
+	return firewallClient.Get(
+		sdk.Ctx,
+		sdk.ResourceGroupName,
+		sdk.ServerName,
+		ruleName,
 	)
 }
 
 // DeleteDB deletes a DB
 func (sdk GoSDKClient) DeleteDB(databaseName string) (result autorest.Response, err error) {
-	dbClient := getGoDbClient()
+	result = autorest.Response{
+		Response: &http.Response{
+			StatusCode: 200,
+		},
+	}
 
-	// check to see if the db exists, if it does then short-circuit
-	_, err = dbClient.Get(
-		sdk.Ctx,
-		sdk.ResourceGroupName,
-		sdk.ServerName,
-		databaseName,
-		"serviceTierAdvisors, transparentDataEncryption",
-	)
-	if err != nil {
-		result = autorest.Response{
-			Response: &http.Response{
-				StatusCode: 200,
-			},
-		}
+	// check to see if the server exists, if it doesn't then short-circuit
+	server, err := sdk.GetServer()
+	if err != nil || *server.State != "Ready" {
 		return result, nil
 	}
 
+	// check to see if the db exists, if it doesn't then short-circuit
+	_, err = sdk.GetDB(databaseName)
+	if err != nil {
+		return result, nil
+	}
+
+	dbClient := getGoDbClient()
 	result, err = dbClient.Delete(
 		sdk.Ctx,
 		sdk.ResourceGroupName,
@@ -168,19 +189,20 @@ func (sdk GoSDKClient) DeleteDB(databaseName string) (result autorest.Response, 
 
 // DeleteSQLFirewallRule deletes a firewall rule
 func (sdk GoSDKClient) DeleteSQLFirewallRule(ruleName string) (err error) {
-	serversClient := getGoServersClient()
-	firewallClient := getGoFirewallClient()
 
 	// check to see if the server exists, if it doesn't then short-circuit
-	server, err := serversClient.Get(
-		sdk.Ctx,
-		sdk.ResourceGroupName,
-		sdk.ServerName,
-	)
+	server, err := sdk.GetServer()
 	if err != nil || *server.State != "Ready" {
-		return err
+		return nil
 	}
 
+	// check to see if the rule exists, if it doesn't then short-circuit
+	_, err = sdk.GetSQLFirewallRule(ruleName)
+	if err != nil {
+		return nil
+	}
+
+	firewallClient := getGoFirewallClient()
 	_, err = firewallClient.Delete(
 		sdk.Ctx,
 		sdk.ResourceGroupName,
@@ -193,23 +215,19 @@ func (sdk GoSDKClient) DeleteSQLFirewallRule(ruleName string) (err error) {
 
 // DeleteSQLServer deletes a SQL server
 func (sdk GoSDKClient) DeleteSQLServer() (result autorest.Response, err error) {
-	serversClient := getGoServersClient()
+	result = autorest.Response{
+		Response: &http.Response{
+			StatusCode: 200,
+		},
+	}
 
 	// check to see if the server exists, if it doesn't then short-circuit
-	_, err = serversClient.Get(
-		sdk.Ctx,
-		sdk.ResourceGroupName,
-		sdk.ServerName,
-	)
+	_, err = sdk.GetServer()
 	if err != nil {
-		result = autorest.Response{
-			Response: &http.Response{
-				StatusCode: 200,
-			},
-		}
 		return result, nil
 	}
 
+	serversClient := getGoServersClient()
 	future, err := serversClient.Delete(
 		sdk.Ctx,
 		sdk.ResourceGroupName,
@@ -220,26 +238,4 @@ func (sdk GoSDKClient) DeleteSQLServer() (result autorest.Response, err error) {
 	}
 
 	return future.Result(serversClient)
-}
-
-// IsAsyncNotCompleted returns true if the error is due to async not completed
-func (sdk GoSDKClient) IsAsyncNotCompleted(err error) (result bool) {
-	result = false
-	if err != nil && strings.Contains(err.Error(), "asynchronous operation has not completed") {
-		result = true
-	} else if strings.Contains(err.Error(), "is busy with another operation") {
-		result = true
-	}
-	return result
-}
-
-// GetServer returns a server
-func (sdk GoSDKClient) GetServer() (result sql.Server, err error) {
-	serversClient := getGoServersClient()
-
-	return serversClient.Get(
-		sdk.Ctx,
-		sdk.ResourceGroupName,
-		sdk.ServerName,
-	)
 }
