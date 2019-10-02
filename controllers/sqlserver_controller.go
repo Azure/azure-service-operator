bc82b57bd7c91a3ca00313a5510771608c9380e8
Amanda Kaufman <aka@microsoft.com>
2019-10-02 13:48:10 -0600

    Changes

--- controllers/sqlserver_controller.go
+++ controllers/sqlserver_controller.go
@@ -107,15 +107,25 @@ func (r *SqlServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		}
 	}
 
-	availableResp, err := sdkClient.CheckNameAvailability()
-	if err != nil {
-		log.Info("error validating name")
-		return ctrl.Result{}, err
-	}
-	if !availableResp.Available {
-		log.Info("Servername is invalid or not available")
-		r.Recorder.Event(&instance, "Warning", "Failed", "Servername is invalid")
-		return ctrl.Result{Requeue: false}, fmt.Errorf("Servername invalid %s", availableResp.Name)
+	// Re-create secret if server is provisioned but secret doesn't exist
+	if instance.IsProvisioned() {
+		name := instance.ObjectMeta.Name
+
+		secret := &v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: instance.Namespace,
+			},
+			Type: "Opaque",
+		}
+
+		if err := r.Get(context.Background(), types.NamespacedName{Name: name, Namespace: instance.Namespace}, secret); err != nil {
+			r.Log.Info("Error", "ReconcileSecret", "Server exists but secret does not, recreating now")
+
+			// Add admin credentials to "data" block in secret
+
+			// CreateOrUpdate secret
+		}
 	}
 
 	if !instance.IsSubmitted() {
