e13fa50dbcf9265ebad766cd3ba7bd455e4451ed
William Mortl <32373900+WilliamMortlMicrosoft@users.noreply.github.com>
2019-10-02 23:35:09 -0600

    fixed YAML, removed check name functionality (#276)
    * fixed YAML, removed Mel's fix, updated DB controller
    
    * janani feedback:
    
    * removed whitespace

--- controllers/sqldatabase_controller.go
+++ controllers/sqldatabase_controller.go
@@ -120,11 +120,15 @@ func (r *SqlDatabaseReconciler) SetupWithManager(mgr ctrl.Manager) error {
 func (r *SqlDatabaseReconciler) reconcileExternal(instance *azurev1alpha1.SqlDatabase) error {
 	ctx := context.Background()
 	location := instance.Spec.Location
-	server := instance.Spec.Server
 	groupName := instance.Spec.ResourceGroup
+	server := instance.Spec.Server
 	dbEdition := instance.Spec.Edition
-
 	dbName := instance.ObjectMeta.Name
+
+	sqlDatabaseProperties := sql.SQLDatabaseProperties{
+		DatabaseName: dbName,
+		Edition:      dbEdition,
+	}
 
 	sdkClient := sql.GoSDKClient{
 		Ctx:               ctx,
@@ -133,19 +137,13 @@ func (r *SqlDatabaseReconciler) reconcileExternal(instance *azurev1alpha1.SqlDat
 		Location:          location,
 	}
 
-	sqlDatabaseProperties := sql.SQLDatabaseProperties{
-		DatabaseName: dbName,
-		Edition:      dbEdition,
-	}
-
 	r.Log.Info("Calling createorupdate SQL database")
 
-	//get owner instance of SqlServer
+	// get owner instance of SqlServer
 	r.Recorder.Event(instance, "Normal", "UpdatingOwner", "Updating owner SqlServer instance")
 	var ownerInstance azurev1alpha1.SqlServer
 	sqlServerNamespacedName := types.NamespacedName{Name: server, Namespace: instance.Namespace}
 	err := r.Get(ctx, sqlServerNamespacedName, &ownerInstance)
-
 	if err != nil {
 		//log error and kill it, as the parent might not exist in the cluster. It could have been created elsewhere or through the portal directly
 		r.Recorder.Event(instance, "Warning", "Failed", "Unable to get owner instance of SqlServer")
@@ -193,10 +191,10 @@ func (r *SqlDatabaseReconciler) reconcileExternal(instance *azurev1alpha1.SqlDat
 
 func (r *SqlDatabaseReconciler) deleteExternal(instance *azurev1alpha1.SqlDatabase) error {
 	ctx := context.Background()
-	dbname := instance.ObjectMeta.Name
+	location := instance.Spec.Location
 	groupName := instance.Spec.ResourceGroup
 	server := instance.Spec.Server
-	location := instance.Spec.Location
+	dbname := instance.ObjectMeta.Name
 
 	// create the Go SDK client with relevant info
 	sdk := sql.GoSDKClient{
