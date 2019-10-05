5ab98db564823a8e4faa6bb1fbeca8fdbf93e8c8
Michael Dobrzynski <michaeldobrzynski@MININT-UPP0L65.redmond.corp.microsoft.com>
2019-10-05 12:16:46 -0600

    proof of concept full pipeline from secret creation, database transactions for user create and permissions, and finally writing secret

--- controllers/sqluser_controller.go
+++ controllers/sqluser_controller.go
@@ -21,10 +21,14 @@ import (
 	"fmt"
 
 	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	azurev1 "github.com/Azure/azure-service-operator/api/v1"
 
@@ -52,14 +56,18 @@ func (r *SqlUserReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
+	secret := r.GetOrPrepareSecret(&instance, instance.Spec.Server)
+
+	log.Info("GOT SECRET", "secret", string(secret.Data["username"]))
+	log.Info("GOT SECRET", "secret", string(secret.Data["password"]))
+
 	//location := instance.Spec.Location
-	server := "midobrzysqlserver.database.windows.net"
+	server := fmt.Sprintf("%s.database.windows.net", instance.Spec.Server)
 	//groupName := instance.Spec.ResourceGroup
 	database := "sqldatabase-aso-sql"
 	var port = 1433
-	//secret, _ := helpers.GetSecret("name", "namespace")
-	var user = ")W_FFhAe"
-	var password = "mS_n9M<lfLrqiAo&"
+	var user = string(secret.Data["username"])
+	var password = string(secret.Data["password"])
 
 	// Build connection string
 	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
@@ -73,7 +81,68 @@ func (r *SqlUserReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		log.Info("succesfully logged in")
 	}
 
+	newUser := "usernametest0"
+	newPassword := "pass@word1"
+	tsql := fmt.Sprintf("CREATE USER \"%s\" WITH PASSWORD='%s'", newUser, newPassword)
+
+	// Execute non-query with named parameters
+	_, err = db.ExecContext(ctx, tsql)
+	if err != nil {
+		log.Info("Error executing", "err", err.Error())
+	} else {
+		log.Info("Successfully executed user create")
+	}
+
+	tsql = fmt.Sprintf("sp_addrolemember '%s', %s", "db_owner", newUser)
+	_, err = db.ExecContext(ctx, tsql)
+	if err != nil {
+		log.Info("Error executing", "err", err.Error())
+	} else {
+		log.Info("Successfully gave user owner creds")
+	}
+
+	secret = r.GetOrPrepareSecret(&instance, newUser)
+	log.Info("GOT NEW SECRET", "secret", string(secret.Data["username"]))
+	log.Info("GOT NEW SECRET", "secret", string(secret.Data["password"]))
+
+	_, createOrUpdateSecretErr := controllerutil.CreateOrUpdate(context.Background(), r.Client, secret, func() error {
+		r.Log.Info("mutating secret bundle")
+		innerErr := controllerutil.SetControllerReference(&instance, secret, r.Scheme)
+		if innerErr != nil {
+			log.Info("inner err", "err", err.Error())
+		}
+		return nil
+	})
+	if createOrUpdateSecretErr != nil {
+		log.Info("createOrUpdateSecretErr", "err", err.Error())
+	}
+
+	// TODO: write the secret
 	return ctrl.Result{}, nil
+}
+
+// Get or prepare secret
+func (r *SqlUserReconciler) GetOrPrepareSecret(instance *azurev1.SqlUser, name string) *v1.Secret {
+
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: instance.Namespace,
+		},
+		Data: map[string][]byte{
+			"username":           []byte(generateRandomString(8)),
+			"password":           []byte(generateRandomString(16)),
+			"sqlservernamespace": []byte(instance.Namespace),
+			"sqlservername":      []byte(name),
+		},
+		Type: "Opaque",
+	}
+
+	if err := r.Get(context.Background(), types.NamespacedName{Name: name, Namespace: instance.Namespace}, secret); err == nil {
+		r.Log.Info("secret already exists, pulling creds now")
+	}
+
+	return secret
 }
 
 func (r *SqlUserReconciler) SetupWithManager(mgr ctrl.Manager) error {
