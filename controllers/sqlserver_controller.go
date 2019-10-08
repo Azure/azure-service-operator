ecb4e6bdb3a36528be5d273ae5b305eb413dedbc
Melanie Rush <merush@microsoft.com>
2019-10-08 11:07:48 -0600

    adding secret message and moving message

--- controllers/sqlserver_controller.go
+++ controllers/sqlserver_controller.go
@@ -195,6 +195,7 @@ func (r *SqlServerReconciler) reconcileExternal(instance *azurev1.SqlServer) err
 		}
 	} else {
 		r.Recorder.Event(instance, "Normal", "Provisioned", "resource request successfully submitted to Azure")
+		instance.Status.Message = "Successfully Submitted to Azure"
 	}
 
 	_, createOrUpdateSecretErr := controllerutil.CreateOrUpdate(context.Background(), r.Client, secret, func() error {
@@ -210,7 +211,6 @@ func (r *SqlServerReconciler) reconcileExternal(instance *azurev1.SqlServer) err
 	}
 
 	instance.Status.Provisioning = true
-	instance.Status.Message = "Successfully Submitted to Azure"
 
 	// write information back to instance
 	if updateerr := r.Status().Update(ctx, instance); updateerr != nil {
@@ -309,6 +309,7 @@ func (r *SqlServerReconciler) GetOrPrepareSecret(instance *azurev1.SqlServer) *v
 
 	if err := r.Get(context.Background(), types.NamespacedName{Name: name, Namespace: instance.Namespace}, secret); err == nil {
 		r.Log.Info("secret already exists, pulling creds now")
+		instance.Status.Message = "Secret exists on instance"
 	}
 
 	return secret
