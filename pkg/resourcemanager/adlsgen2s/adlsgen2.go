bda207217308314cba6d417d72e8fc03c621f248
root <root@2b131bd1b03f>
2019-10-08 22:36:43 +0000

    stub for resource manager

---
+++ pkg/resourcemanager/adlsgen2s/adlsgen2.go
@@ -0,0 +1,21 @@
+package adlsgen2s
+
+import (
+	""
+)
+
+type azureAdlsGen2Manager struct {}
+
+func getAdlsGen2Client() {}
+
+func (_ *azureAdlsGen2Manager) CreateAdlsGen2() {
+
+}
+
+func (_ *azureAdlsGen2Manager) GetAdlsGen2() {
+
+}
+
+func (_ *azureAdlsGen2Manager) DeleteAdlsGen2() {
+	
+}
