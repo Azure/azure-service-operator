c6569f6f75ba1a9c05aef83b0a2adea74dd84686
root <root@2b131bd1b03f>
2019-10-08 21:22:40 +0000

    add initial specs

--- api/v1/adlsgen2_types.go
+++ api/v1/adlsgen2_types.go
@@ -26,6 +26,8 @@ import (
 type AdlsGen2Spec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Location			string `json:"location, omitempty"`
+	ResourceGroupName 	string `json:"resourceGroup"`
 }
 
 // AdlsGen2Status defines the observed state of AdlsGen2
