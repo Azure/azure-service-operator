a4b1e63e035aa94939506956edd4c40764b78bec
root <root@2b131bd1b03f>
2019-10-09 21:16:53 +0000

    first pass at create

--- pkg/resourcemanager/adlsgen2s/adlsgen2.go
+++ pkg/resourcemanager/adlsgen2s/adlsgen2.go
@@ -1,13 +1,32 @@
 package adlsgen2s
 
-import ()
+import (
+	"context"
+	"log"
+	"github.com/Azure/azure-sdk-for-go/services/storage/datalake/2019-10-31/storagedatalake"
+	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
+	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
+
+)
 
 type azureAdlsGen2Manager struct{}
 
 func getAdlsGen2Client() {}
 
-func (_ *azureAdlsGen2Manager) CreateAdlsGen2() {
+func (_ *azureAdlsGen2Manager) CreateAdlsGen2(ctx context.Context, filesystem string, xMsProperties string, xMsClientRequestID string, timeout *int32, xMsDate string, accountName string) (*storagedatalake.FileSystem, error) {
+	fsClient := getFsClient(accountName)
+	// TODO: check to make sure filesystem name conforms correctly
+	future, err := fsClient.Create(ctx, filesystem, xMsProperties, xMsClientRequestID, timeout, xMsDate)
+	if err != nil {
+		return nil, err
+	}
 
+	err = future.WaitForCompletionRef(ctx, fsClient.FileSystemClient)
+	if err != nil {
+		return nil, err
+	}
+	result, err := future.Result(fsClient)
+	return &result, err
 }
 
 func (_ *azureAdlsGen2Manager) GetAdlsGen2() {
@@ -16,4 +35,17 @@ func (_ *azureAdlsGen2Manager) GetAdlsGen2() {
 
 func (_ *azureAdlsGen2Manager) DeleteAdlsGen2() {
 
+}
+
+func getFsClient(accountName string) storagedatalake.FileSystemClient {
+	xmsversion := "2019-10-31"
+	fsClient := storagedatalake.NewFileSystemClient(xmsversion, accountName)
+	
+	a, err := iam.GetResourceManagementAuthorizer()
+	if err != nil {
+		log.Fatalf("failed to initialize authorizer: %v\n", err)
+	}
+	fsClient.Authorizer = a
+	fsClient.AddToUserAgent(config.UserAgent())
+	return fsClient
 }
