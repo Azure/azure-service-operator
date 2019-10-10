92cabfd64b0345d721a0096276567d6c3d4e0ed3
root <root@2b131bd1b03f>
2019-10-10 21:19:54 +0000

    formatting

--- api/v1/adlsgen2_types.go
+++ api/v1/adlsgen2_types.go
@@ -26,7 +26,7 @@ import (
 type AdlsGen2Spec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	StorageAccountName	string `json:"storageAccountName,omitempty"`
+	StorageAccountName string `json:"storageAccountName,omitempty"`
 }
 
 // AdlsGen2Status defines the observed state of AdlsGen2
