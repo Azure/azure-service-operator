90fa99d4720b8112e1cf358db703e4ca761b8647
Melanie Rush <merush@microsoft.com>
2019-10-14 13:08:33 -0600

    adding types

--- api/v1alpha1/sqlfirewallrule_types.go
+++ api/v1alpha1/sqlfirewallrule_types.go
@@ -36,8 +36,9 @@ type SqlFirewallRuleSpec struct {
 type SqlFirewallRuleStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
-	Provisioning bool `json:"provisioning,omitempty"`
-	Provisioned  bool `json:"provisioned,omitempty"`
+	Provisioning bool 	`json:"provisioning,omitempty"`
+	Provisioned  bool 	`json:"provisioned,omitempty"`
+	Message		 string	`json:"message,omitempty"`
 }
 
 // +kubebuilder:object:root=true
