34695c924769faa2efca0fa17bcddfe177404482
root <root@2609d5ad3bed>
2019-10-22 23:22:58 +0000

    delete reconciles correctly. create without rg still throughs issue

--- pkg/resourcemanager/storages/filesystem.go
+++ pkg/resourcemanager/storages/filesystem.go
@@ -2,12 +2,14 @@ package storages
 
 import (
 	"context"
+	"errors"
+
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-02-01/resources"
 	"github.com/Azure/azure-sdk-for-go/services/storage/datalake/2019-10-31/storagedatalake"
 	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-04-01/storage"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
 	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/to"
 	"log"
 	"net/http"
 )
@@ -15,6 +17,10 @@ import (
 type azureFileSystemManager struct{}
 
 func (_ *azureFileSystemManager) CreateFileSystem(ctx context.Context, groupName string, filesystemName string, timeout *int32, xMsDate string, datalakeName string) (*autorest.Response, error) {
+	err := checkRG(ctx, groupName)
+	if err != nil {
+		return nil, err
+	}
 	client := getFileSystemClient(ctx, groupName, datalakeName)
 	// empty parameters are optional request headers (properties, requestid)
 	result, err := client.Create(ctx, filesystemName, "", "", timeout, xMsDate)
@@ -24,9 +30,30 @@ func (_ *azureFileSystemManager) CreateFileSystem(ctx context.Context, groupName
 
 	return &result, err
 }
+func getResourcesClient() resources.GroupsClient {
+	resourcesClient := resources.NewGroupsClient(config.SubscriptionID())
+	a, _ := iam.GetResourceManagementAuthorizer()
+	resourcesClient.Authorizer = a
+	resourcesClient.AddToUserAgent(config.UserAgent())
+	return resourcesClient
+}
+func checkRG(ctx context.Context, groupName string) error {
+	rgClient := getResourcesClient()
 
+	response, err := rgClient.CheckExistence(ctx, groupName)
+	if response.IsHTTPStatus(404) {
+		return errors.New("resource group does not exist")
+	}
+	return err
+}
 func (_ *azureFileSystemManager) GetFileSystem(ctx context.Context, groupName string, filesystemName string, timeout *int32, xMsDate string, datalakeName string) (autorest.Response, error) {
 	response := autorest.Response{Response: &http.Response{StatusCode: http.StatusNotFound}}
+
+	err := checkRG(ctx, groupName)
+	if err != nil {
+		return response, errors.New("unable to create filesystem")
+	}
+	response = autorest.Response{Response: &http.Response{StatusCode: http.StatusNotFound}}
 	client := getFileSystemClient(ctx, groupName, datalakeName)
 
 	// empty parameters are optional request headers (continuation, maxresults, requestid)
@@ -42,6 +69,11 @@ func (_ *azureFileSystemManager) GetFileSystem(ctx context.Context, groupName st
 }
 
 func (_ *azureFileSystemManager) DeleteFileSystem(ctx context.Context, groupName string, filesystemName string, timeout *int32, xMsDate string, datalakeName string) (autorest.Response, error) {
+	response := autorest.Response{Response: &http.Response{StatusCode: http.StatusAccepted}}
+	err := checkRG(ctx, groupName)
+	if err != nil {
+		return response, nil
+	}
 	client := getFileSystemClient(ctx, groupName, datalakeName)
 	// empty parameters are optional request headers (ifmodifiedsince, ifunmodifiedsince, requestid)
 	return client.Delete(ctx, filesystemName, "", "", "", timeout, xMsDate)
