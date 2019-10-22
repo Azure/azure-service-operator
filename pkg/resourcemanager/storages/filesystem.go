fb78d3aa5573d52926df36b7da0a17687bd6fea4
Claire McCarthy <clmccart@microsoft.com>
2019-10-22 13:21:04 -0700

    seperate out getaccountkey from authorizers

--- pkg/resourcemanager/storages/filesystem.go
+++ pkg/resourcemanager/storages/filesystem.go
@@ -3,6 +3,8 @@ package storages
 import (
 	"context"
 	"github.com/Azure/azure-sdk-for-go/services/storage/datalake/2019-10-31/storagedatalake"
+	"github.com/Azure/azure-sdk-for-go/services/storage/mgmt/2019-04-01/storage"
+
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
 	"github.com/Azure/go-autorest/autorest"
@@ -51,7 +53,12 @@ func getFileSystemClient(ctx context.Context, groupName string, accountName stri
 	fsClient := storagedatalake.NewFilesystemClient(xmsversion, accountName)
 	adlsClient := getStoragesClient()
 
-	a, err := iam.GetSharedKeyAuthorizer(ctx, groupName, accountName, adlsClient)
+	accountKey, err := getAccountKey(ctx, groupName, accountName, adlsClient)
+	if err != nil {
+		log.Fatalf("failed to get the account key for the authorizer: %v\n", err)		
+	}
+
+	a, err := iam.GetSharedKeyAuthorizer(accountName, accountKey)
 
 	if err != nil {
 		log.Fatalf("failed to initialize authorizer: %v\n", err)
@@ -60,4 +67,18 @@ func getFileSystemClient(ctx context.Context, groupName string, accountName stri
 	fsClient.AddToUserAgent(config.UserAgent())
 
 	return fsClient
+}
+
+func getAccountKey(ctx context.Context, groupName string, accountName string, adlsClient storage.AccountsClient) (accountKey string, err error) {
+	keys, err := adlsClient.ListKeys(ctx, groupName, accountName)
+	if err != nil {
+		return "", err
+	}
+
+	for _, key := range *keys.Keys {
+		if *key.KeyName == "key1" {
+			accountKey = *key.Value
+		}
+	}
+	return accountKey, err
 }
