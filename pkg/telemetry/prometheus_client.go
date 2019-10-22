9b0b86caa50cca0793b1db8615a519c9dcbb7399
William Mortl <wmortl@Williams-MBP.guest.corp.microsoft.com>
2019-10-22 14:44:12 -0600

    first

---
+++ pkg/telemetry/prometheus_client.go
@@ -0,0 +1,168 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package telemetry
+
+import (
+	"time"
+
+	"github.com/Azure/go-autorest/autorest/to"
+
+	log "github.com/go-logr/logr"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	namespace      *string
+	subsystem      *string
+	infoCounter    *prometheus.CounterVec
+	warningCounter *prometheus.CounterVec
+	errorCounter   *prometheus.CounterVec
+	activeGuage    *prometheus.GaugeVec
+	successCounter *prometheus.CounterVec
+	failureCounter *prometheus.CounterVec
+	executionTime  *prometheus.HistogramVec
+)
+
+// defaultNamespace is the default namespace for this project
+const defaultNamespace = "Azure"
+
+// defaultSubsystem is the default subsystem for this project
+const defaultSubsystem = "Operators"
+
+// exeuctionTimeStart base time == 0
+const exeuctionTimeStart = 0
+
+// exeuctionTimeWidth is the width of a bucket in the histogram, here it is 10s
+const exeuctionTimeWidth = 10
+
+// executionTimeBuckets is the number of buckets, here it 10 minutes worth of 10s buckets
+const executionTimeBuckets = 6 * 10
+
+// PrometheusClient stores information for the Prometheus implementation of telemetry
+type PrometheusClient struct {
+	Logger    log.Logger
+	Component string
+	StartTime time.Time
+}
+
+// InitializePrometheusDefault initializes a Prometheus client
+func InitializePrometheusDefault(logger log.Logger, component string) (client *PrometheusClient) {
+	return InitializePrometheus(logger, component, defaultNamespace, defaultSubsystem)
+}
+
+// InitializePrometheus initializes a Prometheus client
+func InitializePrometheus(logger log.Logger, component string, namespaceLog string, subsystemLog string) (client *PrometheusClient) {
+
+	// initialize globals if neccessary
+	if namespace == nil {
+		namespace = to.StringPtr(namespaceLog)
+		subsystem = to.StringPtr(subsystemLog)
+		initializeAllCounters()
+	}
+
+	// create the client
+	return &PrometheusClient{
+		Logger:    logger,
+		Component: component,
+	}
+}
+
+// initializeAllCounters inits all counts
+func initializeAllCounters() {
+
+	infoCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "Info",
+		},
+		[]string{
+			"component",
+			"type",
+			"message",
+		},
+	)
+	prometheus.MustRegister(infoCounter)
+
+	warningCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "Warning",
+		},
+		[]string{
+			"component",
+			"type",
+			"message",
+		},
+	)
+	prometheus.MustRegister(warningCounter)
+
+	errorCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "Error",
+		},
+		[]string{
+			"component",
+			"error",
+		},
+	)
+	prometheus.MustRegister(errorCounter)
+
+	executionTime = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "ExecutionTime",
+			Buckets: prometheus.LinearBuckets(
+				exeuctionTimeStart,
+				exeuctionTimeWidth,
+				executionTimeBuckets),
+		},
+		[]string{
+			"component",
+		},
+	)
+	prometheus.MustRegister(executionTime)
+
+	activeGuage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "ActiveGuage",
+		},
+		[]string{
+			"component",
+		},
+	)
+	prometheus.MustRegister(activeGuage)
+
+	successCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "Success",
+		},
+		[]string{
+			"component",
+		},
+	)
+	prometheus.MustRegister(successCounter)
+
+	failureCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      "Failure",
+		},
+		[]string{
+			"component",
+		},
+	)
+	prometheus.MustRegister(failureCounter)
+}
---
+++ pkg/telemetry/prometheus_client_implementation.go
@@ -0,0 +1,99 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package telemetry
+
+import (
+	"math"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// LogTrace logs a trace message, it does not send telemetry to Prometheus
+func (client PrometheusClient) LogTrace(typeTrace string, message string) {
+	client.Logger.Info(message, "Trace Type", typeTrace, "Component", client.Component)
+}
+
+// LogInfo logs an informational message
+func (client PrometheusClient) LogInfo(typeInfo string, message string) {
+	client.Logger.Info(message, "Info Type", typeInfo, "Component", client.Component)
+	infoCounter.WithLabelValues(client.Component, typeInfo, message).Inc()
+}
+
+// LogWarning logs a warning
+func (client PrometheusClient) LogWarning(typeWarning string, message string) {
+	// logs this as info as there's no go-logr warning level
+	client.Logger.Info(message, "Warning Type", typeWarning, "Component", client.Component)
+	warningCounter.WithLabelValues(client.Component, typeWarning, message).Inc()
+}
+
+// LogError logs an error
+func (client PrometheusClient) LogError(message string, err error) {
+	errorString := err.Error()
+
+	// logs the error as info (eventhough there's an error) as this follows the previous pattern
+	client.Logger.Info(message, "Component", client.Component, "Error", errorString)
+	infoCounter.WithLabelValues(client.Component, errorString).Inc()
+}
+
+// LogStart logs the start of a component
+func (client PrometheusClient) LogStart() {
+
+	// mark now as the start time
+	client.StartTime = time.Now()
+
+	// log that operator is running
+	client.Logger.Info("Component has started", "Component", client.Component)
+	activeGuage.WithLabelValues(client.Component).Inc()
+}
+
+// logCompletedOperation logs a component as completed
+func logCompleted(client PrometheusClient) {
+
+	// log that operator has completed
+	activeGuage.WithLabelValues(client.Component).Dec()
+
+	// record the time for the histogram
+	durationInSecs := float64(time.Since(client.StartTime)) * math.Pow10(9)
+	client.Logger.Info("Component has completed", "Component", client.Component, "Duration", durationInSecs)
+	executionTime.WithLabelValues(client.Component).Observe(durationInSecs)
+}
+
+// LogSuccess logs the successful completion of a component
+func (client PrometheusClient) LogSuccess() {
+
+	// log the completion
+	logCompleted(client)
+
+	// log success
+	client.Logger.Info("Component completed successfully", "Component", client.Component)
+	successCounter.WithLabelValues(client.Component).Inc()
+}
+
+// LogFailure logs the successful completion of a component
+func (client PrometheusClient) LogFailure() {
+
+	// log the completion
+	logCompleted(client)
+
+	// log success
+	client.Logger.Info("Component completed unsuccessfully", "Component", client.Component)
+	failureCounter.WithLabelValues(client.Component).Inc()
+}
+
+// CreateHistogram creates a histogram, start = what value the historgram starts at, width = how wide are the buckets,
+// numberOfBuckets = the number of buckets in the histogram
+func (client PrometheusClient) CreateHistogram(name string, start float64, width float64, numberOfBuckets int) (histogram prometheus.Histogram) {
+	histogram = prometheus.NewHistogram(
+		prometheus.HistogramOpts{
+			Namespace: *namespace,
+			Subsystem: *subsystem,
+			Name:      name,
+			Buckets:   prometheus.LinearBuckets(start, width, numberOfBuckets),
+		})
+
+	return histogram
+}
