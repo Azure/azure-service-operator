e524f927c97e7e913ad12afa842c0977bba0cb70
Melanie Rush <merush@microsoft.com>
2019-10-24 16:02:32 -0600

    fix msg

--- controllers/keyvault_controller.go
+++ controllers/keyvault_controller.go
@@ -101,8 +101,8 @@ func (r *KeyVaultReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		}
 		return ctrl.Result{}, fmt.Errorf("error reconciling keyvault in azure: %v", err)
 	}
-	r.Recorder.Event(&instance, v1.EventTypeNormal, "Provisioned", "Keyvault "+instance.ObjectMeta.Name+" provisioned ")
-	msg := fmt.Sprintf("Keyvault%s successfully provisioned", instance.ObjectMeta.Name)
+	msg := fmt.Sprintf("%s successfully provisioned", instance.ObjectMeta.Name)
+	r.Recorder.Event(&instance, v1.EventTypeNormal, "Provisioned", msg)
 	log.Info(msg)
 	instance.Status.Message = msg
 	return ctrl.Result{}, nil
@@ -130,7 +130,7 @@ func (r *KeyVaultReconciler) reconcileExternal(instance *azurev1alpha1.KeyVault)
 	var final error
 	if _, err := r.KeyVaultManager.CreateVault(ctx, groupName, name, location); err != nil {
 		if errhelp.IsAsynchronousOperationNotComplete(err) || errhelp.IsGroupNotFound(err) {
-			msg := fmt.Sprintf("Keyvault%s provisioning", name)
+			msg := fmt.Sprintf("%s provisioning", name)
 			r.Recorder.Event(instance, v1.EventTypeNormal, "Provisioning", msg)
 			instance.Status.Message = msg
 			instance.Status.Provisioning = true
