96d6466f09ae71240166c08800fe75e4f49e53d3
root <root@2609d5ad3bed>
2019-11-01 18:40:28 +0000

    reorder imports

--- pkg/resourcemanager/sqlclient/sqlclient_godsk.go
+++ pkg/resourcemanager/sqlclient/sqlclient_godsk.go
@@ -3,12 +3,12 @@ package sqlclient
 import (
 	"context"
 	"errors"
+	"net/http"
 	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/2015-05-01-preview/sql"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/to"
-	"net/http"
 )
 
 const typeOfService = "Microsoft.Sql/servers"
