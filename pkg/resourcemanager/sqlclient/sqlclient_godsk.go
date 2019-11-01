4df0174fd367473754f2bbf9b4f6ae50f97a705f
Claire McCarthy <clmccart@microsoft.com>
2019-10-31 17:24:18 -0700

    add composite literals

--- pkg/resourcemanager/sqlclient/sqlclient_godsk.go
+++ pkg/resourcemanager/sqlclient/sqlclient_godsk.go
@@ -51,35 +51,35 @@ func getGoFailoverGroupsClient() sql.FailoverGroupsClient {
 
 // CreateOrUpdateSQLServer creates a SQL server in Azure
 func (sdk GoSDKClient) CreateOrUpdateSQLServer(ctx context.Context, resourceGroupName string, location string, serverName string, properties SQLServerProperties) (result sql.Server, err error) {
-	// return MockCreateOrUpdateSQLServer()
-	serversClient := getGoServersClient()
-	serverProp := SQLServerPropertiesToServer(properties)
+	return MockCreateOrUpdateSQLServer()
+	// serversClient := getGoServersClient()
+	// serverProp := SQLServerPropertiesToServer(properties)
 
-	// Check name availability
-	checkNameResult, err := sdk.CheckNameAvailability(ctx, resourceGroupName, serverName)
-	if checkNameResult.Available == false {
-		return result, errors.New("InvalidServerName")
-	}
+	// // Check name availability
+	// checkNameResult, err := sdk.CheckNameAvailability(ctx, resourceGroupName, serverName)
+	// if checkNameResult.Available == false {
+	// 	return result, errors.New("InvalidServerName")
+	// }
 
-	// issue the creation
-	future, err := serversClient.CreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		serverName,
-		sql.Server{
-			Location:         to.StringPtr(location),
-			ServerProperties: &serverProp,
-		})
-	if err != nil {
-		return result, err
-	}
+	// // issue the creation
+	// future, err := serversClient.CreateOrUpdate(
+	// 	ctx,
+	// 	resourceGroupName,
+	// 	serverName,
+	// 	sql.Server{
+	// 		Location:         to.StringPtr(location),
+	// 		ServerProperties: &serverProp,
+	// 	})
+	// if err != nil {
+	// 	return result, err
+	// }
 
-	return future.Result(serversClient)
+	// return future.Result(serversClient)
 }
 
 func MockCreateOrUpdateSQLServer() (result sql.Server, err error) {
 	response := autorest.Response{
-		&http.Response{
+		Response: &http.Response{
 			StatusCode: 400,
 			Status:     "400 RegionDoesNotAllowProvisioning",
 			Proto:      "HTTP/1.0",
@@ -88,17 +88,17 @@ func MockCreateOrUpdateSQLServer() (result sql.Server, err error) {
 	err = errors.New("RegionDoesNotAllowProvisioning")
 
 	result = sql.Server{
-		response,
-		nil,
-		to.StringPtr(""),
-		&sql.ServerProperties{
+		Response: response,
+		Identity: nil,
+		Kind:     to.StringPtr(""),
+		ServerProperties: &sql.ServerProperties{
 			State: to.StringPtr("Unknown"),
 		},
-		to.StringPtr(""),
-		nil,
-		to.StringPtr(""),
-		to.StringPtr(""),
-		to.StringPtr(""),
+		Location: to.StringPtr(""),
+		Tags:     nil,
+		ID:       to.StringPtr(""),
+		Name:     to.StringPtr(""),
+		Type:     to.StringPtr(""),
 	}
 	return result, err
 }
