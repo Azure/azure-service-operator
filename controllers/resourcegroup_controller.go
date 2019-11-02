f291fec684df0c7c1063d54362dc14d8d7ce6f29
Amanda Kaufman <aka@microsoft.com>
2019-11-02 11:57:13 -0600

    Add tests for BlobContainer controller

--- controllers/resourcegroup_controller.go
+++ controllers/resourcegroup_controller.go
@@ -20,9 +20,11 @@ import (
 	resourcegroupsresourcemanager "github.com/Azure/azure-service-operator/pkg/resourcemanager/resourcegroups"
 
 	"github.com/go-logr/logr"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 // ResourceGroupReconciler reconciles a ResourceGroup object
