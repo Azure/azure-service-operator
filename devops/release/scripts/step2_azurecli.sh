6e9fce15a43162bad37d7387f29b0c445f2d46e2
Azadeh Khojandi <azkhojan@microsoft.com>
2019-11-14 13:31:04 +1100

    update release scripts

--- devops/release/scripts/step2_azurecli.sh
+++ devops/release/scripts/step2_azurecli.sh
@@ -1,10 +1,13 @@
 ACR_NAME='azureserviceoperator'
-echo "ACR_NAME"
-echo $ACR_NAME
-echo  $azureserviceoperator_image
-echo $azureserviceoperator_image_base
-echo $azureserviceoperator_image_latest
-echo $azureserviceoperator_image_public
+
+echo "ACR_NAME", $ACR_NAME
+echo  "azureserviceoperator_image", $(azureserviceoperator_image)
+echo "azureserviceoperator_image_base", $(azureserviceoperator_image_base)
+echo "azureserviceoperator_image_latest", $(azureserviceoperator_image_latest)
+echo "azureserviceoperator_image_public", $(azureserviceoperator_image_public)
+echo "azureserviceoperator_image_version", $(azureserviceoperator_image_version)
+
+
 az acr login --name $ACR_NAME
 docker pull $ACR_NAME.azurecr.io/$(azureserviceoperator_image)
 docker tag $ACR_NAME.azurecr.io/$(azureserviceoperator_image)  $ACR_NAME.azurecr.io/$(azureserviceoperator_image_latest)
