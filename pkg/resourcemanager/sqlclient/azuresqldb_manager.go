affb994a4aa98b2809e3f1a624420736184f6aab
Melanie Rush <merush@microsoft.com>
2019-11-25 13:31:32 -0700

    adding azure sqldb client and controller manager code

---
+++ pkg/resourcemanager/sqlclient/azuresqldb_manager.go
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft and contributors.  All rights reserved.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package sqlclient
+
+import (
+	"context"
+
+	"github.com/Azure/azure-sdk-for-go/services/preview/sql/mgmt/2015-05-01-preview/sql"
+	"github.com/Azure/go-autorest/autorest"
+)
+
+func NewAzureSqlDbManager(log logr.Logger) *AzureSqlDbManager {
+	return &AzureSqlDbManager{Log: log}
+}
+
+type AzureSqlDBManager interface {
+	CreateOrUpdateDB(ctx context.Context, resourceGroupName string, location string, serverName string, properties SQLDatabaseProperties) (sql.DatabasesCreateOrUpdateFuture, error)
+	DeleteDB(ctx context.Context, resourceGroupName string, serverName string, databaseName string) (result autorest.Response, err error)
+	GetDB(ctx context.Context, resourceGroupName string, serverName string, databaseName string) (sql.Database, error)
+}
---
+++ pkg/resourcemanager/sqlclient/azuresqldb_test.go
@@ -0,0 +1,23 @@
+package sqlclient
+
+import (
+	"context"
+)
+
+var _ = Describe("Azure SqlDB", func() {
+
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	Context("Create and Delete", func() {
+		It("should create and delete azure sqldb in azure", func() {
+
+		}
+	}
+
+}
