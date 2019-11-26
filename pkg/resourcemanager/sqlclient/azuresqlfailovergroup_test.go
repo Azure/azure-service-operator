c3a59e90eabe9296d86fa39f99158cd905c2e351
Melanie Rush <merush@microsoft.com>
2019-11-26 11:35:15 -0700

    fixing bracket issue in tests

---
+++ pkg/resourcemanager/sqlclient/azuresqlfailovergroup_test.go
@@ -0,0 +1,17 @@
+package sqlclient
+
+var _ = Describe("AzureSqlFailoverGroup", func() {
+
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	Context("Create and Delete", func() {
+		It("should create and delete AzureSqlFailoverGroup in azure", func() {
+		})
+	})
+})
