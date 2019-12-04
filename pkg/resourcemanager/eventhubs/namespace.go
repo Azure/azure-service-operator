4cb1f6021ccae02dedd32deb1986d29078800e1c
Erin Corson <frodopwns@gmail.com>
2019-12-03 17:43:31 -0700

    making the eventhub namespace client fit the AsyncClient requirements

--- pkg/resourcemanager/eventhubs/namespace.go
+++ pkg/resourcemanager/eventhubs/namespace.go
@@ -18,16 +18,27 @@ package eventhubs
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+
+	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
+	"github.com/go-logr/logr"
+
 	"github.com/Azure/go-autorest/autorest"
 
+	"github.com/Azure/azure-service-operator/pkg/errhelp"
+	"github.com/Azure/azure-service-operator/pkg/helpers"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/config"
 	"github.com/Azure/azure-service-operator/pkg/resourcemanager/iam"
 
 	"github.com/Azure/azure-sdk-for-go/services/eventhub/mgmt/2017-04-01/eventhub"
 	"github.com/Azure/go-autorest/autorest/to"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type azureEventHubNamespaceManager struct{}
+type azureEventHubNamespaceManager struct {
+	Log logr.Logger
+}
 
 func getNamespacesClient() eventhub.NamespacesClient {
 	nsClient := eventhub.NewNamespacesClient(config.SubscriptionID())
@@ -35,6 +46,12 @@ func getNamespacesClient() eventhub.NamespacesClient {
 	nsClient.Authorizer = auth
 	nsClient.AddToUserAgent(config.UserAgent())
 	return nsClient
+}
+
+func NewEventHubNamespaceClient(log logr.Logger) *azureEventHubNamespaceManager {
+	return &azureEventHubNamespaceManager{
+		Log: log,
+	}
 }
 
 // DeleteNamespace deletes an existing namespace. This operation also removes all associated resources under the namespace.
@@ -94,4 +111,132 @@ func (_ *azureEventHubNamespaceManager) CreateNamespaceAndWait(ctx context.Conte
 
 	result, err := future.Result(nsClient)
 	return &result, err
+}
+
+func (_ *azureEventHubNamespaceManager) CreateNamespace(ctx context.Context, resourceGroupName string, namespaceName string, location string) (eventhub.EHNamespace, error) {
+	nsClient := getNamespacesClient()
+
+	future, err := nsClient.CreateOrUpdate(
+		ctx,
+		resourceGroupName,
+		namespaceName,
+		eventhub.EHNamespace{
+			Location: to.StringPtr(location),
+		},
+	)
+	if err != nil {
+		return eventhub.EHNamespace{}, err
+	}
+
+	return future.Result(nsClient)
+}
+
+func (ns *azureEventHubNamespaceManager) Ensure(ctx context.Context, obj runtime.Object) (bool, error) {
+
+	instance, err := ns.convert(obj)
+	if err != nil {
+		return false, err
+	}
+
+	namespaceLocation := instance.Spec.Location
+	namespaceName := instance.Name
+	resourcegroup := instance.Spec.ResourceGroup
+
+	// write information back to instance
+	instance.Status.Provisioning = true
+
+	// @todo handle updates
+	evhns, err := ns.GetNamespace(ctx, resourcegroup, namespaceName)
+	if err == nil {
+		instance.Status.Provisioning = false
+		instance.Status.Provisioned = true
+		instance.Status.State = *evhns.ProvisioningState
+		instance.Status.Message = "Namespace already existed"
+
+		if *evhns.ProvisioningState == "Succeeded" {
+			return true, nil
+		}
+
+		return false, nil
+	}
+
+	// @todo update owner reference to point at resourcegroup
+
+	// create Event Hubs namespace
+	_, err = ns.CreateNamespace(ctx, resourcegroup, namespaceName, namespaceLocation)
+	if err != nil {
+		catch := []string{
+			errhelp.ResourceGroupNotFoundErrorCode,
+			errhelp.AsyncOpIncompleteError,
+		}
+		azerr := errhelp.NewAzureErrorAzureError(err)
+		if helpers.ContainsString(catch, azerr.Type) {
+			instance.Status.Message = err.Error()
+			return false, nil
+		}
+
+		instance.Status.Provisioning = false
+
+		return true, fmt.Errorf("EventhubNamespace create error %v", err)
+
+	}
+	// write information back to instance
+	instance.Status.Provisioning = false
+	instance.Status.Provisioned = true
+
+	return true, nil
+}
+
+func (ns *azureEventHubNamespaceManager) Delete(ctx context.Context, obj runtime.Object) (bool, error) {
+
+	instance, err := ns.convert(obj)
+	if err != nil {
+		return false, err
+	}
+
+	namespaceName := instance.Name
+	resourcegroup := instance.Spec.ResourceGroup
+
+	resp, err := ns.DeleteNamespace(ctx, resourcegroup, namespaceName)
+	if err != nil {
+		azerr := errhelp.NewAzureErrorAzureError(err)
+
+		// check if async op from previous delete was still happening
+		catch := []string{
+			errhelp.AsyncOpIncompleteError,
+			errhelp.RequestConflictError,
+		}
+		if helpers.ContainsString(catch, azerr.Type) {
+			return true, nil
+		}
+
+		// check if namespace was already gone
+		catch = []string{
+			errhelp.NotFoundErrorCode,
+		}
+		if helpers.ContainsString(catch, azerr.Type) {
+			return false, nil
+		}
+
+		// some error we don't know about or can't handle happened
+		instance.Status.Provisioning = false
+
+		return true, fmt.Errorf("EventhubNamespace delete error %v", err)
+
+	}
+
+	// if NoContent response is returned, namespace is already gone
+	if resp.StatusCode == http.StatusNoContent {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (ns *azureEventHubNamespaceManager) convert(obj runtime.Object) (*azurev1alpha1.EventhubNamespace, error) {
+	local, ok := obj.(*azurev1alpha1.EventhubNamespace)
+	if !ok {
+		return nil, fmt.Errorf("failed type assertion on kind: %s", obj.GetObjectKind().GroupVersionKind().String())
+	}
+	return local, nil
 }
