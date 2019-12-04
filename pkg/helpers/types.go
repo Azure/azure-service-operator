3b85f2597106f80284520c23c8e95f256d7da509
Erin Corson <frodopwns@gmail.com>
2019-12-04 12:12:36 -0700

    create new type for passing parents for referencing ownership

--- pkg/helpers/types.go
+++ pkg/helpers/types.go
@@ -1,6 +1,16 @@
 package helpers
 
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+)
+
 // Data wrapps the object that is needed for the services
 type Data struct {
 	Obj interface{}
+}
+
+type KubeParent struct {
+	Key    types.NamespacedName
+	Target runtime.Object
 }
