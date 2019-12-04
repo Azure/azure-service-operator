3082b0ba140380fa42be2d4afa485f037f7af85c
Melanie Rush <merush@microsoft.com>
2019-12-04 12:57:17 -0700

    adding skeleton for azuresqluser tests

--- pkg/resourcemanager/sqlclient/azuresqluser_test.go
+++ pkg/resourcemanager/sqlclient/azuresqluser_test.go
@@ -4,3 +4,23 @@
 // LICENSE file in the root directory of this source tree.
 
 package sqlclient
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("AzureSqlUser", func() {
+
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	Context("Create and Delete", func() {
+		It("should create and delete AzureSqlUserin azure", func() {
+		})
+	})
+})
