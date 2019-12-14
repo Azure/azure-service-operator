3ec0d8c6765ac1e527e4b0a3adde166a138b4839
James Kiefer <jakiefer@microsoft.com>
2019-12-13 17:50:26 -0700

    Debug: Try waiting for completion in DeleteGroup on future

--- pkg/resourcemanager/resourcegroups/resourcegroup.go
+++ pkg/resourcemanager/resourcegroups/resourcegroup.go
@@ -91,6 +91,8 @@ func (_ *AzureResourceGroupManager) DeleteGroup(ctx context.Context, groupName s
 		return autorest.Response{}, err
 	}
 
+	future.WaitForCompletionRef(ctx, client.Client)
+
 	return future.Result(client)
 }
 
