2e630e0797d07c81e49b5eaf9ad9e53e71aee0e1
Erin Corson <frodopwns@gmail.com>
2019-12-19 17:03:09 -0700

    changing this back

--- pkg/resourcemanager/sqlclient/endtoend_test.go
+++ pkg/resourcemanager/sqlclient/endtoend_test.go
@@ -154,7 +154,7 @@ func TestCreateOrUpdateSQLServer(t *testing.T) {
 	// create secondary SQL server
 	// create the Go SDK client with relevant info
 	secSrvName := generateName("sqlsrvsecondary")
-	secLocation := "eastus2"
+	secLocation := "westus"
 
 	// create the server
 	sqlServerProperties = SQLServerProperties{
