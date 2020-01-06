469b9e41bab3958ddd46c5b3aa371927fee85685
William Mortl <wmortl@Williams-MBP.guest.corp.microsoft.com>
2020-01-05 18:10:00 -0700

    other groups

--- pkg/resourcemanager/resources/groups.go
+++ pkg/resourcemanager/resources/groups.go
@@ -8,7 +8,6 @@ package resources
 import (
 	"context"
 	"fmt"
-	"log"
 	"strings"
 	"sync"
 
@@ -54,7 +53,6 @@ func CreateGroup(ctx context.Context, groupName string) (group resources.Group, 
 	if err != nil {
 		return resources.Group{}, err
 	}
-	log.Println(fmt.Sprintf("creating resource group '%s' on location: %v", groupName, config.Location()))
 	return groupsClient.CreateOrUpdate(
 		ctx,
 		groupName,
@@ -66,8 +64,10 @@ func CreateGroup(ctx context.Context, groupName string) (group resources.Group, 
 // CreateGroupWithAuthFile creates a new resource group. The client authorizer
 // is set up based on an auth file created using the Azure CLI.
 func CreateGroupWithAuthFile(ctx context.Context, groupName string) (resources.Group, error) {
-	groupsClient := getGroupsClientWithAuthFile()
-	log.Println(fmt.Sprintf("creating resource group '%s' on location: %v", groupName, config.Location()))
+	groupsClient, err := getGroupsClientWithAuthFile()
+	if err != nil {
+		return resources.Group{}, err
+	}
 	return groupsClient.CreateOrUpdate(
 		ctx,
 		groupName,
@@ -78,26 +78,34 @@ func CreateGroupWithAuthFile(ctx context.Context, groupName string) (resources.G
 
 // DeleteGroup removes the resource group named by env var
 func DeleteGroup(ctx context.Context, groupName string) (result resources.GroupsDeleteFuture, err error) {
-	groupsClient := getGroupsClient()
+	groupsClient, err := getGroupsClient()
+	if err != nil {
+		return resources.GroupsDeleteFuture{}, err
+	}
 	return groupsClient.Delete(ctx, groupName)
 }
 
 // ListGroups gets an interator that gets all resource groups in the subscription
 func ListGroups(ctx context.Context) (resources.GroupListResultIterator, error) {
-	groupsClient := getGroupsClient()
+	groupsClient, err := getGroupsClient()
+	if err != nil {
+		return resources.GroupListResultIterator{}, err
+	}
 	return groupsClient.ListComplete(ctx, "", nil)
 }
 
 // GetGroup gets info on the resource group in use
 func GetGroup(ctx context.Context) (resources.Group, error) {
-	groupsClient := getGroupsClient()
+	groupsClient, err := getGroupsClient()
+	if err != nil {
+		return resources.Group{}, err
+	}
 	return groupsClient.Get(ctx, config.GroupName())
 }
 
 // DeleteAllGroupsWithPrefix deletes all rescource groups that start with a certain prefix
 func DeleteAllGroupsWithPrefix(ctx context.Context, prefix string) (futures []resources.GroupsDeleteFuture, groups []string) {
 	if config.KeepResources() {
-		log.Println("keeping resource groups")
 		return
 	}
 	for list, _ := ListGroups(ctx); list.NotDone(); {
@@ -114,13 +122,14 @@ func DeleteAllGroupsWithPrefix(ctx context.Context, prefix string) (futures []re
 
 // WaitForDeleteCompletion concurrently waits for delete group operations to finish
 func WaitForDeleteCompletion(ctx context.Context, wg *sync.WaitGroup, futures []resources.GroupsDeleteFuture, groups []string) {
+	groupsClient, err := getGroupsClient()
+	if err != nil {
+		return
+	}
 	for i, f := range futures {
 		wg.Add(1)
 		go func(ctx context.Context, future resources.GroupsDeleteFuture, rg string) {
-			err := future.WaitForCompletionRef(ctx, getGroupsClient().Client)
-			if err == nil {
-				fmt.Printf("finished deleting group '%s'\n", rg)
-			}
+			err = future.WaitForCompletionRef(ctx, groupsClient.Client)
 			wg.Done()
 		}(ctx, f, groups[i])
 	}
