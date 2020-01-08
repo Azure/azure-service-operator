10380667f13abe66759cd1b484350d2bd3c92633
Erin Corson <frodopwns@gmail.com>
2020-01-08 14:43:05 -0700

    refactored all controller tests to remove ginkgo and add concurrency

--- controllers/keyvault_controller_test.go
+++ controllers/keyvault_controller_test.go
@@ -1,104 +1,83 @@
-/*
-Copyright 2019 microsoft.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// +build all keyvault
 
 package controllers
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"net/http"
-	"strings"
+	"testing"
 	"time"
 
 	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
 	"github.com/Azure/azure-service-operator/pkg/helpers"
-	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("KeyVault Controller", func() {
+func TestKeyvaultController(t *testing.T) {
+	t.Parallel()
+	RegisterTestingT(t)
+	PanicRecover()
+	ctx := context.Background()
 
 	keyVaultName := "t-kv-dev-" + helpers.RandomString(10)
 	const poll = time.Second * 10
 
-	Context("Key Vault Controller", func() {
+	keyVaultLocation := tc.resourceGroupLocation
 
-		It("Should Create and Delete Key Vault instances", func() {
+	// Declare KeyVault object
+	keyVaultInstance := &azurev1alpha1.KeyVault{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      keyVaultName,
+			Namespace: "default",
+		},
+		Spec: azurev1alpha1.KeyVaultSpec{
+			Location:          keyVaultLocation,
+			ResourceGroupName: tc.resourceGroupName,
+		},
+	}
 
-			defer GinkgoRecover()
+	// Create the Keyvault object and expect the Reconcile to be created
+	err := tc.k8sClient.Create(ctx, keyVaultInstance)
+	Expect(apierrors.IsInvalid(err)).To(Equal(false))
+	Expect(err).NotTo(HaveOccurred())
 
-			keyVaultLocation := tc.resourceGroupLocation
+	// Prep query for get
+	keyVaultNamespacedName := types.NamespacedName{Name: keyVaultName, Namespace: "default"}
 
-			// Declare KeyVault object
-			keyVaultInstance := &azurev1alpha1.KeyVault{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      keyVaultName,
-					Namespace: "default",
-				},
-				Spec: azurev1alpha1.KeyVaultSpec{
-					Location:          keyVaultLocation,
-					ResourceGroupName: tc.resourceGroupName,
-				},
-			}
+	// Wait until key vault is provisioned
+	Eventually(func() bool {
+		_ = tc.k8sClient.Get(ctx, keyVaultNamespacedName, keyVaultInstance)
+		//log.Print(keyVaultInstance.Status)
+		return keyVaultInstance.Status.Provisioned
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			// Create the Keyvault object and expect the Reconcile to be created
-			log.Print("Create")
-			err := tc.k8sClient.Create(context.Background(), keyVaultInstance)
-			Expect(apierrors.IsInvalid(err)).To(Equal(false))
-			Expect(err).NotTo(HaveOccurred())
+	// verify key vault exists in Azure
+	Eventually(func() bool {
+		result, _ := tc.keyVaultManager.GetVault(ctx, tc.resourceGroupName, keyVaultInstance.Name)
+		return result.Response.StatusCode == http.StatusOK
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			// Prep query for get
-			keyVaultNamespacedName := types.NamespacedName{Name: keyVaultName, Namespace: "default"}
+	// delete key vault
+	err = tc.k8sClient.Delete(ctx, keyVaultInstance)
+	Expect(err).NotTo(HaveOccurred())
 
-			// Wait until key vault is provisioned
-			Eventually(func() bool {
-				_ = tc.k8sClient.Get(context.Background(), keyVaultNamespacedName, keyVaultInstance)
-				//log.Print(keyVaultInstance.Status)
-				return keyVaultInstance.Status.Provisioned == true
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
+	// verify key vault is gone from kubernetes
+	Eventually(func() bool {
+		err := tc.k8sClient.Get(ctx, keyVaultNamespacedName, keyVaultInstance)
+		return apierrors.IsNotFound(err)
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			// verify key vault exists in Azure
-			Eventually(func() bool {
-				result, _ := tc.keyVaultManager.GetVault(context.Background(), tc.resourceGroupName, keyVaultInstance.Name)
-				return result.Response.StatusCode == http.StatusOK
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
+	// confirm key vault is gone from Azure
+	Eventually(func() bool {
+		result, _ := tc.keyVaultManager.GetVault(ctx, tc.resourceGroupName, keyVaultInstance.Name)
+		return result.Response.StatusCode == http.StatusNotFound
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			// delete key vault
-			err = tc.k8sClient.Delete(context.Background(), keyVaultInstance)
-			Expect(err).NotTo(HaveOccurred())
-
-			// verify key vault is gone from kubernetes
-			Eventually(func() bool {
-				err := tc.k8sClient.Get(context.Background(), keyVaultNamespacedName, keyVaultInstance)
-				if err == nil {
-					err = fmt.Errorf("")
-				}
-				return strings.Contains(err.Error(), "not found")
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
-
-			// confirm key vault is gone from Azure
-			Eventually(func() bool {
-				result, _ := tc.keyVaultManager.GetVault(context.Background(), tc.resourceGroupName, keyVaultInstance.Name)
-				return result.Response.StatusCode == http.StatusNotFound
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
-		})
-	})
-})
+}
--- controllers/postgresqlserver_controller_test.go
+++ controllers/postgresqlserver_controller_test.go
@@ -1,108 +1,81 @@
-/*
-Copyright 2019 microsoft.
+// +build all psql psqlserver
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package controllers
 
 import (
 	"context"
+	"testing"
 
 	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
 	"github.com/Azure/azure-service-operator/pkg/helpers"
-	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
-var _ = Describe("PostgreSQLServer Controller", func() {
+func TestPSQLServerController(t *testing.T) {
+	t.Parallel()
+	RegisterTestingT(t)
+	PanicRecover()
+	ctx := context.Background()
 
 	var rgName string
 	var rgLocation string
 	var postgreSQLServerName string
 
-	BeforeEach(func() {
-		// Add any setup steps that needs to be executed before each test
-		rgName = tc.resourceGroupName
-		rgLocation = tc.resourceGroupLocation
-	})
+	// Add any setup steps that needs to be executed before each test
+	rgName = tc.resourceGroupName
+	rgLocation = tc.resourceGroupLocation
 
-	AfterEach(func() {
-		// Add any teardown steps that needs to be executed after each test
-	})
+	postgreSQLServerName = "t-psql-srv-" + helpers.RandomString(10)
 
-	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
-	// your API definition.
-	// Avoid adding tests for vanilla CRUD operations because they would
-	// test Kubernetes API server, which isn't the goal here.
-	Context("Create and Delete", func() {
-		It("should create and delete PostgreSQL servers", func() {
+	// Create the PostgreSQLServer object and expect the Reconcile to be created
+	postgreSQLServerInstance := &azurev1alpha1.PostgreSQLServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      postgreSQLServerName,
+			Namespace: "default",
+		},
+		Spec: azurev1alpha1.PostgreSQLServerSpec{
+			Location:      rgLocation,
+			ResourceGroup: rgName,
+			Sku: azurev1alpha1.PSQLSku{
+				Name:     "B_Gen5_2",
+				Tier:     azurev1alpha1.SkuTier("Basic"),
+				Family:   "Gen5",
+				Size:     "51200",
+				Capacity: 2,
+			},
+			ServerVersion:  azurev1alpha1.ServerVersion("10"),
+			SSLEnforcement: azurev1alpha1.SslEnforcementEnumEnabled,
+		},
+	}
 
-			defer GinkgoRecover()
-			postgreSQLServerName = "t-psql-srv-" + helpers.RandomString(10)
+	err := tc.k8sClient.Create(ctx, postgreSQLServerInstance)
+	Expect(apierrors.IsInvalid(err)).To(Equal(false))
+	Expect(err).NotTo(HaveOccurred())
 
-			var err error
+	postgreSQLServerNamespacedName := types.NamespacedName{Name: postgreSQLServerName, Namespace: "default"}
 
-			// Create the PostgreSQLServer object and expect the Reconcile to be created
-			postgreSQLServerInstance := &azurev1alpha1.PostgreSQLServer{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      postgreSQLServerName,
-					Namespace: "default",
-				},
-				Spec: azurev1alpha1.PostgreSQLServerSpec{
-					Location:      rgLocation,
-					ResourceGroup: rgName,
-					Sku: azurev1alpha1.PSQLSku{
-						Name:     "B_Gen5_2",
-						Tier:     azurev1alpha1.SkuTier("Basic"),
-						Family:   "Gen5",
-						Size:     "51200",
-						Capacity: 2,
-					},
-					ServerVersion:  azurev1alpha1.ServerVersion("10"),
-					SSLEnforcement: azurev1alpha1.SslEnforcementEnumEnabled,
-				},
-			}
+	Eventually(func() bool {
+		_ = tc.k8sClient.Get(ctx, postgreSQLServerNamespacedName, postgreSQLServerInstance)
+		return helpers.HasFinalizer(postgreSQLServerInstance, finalizerName)
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			err = tc.k8sClient.Create(context.Background(), postgreSQLServerInstance)
-			Expect(apierrors.IsInvalid(err)).To(Equal(false))
-			Expect(err).NotTo(HaveOccurred())
+	Eventually(func() bool {
+		_ = tc.k8sClient.Get(ctx, postgreSQLServerNamespacedName, postgreSQLServerInstance)
+		return postgreSQLServerInstance.Status.Provisioned
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			postgreSQLServerNamespacedName := types.NamespacedName{Name: postgreSQLServerName, Namespace: "default"}
+	err = tc.k8sClient.Delete(ctx, postgreSQLServerInstance)
+	Expect(err).NotTo(HaveOccurred())
 
-			Eventually(func() bool {
-				_ = tc.k8sClient.Get(context.Background(), postgreSQLServerNamespacedName, postgreSQLServerInstance)
-				return helpers.HasFinalizer(postgreSQLServerInstance, finalizerName)
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
+	Eventually(func() bool {
+		err = tc.k8sClient.Get(ctx, postgreSQLServerNamespacedName, postgreSQLServerInstance)
+		return apierrors.IsNotFound(err)
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
 
-			Eventually(func() bool {
-				_ = tc.k8sClient.Get(context.Background(), postgreSQLServerNamespacedName, postgreSQLServerInstance)
-				return postgreSQLServerInstance.Status.Provisioned
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
-
-			err = tc.k8sClient.Delete(context.Background(), postgreSQLServerInstance)
-			Expect(err).NotTo(HaveOccurred())
-
-			Eventually(func() bool {
-				err = tc.k8sClient.Get(context.Background(), postgreSQLServerNamespacedName, postgreSQLServerInstance)
-				return err != nil
-			}, tc.timeout, tc.retry,
-			).Should(BeTrue())
-
-		})
-	})
-})
+}
