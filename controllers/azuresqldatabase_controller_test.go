b5fd85069b2139022a4a7cfe8126ee1ccb271db9
Erin Corson <frodopwns@gmail.com>
2020-01-09 14:25:07 -0700

    share test between sql database and server for happy path

--- controllers/azuresqldatabase_controller_test.go
+++ controllers/azuresqldatabase_controller_test.go
@@ -1,4 +1,4 @@
-// +build all azuresqldatabase
+// +build all azuresqlserver azuresqldatabase
 
 package controllers
 
@@ -8,6 +8,7 @@ import (
 
 	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
 
+	"github.com/Azure/azure-service-operator/pkg/errhelp"
 	helpers "github.com/Azure/azure-service-operator/pkg/helpers"
 	. "github.com/onsi/gomega"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -15,54 +16,66 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 )
 
-func TestAzureSqlDatabaseControllerHappyPath(t *testing.T) {
+func TestAzureSqlDatabaseControllerNoResourceGroup(t *testing.T) {
 	t.Parallel()
 	RegisterTestingT(t)
 	PanicRecover()
 	ctx := context.Background()
 
-	var rgName string
-	var rgLocation string
-	var sqlServerName string
-	var err error
-
 	// Add any setup steps that needs to be executed before each test
-	rgName = tc.resourceGroupName
-	rgLocation = tc.resourceGroupLocation
-	sqlServerName = "t-sqldb-test-srv" + helpers.RandomString(10)
+	rgLocation := tc.resourceGroupLocation
+	sqlServerName := "t-sqldb-test-srv" + helpers.RandomString(10)
 
-	// Create the SQL servers
-	// Create the SqlServer object and expect the Reconcile to be created
-	sqlServerInstance := &azurev1alpha1.AzureSqlServer{
+	sqlDatabaseName := "t-sqldatabase-dev-" + helpers.RandomString(10)
+
+	// Create the SqlDatabase object and expect the Reconcile to be created
+	sqlDatabaseInstance := &azurev1alpha1.AzureSqlDatabase{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      sqlServerName,
+			Name:      sqlDatabaseName,
 			Namespace: "default",
 		},
-		Spec: azurev1alpha1.AzureSqlServerSpec{
+		Spec: azurev1alpha1.AzureSqlDatabaseSpec{
 			Location:      rgLocation,
-			ResourceGroup: rgName,
+			ResourceGroup: "t-rg-test-srv" + helpers.RandomString(10),
+			Server:        sqlServerName,
+			Edition:       0,
 		},
 	}
 
-	err = tc.k8sClient.Create(ctx, sqlServerInstance)
+	err := tc.k8sClient.Create(ctx, sqlDatabaseInstance)
+	Expect(apierrors.IsInvalid(err)).To(Equal(false))
 	Expect(err).NotTo(HaveOccurred())
 
-	sqlServerNamespacedName := types.NamespacedName{Name: sqlServerName, Namespace: "default"}
+	sqlDatabaseNamespacedName := types.NamespacedName{Name: sqlDatabaseName, Namespace: "default"}
 
-	// Check to make sure the SQL server is provisioned before moving ahead
 	Eventually(func() string {
-		_ = tc.k8sClient.Get(ctx, sqlServerNamespacedName, sqlServerInstance)
-		return sqlServerInstance.Status.Message
+		_ = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
+		return sqlDatabaseInstance.Status.Message
 	}, tc.timeout, tc.retry,
-	).Should(ContainSubstring("successfully provisioned"))
+	).Should(ContainSubstring(errhelp.ResourceGroupNotFoundErrorCode))
 
-	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
-	// your API definition.
-	// Avoid adding tests for vanilla CRUD operations because they would
-	// test Kubernetes API server, which isn't the goal here.
+	err = tc.k8sClient.Delete(ctx, sqlDatabaseInstance)
+	//Expect(err).NotTo(HaveOccurred())  //Commenting as this call is async and returns an asyncopincomplete error
 
-	randomName := helpers.RandomString(10)
-	sqlDatabaseName := "t-sqldatabase-dev-" + randomName
+	Eventually(func() bool {
+		err = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
+		return apierrors.IsNotFound(err)
+	}, tc.timeout, tc.retry,
+	).Should(BeTrue())
+
+}
+
+func TestAzureSqlDatabaseControllerNoServer(t *testing.T) {
+	t.Parallel()
+	RegisterTestingT(t)
+	PanicRecover()
+	ctx := context.Background()
+
+	// Add any setup steps that needs to be executed before each test
+	rgName := tc.resourceGroupName
+	rgLocation := tc.resourceGroupLocation
+	sqlServerName := "t-sqldb-test-srv" + helpers.RandomString(10)
+	sqlDatabaseName := "t-sqldatabase-dev-" + helpers.RandomString(10)
 
 	// Create the SqlDatabase object and expect the Reconcile to be created
 	sqlDatabaseInstance := &azurev1alpha1.AzureSqlDatabase{
@@ -78,39 +91,23 @@ func TestAzureSqlDatabaseControllerHappyPath(t *testing.T) {
 		},
 	}
 
-	err = tc.k8sClient.Create(ctx, sqlDatabaseInstance)
+	err := tc.k8sClient.Create(ctx, sqlDatabaseInstance)
 	Expect(apierrors.IsInvalid(err)).To(Equal(false))
 	Expect(err).NotTo(HaveOccurred())
 
 	sqlDatabaseNamespacedName := types.NamespacedName{Name: sqlDatabaseName, Namespace: "default"}
 
-	Eventually(func() bool {
-		_ = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
-		return helpers.HasFinalizer(sqlDatabaseInstance, AzureSQLDatabaseFinalizerName)
-	}, tc.timeout, tc.retry,
-	).Should(BeTrue())
-
 	Eventually(func() string {
 		_ = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
 		return sqlDatabaseInstance.Status.Message
 	}, tc.timeout, tc.retry,
-	).Should(ContainSubstring("successfully provisioned"))
+	).Should(ContainSubstring(errhelp.ParentNotFoundErrorCode))
 
 	err = tc.k8sClient.Delete(ctx, sqlDatabaseInstance)
 	//Expect(err).NotTo(HaveOccurred())  //Commenting as this call is async and returns an asyncopincomplete error
 
 	Eventually(func() bool {
 		err = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
-		return apierrors.IsNotFound(err)
-	}, tc.timeout, tc.retry,
-	).Should(BeTrue())
-
-	// Add any teardown steps that needs to be executed after each test
-	// delete the sql servers from K8s
-	err = tc.k8sClient.Delete(ctx, sqlServerInstance)
-
-	Eventually(func() bool {
-		err = tc.k8sClient.Get(ctx, sqlServerNamespacedName, sqlServerInstance)
 		return apierrors.IsNotFound(err)
 	}, tc.timeout, tc.retry,
 	).Should(BeTrue())
--- controllers/azuresqlserver_controller_test.go
+++ controllers/azuresqlserver_controller_test.go
@@ -8,34 +8,21 @@ import (
 
 	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
 
+	"github.com/Azure/azure-service-operator/pkg/errhelp"
 	helpers "github.com/Azure/azure-service-operator/pkg/helpers"
 	. "github.com/onsi/gomega"
-	v1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 )
 
-func TestAzureSqlServerControllerHappyPath(t *testing.T) {
+func TestAzureSqlServerControllerNoResourceGroup(t *testing.T) {
 	t.Parallel()
 	RegisterTestingT(t)
 	PanicRecover()
 	ctx := context.Background()
 
-	var rgName string
-	var rgLocation string
-
-	// Add any setup steps that needs to be executed before each test
-	rgName = tc.resourceGroupName
-	rgLocation = tc.resourceGroupLocation
-
-	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
-	// your API definition.
-	// Avoid adding tests for vanilla CRUD operations because they would
-	// test Kubernetes API server, which isn't the goal here.
-
 	sqlServerName := "t-sqlserver-dev-" + helpers.RandomString(10)
-	var err error
 
 	// Create the SqlServer object and expect the Reconcile to be created
 	sqlServerInstance := &azurev1alpha1.AzureSqlServer{
@@ -44,12 +31,12 @@ func TestAzureSqlServerControllerHappyPath(t *testing.T) {
 			Namespace: "default",
 		},
 		Spec: azurev1alpha1.AzureSqlServerSpec{
-			Location:      rgLocation,
-			ResourceGroup: rgName,
+			Location:      tc.resourceGroupLocation,
+			ResourceGroup: "t-rg-fake-dev-" + helpers.RandomString(10),
 		},
 	}
 
-	err = tc.k8sClient.Create(ctx, sqlServerInstance)
+	err := tc.k8sClient.Create(ctx, sqlServerInstance)
 	Expect(apierrors.IsInvalid(err)).To(Equal(false))
 	Expect(err).NotTo(HaveOccurred())
 
@@ -65,20 +52,7 @@ func TestAzureSqlServerControllerHappyPath(t *testing.T) {
 		_ = tc.k8sClient.Get(ctx, sqlServerNamespacedName, sqlServerInstance)
 		return sqlServerInstance.Status.Message
 	}, tc.timeout, tc.retry,
-	).Should(ContainSubstring("successfully provisioned"))
-
-	//verify secret exists in k8s
-	secret := &v1.Secret{}
-	Eventually(func() bool {
-		err = tc.k8sClient.Get(ctx, types.NamespacedName{Name: sqlServerName, Namespace: sqlServerInstance.Namespace}, secret)
-		if err == nil {
-			if (secret.ObjectMeta.Name == sqlServerName) && (secret.ObjectMeta.Namespace == sqlServerInstance.Namespace) {
-				return true
-			}
-		}
-		return false
-	}, tc.timeout, tc.retry,
-	).Should(BeTrue())
+	).Should(ContainSubstring(errhelp.ResourceGroupNotFoundErrorCode))
 
 	err = tc.k8sClient.Delete(ctx, sqlServerInstance)
 	Expect(err).NotTo(HaveOccurred()) //sql server deletion is async
