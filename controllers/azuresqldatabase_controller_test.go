fa8bf790e2cd0dae232ea82975545186a98e8933
Erin Corson <frodopwns@gmail.com>
2020-01-08 17:00:44 -0700

    better checks for asqldb tests

--- controllers/azuresqldatabase_controller_test.go
+++ controllers/azuresqldatabase_controller_test.go
@@ -92,7 +92,7 @@ func TestAzureSqlDatabaseControllerHappyPath(t *testing.T) {
 
 	Eventually(func() bool {
 		_ = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
-		return sqlDatabaseInstance.IsSubmitted()
+		return sqlDatabaseInstance.Status.Provisioned
 	}, tc.timeout, tc.retry,
 	).Should(BeTrue())
 
@@ -100,8 +100,8 @@ func TestAzureSqlDatabaseControllerHappyPath(t *testing.T) {
 	//Expect(err).NotTo(HaveOccurred())  //Commenting as this call is async and returns an asyncopincomplete error
 
 	Eventually(func() bool {
-		_ = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
-		return helpers.IsBeingDeleted(sqlDatabaseInstance)
+		err = tc.k8sClient.Get(ctx, sqlDatabaseNamespacedName, sqlDatabaseInstance)
+		return apierrors.IsNotFound(err)
 	}, tc.timeout, tc.retry,
 	).Should(BeTrue())
 
@@ -110,8 +110,8 @@ func TestAzureSqlDatabaseControllerHappyPath(t *testing.T) {
 	err = tc.k8sClient.Delete(ctx, sqlServerInstance)
 
 	Eventually(func() bool {
-		_ = tc.k8sClient.Get(ctx, sqlServerNamespacedName, sqlServerInstance)
-		return helpers.IsBeingDeleted(sqlServerInstance)
+		err = tc.k8sClient.Get(ctx, sqlServerNamespacedName, sqlServerInstance)
+		return apierrors.IsNotFound(err)
 	}, tc.timeout, tc.retry,
 	).Should(BeTrue())
 
