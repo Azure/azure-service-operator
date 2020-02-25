d34a5b49c84d7c73bd0e899b37f3e8424f67b74e
Erin Corson <frodopwns@gmail.com>
2020-02-25 12:49:07 -0700

    this function has been removed

--- controllers/keyvault_controller_test.go
+++ controllers/keyvault_controller_test.go
@@ -115,7 +115,7 @@ func TestKeyvaultControllerWithAccessPolicies(t *testing.T) {
 	keyVaultNamespacedName := types.NamespacedName{Name: keyVaultName, Namespace: "default"}
 	assert.Eventually(func() bool {
 		_ = tc.k8sClient.Get(ctx, keyVaultNamespacedName, keyVaultInstance)
-		return helpers.HasFinalizer(keyVaultInstance, finalizerName)
+		return HasFinalizer(keyVaultInstance, finalizerName)
 	}, tc.timeout, tc.retry, "wait for keyvault to have finalizer")
 	// Wait until key vault is provisioned
 	assert.Eventually(func() bool {
