d06ba1b06c94e9e1c897f977370d5384d1d291bd
Melanie Rush <merush@microsoft.com>
2020-04-09 14:15:30 -0600

    fixing createstorage 404

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -707,7 +707,7 @@ func setup() error {
 	// Create the Storage Account and Container
 	_, _ = storageAccountManager.CreateStorage(context.Background(), resourceGroupName, storageAccountName, resourcegroupLocation, azurev1alpha1.StorageAccountSku{
 		Name: "Standard_LRS",
-	}, "StorageAccount", map[string]*string{}, "", nil, nil)
+	}, "Storage", map[string]*string{}, "", nil, nil)
 
 	// Storage account needs to be in "Suceeded" state
 	// for container create to succeed
