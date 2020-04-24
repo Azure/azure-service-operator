13eed6ab4df248213d3ac59fc4bc6f011a9e5297
William Mortl <wmortl@Williams-MacBook-Pro.local>
2020-04-24 13:28:01 -0700

    removed fs

--- pkg/resourcemanager/storages/managers.go
+++ pkg/resourcemanager/storages/managers.go
@@ -20,20 +20,10 @@ func AzureStorageManagers(secretClient secrets.SecretClient, scheme *runtime.Sch
 	return StorageManagers{
 		StorageAccount: storageaccount.New(secretClient, scheme),
 		BlobContainer:  blobcontainer.New(),
-		FileSystem: &azureFileSystemManager{
-			SecretClient: secretClient,
-			Scheme:       scheme,
-		},
 	}
 }
 
 var EmptyAzureStorageManagers = StorageManagers{
 	StorageAccount: storageaccount.EmptyStorageManager(),
 	BlobContainer:  blobcontainer.EmptyBlobContainerManager(),
-	FileSystem:     &azureFileSystemManager{},
-=======
-var AzureStorageManagers = StorageManagers{
-	StorageAccount: storageaccount.New(),
-	BlobContainer:  blobcontainer.New(),
->>>>>>> 81d67f98519f5d0e8dba4aa13b157cd7b1e44a44
 }
