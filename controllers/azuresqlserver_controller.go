de8bb9587044a15f50540f3f6f36c6ce5cdf05b2
Erin Corson <frodopwns@gmail.com>
2020-05-12 12:51:16 -0600

    have to pass the correct version to the controller

--- controllers/azuresqlserver_controller.go
+++ controllers/azuresqlserver_controller.go
@@ -6,7 +6,6 @@ package controllers
 import (
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
 	"github.com/Azure/azure-service-operator/api/v1beta1"
 )
 
@@ -19,7 +18,7 @@ type AzureSqlServerReconciler struct {
 // +kubebuilder:rbac:groups=azure.microsoft.com,resources=azuresqlservers/status,verbs=get;update;patch
 
 func (r *AzureSqlServerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	return r.Reconciler.Reconcile(req, &azurev1alpha1.AzureSqlServer{})
+	return r.Reconciler.Reconcile(req, &v1beta1.AzureSqlServer{})
 }
 
 func (r *AzureSqlServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
