e8f97bc20f505a8ba02721fe875587acfd99a21e
Erin Corson <frodopwns@gmail.com>
2020-05-13 14:47:56 -0600

    add printer columns for v1beta1 of azuresqlserver

--- api/v1beta1/azuresqlserver_types.go
+++ api/v1beta1/azuresqlserver_types.go
@@ -25,6 +25,8 @@ type AzureSqlServerSpec struct {
 // +kubebuilder:storageversion
 
 // AzureSqlServer is the Schema for the azuresqlservers API
+// +kubebuilder:printcolumn:name="Provisioned",type="string",JSONPath=".status.provisioned"
+// +kubebuilder:printcolumn:name="Message",type="string",JSONPath=".status.message"
 type AzureSqlServer struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
