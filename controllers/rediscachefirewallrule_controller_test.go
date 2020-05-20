d803f089497b2dfe1bf8367b430eb75200e9261b
Melanie Rush <merush@microsoft.com>
2020-05-20 10:38:23 -0600

    comma fix

--- controllers/rediscachefirewallrule_controller_test.go
+++ controllers/rediscachefirewallrule_controller_test.go
@@ -7,7 +7,6 @@ package controllers
 
 import (
 	"context"
-	"net/http"
 	"testing"
 
 	azurev1alpha1 "github.com/Azure/azure-service-operator/api/v1alpha1"
@@ -31,16 +30,16 @@ func TestRedisCacheFirewallRuleHappyPath(t *testing.T) {
 	redisCacheFirewallRule := GenerateTestResourceNameWithRandom("rediscachefirewallrule", 10)
 
 	redisCacheFirewallRuleInstance := &azurev1alpha1.RedisCacheFirewallRule{
-		ObjectMeta: metav1.ObjectMeta {
-			Name: redisCacheFirewallRule,
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      redisCacheFirewallRule,
 			Namespace: "default",
-		}
+		},
 		Spec: azurev1alpha1.RedisCacheFirewallRuleSpec{
-			ResourceGroupName:rgName,
-			RedisCache: redisCache,
-			Properties: azurev1alpha1.RedisCacheFirewallRuleProperties {
-				StartIP: 0.0.0.0,
-				EndIP: 0.0.0.0,
+			ResourceGroupName: rgName,
+			RedisCache:        redisCache,
+			Properties: azurev1alpha1.RedisCacheFirewallRuleProperties{
+				StartIP: "0.0.0.0",
+				EndIP:   "0.0.0.0",
 			},
 		},
 	}
@@ -49,4 +48,5 @@ func TestRedisCacheFirewallRuleHappyPath(t *testing.T) {
 	EnsureInstance(ctx, t, tc, redisCacheFirewallRuleInstance)
 
 	// delete rcfwr
-	EnsureDelete(ctx, t, tc, redisCacheFirewallRuleInstance) 
+	EnsureDelete(ctx, t, tc, redisCacheFirewallRuleInstance)
+}
