d379a712c9e7d552caf654ded77ef1a171e5bc12
Matthew Christopher <matthchr@microsoft.com>
2021-02-09 17:30:41 -0800

    Add 1.1.0 Helm chart (#1374)
    * Fix scripts again...
    
    * Add 1.1.0 Helm chart

--- devops/release/scripts/step1_releasepackage.sh
+++ devops/release/scripts/step1_releasepackage.sh
@@ -8,32 +8,32 @@ if [[ ! -f _Azure.azure-service-operator/drop/azure-service-operator.txt ]] ; th
     echo 'ERROR: File "_Azure.azure-service-operator/drop/azure-service-operator.txt" is not there, aborting.'
     exit -1
 fi
-azureserviceoperator_image=$(head -n 1 _Azure.azure-service-operator/drop/azure-service-operator.txt)
-azureserviceoperator_image_public=$(echo ${azureserviceoperator_image//candidate/public})
-azureserviceoperator_image_base=$(echo $azureserviceoperator_image | cut -d':' -f 1)
-azureserviceoperator_image_version=$(echo $azureserviceoperator_image | cut -d':' -f 2)
-azureserviceoperator_image_latest=$(echo ${azureserviceoperator_image_base//candidate/public})':latest'
+AZURESERVICEOPERATOR_IMAGE=$(head -n 1 _Azure.azure-service-operator/drop/azure-service-operator.txt)
+AZURESERVICEOPERATOR_IMAGE_PUBLIC=$(echo ${AZURESERVICEOPERATOR_IMAGE//candidate/public})
+AZURESERVICEOPERATOR_IMAGE_BASE=$(echo $AZURESERVICEOPERATOR_IMAGE | cut -d':' -f 1)
+AZURESERVICEOPERATOR_IMAGE_VERSION=$(echo $AZURESERVICEOPERATOR_IMAGE | cut -d':' -f 2)
+AZURESERVICEOPERATOR_IMAGE_LATEST=$(echo ${AZURESERVICEOPERATOR_IMAGE_BASE//candidate/public})':latest'
 
-echo "image: $azureserviceoperator_image"
-echo "image_base: $azureserviceoperator_image_base"
-echo "image_latest: $azureserviceoperator_image_latest"
-echo "image_public: $azureserviceoperator_image_public"
+echo "image: $AZURESERVICEOPERATOR_IMAGE"
+echo "image_base: $AZURESERVICEOPERATOR_IMAGE_BASE"
+echo "image_latest: $AZURESERVICEOPERATOR_IMAGE_LATEST"
+echo "image_public: $AZURESERVICEOPERATOR_IMAGE_PUBLIC"
 
-echo "##vso[task.setvariable variable=azureserviceoperator_image_latest]$azureserviceoperator_image_latest"
-echo "##vso[task.setvariable variable=azureserviceoperator_image]$azureserviceoperator_image"
-echo "##vso[task.setvariable variable=azureserviceoperator_image_public]$azureserviceoperator_image_public"
-echo "##vso[task.setvariable variable=azureserviceoperator_image_base]$azureserviceoperator_image_base"
-echo "##vso[task.setvariable variable=azureserviceoperator_image_version]$azureserviceoperator_image_version"
+echo "##vso[task.setvariable variable=AZURESERVICEOPERATOR_IMAGE_LATEST]$AZURESERVICEOPERATOR_IMAGE_LATEST"
+echo "##vso[task.setvariable variable=AZURESERVICEOPERATOR_IMAGE]$AZURESERVICEOPERATOR_IMAGE"
+echo "##vso[task.setvariable variable=AZURESERVICEOPERATOR_IMAGE_PUBLIC]$AZURESERVICEOPERATOR_IMAGE_PUBLIC"
+echo "##vso[task.setvariable variable=AZURESERVICEOPERATOR_IMAGE_BASE]$AZURESERVICEOPERATOR_IMAGE_BASE"
+echo "##vso[task.setvariable variable=AZURESERVICEOPERATOR_IMAGE_VERSION]$AZURESERVICEOPERATOR_IMAGE_VERSION"
 
 mkdir release/config -p
 cp -r _Azure.azure-service-operator/drop/setup.yaml ./release/config
 # Not currently including Helm charts as part of release so skip this
 # cp -r _Azure.azure-service-operator/drop/*.tgz ./release/config
-IMG=${azureserviceoperator_image_public/"public/"/"mcr.microsoft.com/"}
+IMG=${AZURESERVICEOPERATOR_IMAGE_PUBLIC/"public/"/"mcr.microsoft.com/"}
 echo "updating the manager image "
 echo $IMG
 sed -i'' -e 's@image: candidate/k8s@image: mcr.microsoft.com/k8s@' ./release/config/setup.yaml
-echo  ${azureserviceoperator_image_public/"public/"/"mcr.microsoft.com/"} >> ./release/notes.txt
+echo  ${AZURESERVICEOPERATOR_IMAGE_PUBLIC/"public/"/"mcr.microsoft.com/"} >> ./release/notes.txt
 cat ./release/config/setup.yaml
 echo 'ls ./release'
 ls ./release
--- devops/release/scripts/step3_pushhelmchart.sh
+++ devops/release/scripts/step3_pushhelmchart.sh
@@ -4,18 +4,18 @@ export HELM_EXPERIMENTAL_OCI=1
 ACR_NAME='asorelease'
 
 echo "ACR_NAME", $ACR_NAME
-echo "azureserviceoperator_image: $azureserviceoperator_image"
-echo "azureserviceoperator_image_base $azureserviceoperator_image_base"
-echo "azureserviceoperator_image_latest: $azureserviceoperator_image_latest"
-echo "azureserviceoperator_image_public: $azureserviceoperator_image_public"
-echo "azureserviceoperator_image_version: $azureserviceoperator_image_version"
+echo "AZURESERVICEOPERATOR_IMAGE: $AZURESERVICEOPERATOR_IMAGE"
+echo "AZURESERVICEOPERATOR_IMAGE_BASE $AZURESERVICEOPERATOR_IMAGE_BASE"
+echo "AZURESERVICEOPERATOR_IMAGE_LATEST: $AZURESERVICEOPERATOR_IMAGE_LATEST"
+echo "AZURESERVICEOPERATOR_IMAGE_PUBLIC: $AZURESERVICEOPERATOR_IMAGE_PUBLIC"
+echo "AZURESERVICEOPERATOR_IMAGE_VERSION: $AZURESERVICEOPERATOR_IMAGE_VERSION"
 
 echo $AZURE_CLIENT_SECRET | helm registry login $ACR_NAME.azurecr.io --username $AZURE_CLIENT_ID --password-stdin
 
-helm chart pull $ACR_NAME.azurecr.io/candidate/k8s/asohelmchart:$azureserviceoperator_image_version
-helm chart export $ACR_NAME.azurecr.io/candidate/k8s/asohelmchart:$azureserviceoperator_image_version --destination .
+helm chart pull $ACR_NAME.azurecr.io/candidate/k8s/asohelmchart:$AZURESERVICEOPERATOR_IMAGE_VERSION
+helm chart export $ACR_NAME.azurecr.io/candidate/k8s/asohelmchart:$AZURESERVICEOPERATOR_IMAGE_VERSION --destination .
 cd azure-service-operator
-helm chart save . $ACR_NAME.azurecr.io/public/k8s/asohelmchart:$azureserviceoperator_image_version
+helm chart save . $ACR_NAME.azurecr.io/public/k8s/asohelmchart:$AZURESERVICEOPERATOR_IMAGE_VERSION
 helm chart save . $ACR_NAME.azurecr.io/public/k8s/asohelmchart:latest
-helm chart push $ACR_NAME.azurecr.io/public/k8s/asohelmchart:$azureserviceoperator_image_version
+helm chart push $ACR_NAME.azurecr.io/public/k8s/asohelmchart:$AZURESERVICEOPERATOR_IMAGE_VERSION
 helm chart push $ACR_NAME.azurecr.io/public/k8s/asohelmchart:latest
