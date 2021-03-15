f6c26d0c48d904a6da1118dd443708a00f4c1e6b
Christian Muirhead <xtian@babbageclunk.com>
2021-03-15 16:55:48 +1300

    Never error when converting v1alpha2 MySQLAADUser -> v1alpha1
    (Unless JSON serialisation fails for some reason.) Instead we store
    the changed fields in an annotation and allow roundtripping that when
    converting in the other direction.

--- api/v1alpha1/mysqlaaduser_conversion.go
+++ api/v1alpha1/mysqlaaduser_conversion.go
@@ -4,7 +4,11 @@
 package v1alpha1
 
 import (
+	"encoding/json"
+	"sort"
+
 	"github.com/pkg/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
 	"github.com/Azure/azure-service-operator/api/v1alpha2"
@@ -12,11 +16,36 @@ import (
 
 var _ conversion.Convertible = &MySQLAADUser{}
 
+// To avoid losing information converting a v1alpha2 instance into
+// v1alpha1, we stash the affected fields (roles and database roles)
+// into a json-serialised struct in the annotations. When converting
+// back to a v1alpha2 instance we use any stashed values, layering any
+// changes to the database roles back over the top. The conversions
+// will only return errors if JSON marshalling/unmarshalling fails.
+
 func (src *MySQLAADUser) ConvertTo(dstRaw conversion.Hub) error {
 	dst := dstRaw.(*v1alpha2.MySQLAADUser)
 
 	// ObjectMeta
 	dst.ObjectMeta = src.ObjectMeta
+
+	// If there are stashed values in the annotations then we need to
+	// retrieve them first.
+	if encoded, found := src.getStashedAnnotation(src.ObjectMeta); found {
+		var stashedValues stashedMySQLAADUserFields
+		err := json.Unmarshal([]byte(encoded), &stashedValues)
+		if err != nil {
+			return errors.Wrap(err, "decoding stashed fields")
+		}
+		dst.Spec.Roles = stashedValues.Roles
+		dst.Spec.DatabaseRoles = stashedValues.DatabaseRoles
+
+		// Clear out the annotation to avoid confusion.
+		delete(dst.ObjectMeta.Annotations, conversionStashAnnotation)
+		if len(dst.ObjectMeta.Annotations) == 0 {
+			dst.ObjectMeta.Annotations = nil
+		}
+	}
 
 	// Spec
 	dst.Spec.ResourceGroup = src.Spec.ResourceGroup
@@ -24,10 +53,12 @@ func (src *MySQLAADUser) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.AADID = src.Spec.AADID
 	dst.Spec.Username = src.Spec.Username
 
-	// v1alpha1 doesn't support server-level roles, only
-	// database-level ones, so we move them into the new field.
-	dst.Spec.Roles = []string{}
-	dst.Spec.DatabaseRoles = make(map[string][]string)
+	if dst.Spec.Roles == nil {
+		dst.Spec.Roles = []string{}
+	}
+	if dst.Spec.DatabaseRoles == nil {
+		dst.Spec.DatabaseRoles = make(map[string][]string)
+	}
 	dst.Spec.DatabaseRoles[src.Spec.DBName] = append([]string(nil), src.Spec.Roles...)
 
 	// Status
@@ -36,21 +67,37 @@ func (src *MySQLAADUser) ConvertTo(dstRaw conversion.Hub) error {
 	return nil
 }
 
+func (dst *MySQLAADUser) getStashedAnnotation(meta metav1.ObjectMeta) (string, bool) {
+	if meta.Annotations == nil {
+		return "", false
+	}
+	value, found := meta.Annotations[conversionStashAnnotation]
+	return value, found
+}
+
 func (dst *MySQLAADUser) ConvertFrom(srcRaw conversion.Hub) error {
 	src := srcRaw.(*v1alpha2.MySQLAADUser)
 
-	// Converting a v1alpha2 user into a v1alpha1 one is only allowed
-	// if it has exactly one database...
-	if len(src.Spec.DatabaseRoles) != 1 {
-		return errors.Errorf("can't convert user %q to %T because it has privileges in %d databases", src.ObjectMeta.Name, dst, len(src.Spec.DatabaseRoles))
-	}
-	// ...and no server-level roles.
-	if len(src.Spec.Roles) != 0 {
-		return errors.Errorf("can't convert user %q to %T because it has server-level roles", src.ObjectMeta.Name, dst)
-	}
-
 	// ObjectMeta
 	dst.ObjectMeta = src.ObjectMeta
+
+	if len(src.Spec.DatabaseRoles) != 1 || len(src.Spec.Roles) != 0 {
+		// If this can't be represented exactly as a v1alpha1, store
+		// the original server-level and database roles in an
+		// annotation.
+		if dst.ObjectMeta.Annotations == nil {
+			dst.ObjectMeta.Annotations = make(map[string]string)
+		}
+		stashValues := stashedMySQLAADUserFields{
+			DatabaseRoles: src.Spec.DatabaseRoles,
+			Roles:         src.Spec.Roles,
+		}
+		encoded, err := json.Marshal(stashValues)
+		if err != nil {
+			return errors.Wrap(err, "encoding stashed fields")
+		}
+		dst.ObjectMeta.Annotations[conversionStashAnnotation] = string(encoded)
+	}
 
 	// Spec
 	dst.Spec.ResourceGroup = src.Spec.ResourceGroup
@@ -58,15 +105,37 @@ func (dst *MySQLAADUser) ConvertFrom(srcRaw conversion.Hub) error {
 	dst.Spec.AADID = src.Spec.AADID
 	dst.Spec.Username = src.Spec.Username
 
-	for dbName, roles := range src.Spec.DatabaseRoles {
-		dst.Spec.DBName = dbName
-		dst.Spec.Roles = append(dst.Spec.Roles, roles...)
-		break
+	// Pick the first database name to include as the DBName.
+	var dbNames []string
+	for dbName := range src.Spec.DatabaseRoles {
+		dbNames = append(dbNames, dbName)
 	}
+	sort.Strings(dbNames)
+	var (
+		dbName string
+		roles  []string
+	)
+	if len(dbNames) != 0 {
+		dbName = dbNames[0]
+		roles = src.Spec.DatabaseRoles[dbName]
+	}
+
+	dst.Spec.DBName = dbName
+	dst.Spec.Roles = append(dst.Spec.Roles, roles...)
 
 	// Status
 	dst.Status = ASOStatus(src.Status)
 
 	return nil
 
+}
+
+const conversionStashAnnotation = "azure.microsoft.com/convert-stash"
+
+// stashedMySQLAADUserFields stores values that can't be represented
+// directly on a v1alpha1 spec struct, so that they can be stored in
+// an annotation and used when converting to v1alpha2.
+type stashedMySQLAADUserFields struct {
+	DatabaseRoles map[string][]string `json:"databaseRoles,omitempty"`
+	Roles         []string            `json:"roles,omitempty"`
 }
