c8ab4ca5cd15ff261b5c60efaad81b96fb867c83
Matthew Christopher <matthchr@microsoft.com>
2021-03-15 17:08:13 -0700

    Update code_generator.go file structure (#415)
    - Rename confusingly named test file.
    - Move methods in code_generator_test into code_generator
      as they are on CodeGenerator and should be next to the
      struct definition for clarity.

--- hack/generator/pkg/codegen/code_generator.go
+++ hack/generator/pkg/codegen/code_generator.go
@@ -7,6 +7,7 @@ package codegen
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/pkg/errors"
 	kerrors "k8s.io/apimachinery/pkg/util/errors"
@@ -210,4 +211,91 @@ func (generator *CodeGenerator) verifyPipeline() error {
 	}
 
 	return kerrors.NewAggregate(errs)
+}
+
+// RemoveStages will remove all stages from the pipeline with the given ids.
+// Only available for test builds.
+// Will panic if you specify an unknown id.
+func (generator *CodeGenerator) RemoveStages(stageIds ...string) {
+	stagesToRemove := make(map[string]bool)
+	for _, s := range stageIds {
+		stagesToRemove[s] = false
+	}
+
+	var pipeline []PipelineStage
+
+	for _, stage := range generator.pipeline {
+		if _, ok := stagesToRemove[stage.id]; ok {
+			stagesToRemove[stage.id] = true
+			continue
+		}
+
+		pipeline = append(pipeline, stage)
+	}
+
+	for stage, removed := range stagesToRemove {
+		if !removed {
+			panic(fmt.Sprintf("Expected to remove stage %s from pipeline, but it wasn't found.", stage))
+		}
+	}
+
+	generator.pipeline = pipeline
+}
+
+// ReplaceStage replaces all uses of an existing stage with another one.
+// Only available for test builds.
+// Will panic if the existing stage is not found.
+func (generator *CodeGenerator) ReplaceStage(existingStage string, stage PipelineStage) {
+	replaced := false
+	for i, s := range generator.pipeline {
+		if s.HasId(existingStage) {
+			generator.pipeline[i] = stage
+			replaced = true
+		}
+	}
+
+	if !replaced {
+		panic(fmt.Sprintf("Expected to replace stage %s but it wasn't found", existingStage))
+	}
+}
+
+// InjectStageAfter injects a new stage immediately after the first occurrence of an existing stage
+// Only available for test builds.
+// Will panic if the existing stage is not found.
+func (generator *CodeGenerator) InjectStageAfter(existingStage string, stage PipelineStage) {
+	injected := false
+
+	for i, s := range generator.pipeline {
+		if s.HasId(existingStage) {
+			var p []PipelineStage
+			p = append(p, generator.pipeline[:i+1]...)
+			p = append(p, stage)
+			p = append(p, generator.pipeline[i+1:]...)
+			generator.pipeline = p
+			injected = true
+			break
+		}
+	}
+
+	if !injected {
+		panic(fmt.Sprintf("Expected to inject stage %s but %s wasn't found", stage.id, existingStage))
+	}
+}
+
+// HasStage tests whether the pipeline has a stage with the given id
+// Only available for test builds.
+func (generator *CodeGenerator) HasStage(id string) bool {
+	return generator.IndexOfStage(id) != -1
+}
+
+// IndexOfStage returns the index of the stage, if present, or -1 if not
+// Only available for test builds.
+func (generator *CodeGenerator) IndexOfStage(id string) int {
+	for i, s := range generator.pipeline {
+		if s.HasId(id) {
+			return i
+		}
+	}
+
+	return -1
 }
