bfabead441f2cb725f411c5b1ddd836fdb6c4d79
Christian Muirhead <christian.muirhead@microsoft.com>
2021-03-24 16:26:25 +1300

    Always allow converting MySQL[AAD]Users back to v1alpha1 (#1393)
    * Remove ignore entry for manager
    
    The output of `make manager` goes into bin/ which is already
    ignored. This entry was causing very confusing things to happen with
    directories called `manager` further down the directory tree.
    
    * Never error when converting v1alpha2 MySQLAADUser -> v1alpha1
    
    (Unless JSON serialisation fails for some reason.) Instead we store
    the changed fields in an annotation and allow roundtripping that when
    converting in the other direction.
    
    * Never error when converting v1alpha2 MySQLUser -> v1alpha1
    
    (Unless JSON serialisation fails for some reason.) Instead we store
    the changed fields in an annotation and allow roundtripping that when
    converting in the other direction.
    
    * Review feedback, thanks @matthchr
    
    * Comment out the replica server in the MySQL happy path test
    
    This is perpetually timing out for me at the moment, and testing
    manually shows that creation can take more than an hour.

--- api/v1alpha1/mysqlaaduser_types_test.go
+++ api/v1alpha1/mysqlaaduser_types_test.go
@@ -86,7 +86,7 @@ var _ = Describe("MySQLAADUser", func() {
 			}))
 		})
 
-		It("can't downgrade with roles for multiple databases", func() {
+		It("can downgrade with roles for multiple databases", func() {
 			v2 := v1alpha2.MySQLAADUser{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "foo",
@@ -99,13 +99,34 @@ var _ = Describe("MySQLAADUser", func() {
 						"mydb":    {"role1", "role2", "role3"},
 						"otherdb": {"otherrole"},
 					},
+					AADID:    "aadid",
+					Username: "username",
 				},
 			}
 			var v1 MySQLAADUser
-			Expect(v1.ConvertFrom(&v2)).To(MatchError("can't convert user \"foo\" to *v1alpha1.MySQLAADUser because it has privileges in 2 databases"))
+			Expect(v1.ConvertFrom(&v2)).To(Succeed())
+			Expect(v1).To(Equal(MySQLAADUser{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "default",
+					// Might need to account for ordering here -
+					// compare deserialised instead.
+					Annotations: map[string]string{
+						"azure.microsoft.com/convert-stash": `{"databaseRoles":{"mydb":["role1","role2","role3"],"otherdb":["otherrole"]}}`,
+					},
+				},
+				Spec: MySQLAADUserSpec{
+					Server:        "myserver",
+					ResourceGroup: "foo-group",
+					DBName:        "mydb",
+					Roles:         []string{"role1", "role2", "role3"},
+					AADID:         "aadid",
+					Username:      "username",
+				},
+			}))
 		})
 
-		It("can't downgrade with roles for no databases", func() {
+		It("can downgrade with roles for no databases", func() {
 			v2 := v1alpha2.MySQLAADUser{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "foo",
@@ -115,13 +136,32 @@ var _ = Describe("MySQLAADUser", func() {
 					Server:        "myserver",
 					ResourceGroup: "foo-group",
 					DatabaseRoles: nil,
+					AADID:         "aadid",
+					Username:      "username",
 				},
 			}
 			var v1 MySQLAADUser
-			Expect(v1.ConvertFrom(&v2)).To(MatchError("can't convert user \"foo\" to *v1alpha1.MySQLAADUser because it has privileges in 0 databases"))
+			Expect(v1.ConvertFrom(&v2)).To(Succeed())
+			Expect(v1).To(Equal(MySQLAADUser{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "default",
+					Annotations: map[string]string{
+						"azure.microsoft.com/convert-stash": `{}`,
+					},
+				},
+				Spec: MySQLAADUserSpec{
+					Server:        "myserver",
+					ResourceGroup: "foo-group",
+					DBName:        "",
+					Roles:         nil,
+					AADID:         "aadid",
+					Username:      "username",
+				},
+			}))
 		})
 
-		It("can't downgrade with server roles", func() {
+		It("can downgrade with server roles", func() {
 			v2 := v1alpha2.MySQLAADUser{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      "foo",
@@ -134,10 +174,70 @@ var _ = Describe("MySQLAADUser", func() {
 					DatabaseRoles: map[string][]string{
 						"mydb": {"role1", "role2", "role3"},
 					},
+					AADID:    "aadid",
+					Username: "username",
 				},
 			}
 			var v1 MySQLAADUser
-			Expect(v1.ConvertFrom(&v2)).To(MatchError("can't convert user \"foo\" to *v1alpha1.MySQLAADUser because it has server-level roles"))
+			Expect(v1.ConvertFrom(&v2)).To(Succeed())
+			Expect(v1).To(Equal(MySQLAADUser{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "default",
+					Annotations: map[string]string{
+						"azure.microsoft.com/convert-stash": `{"databaseRoles":{"mydb":["role1","role2","role3"]},"roles":["somekindofsuperuser"]}`,
+					},
+				},
+				Spec: MySQLAADUserSpec{
+					Server:        "myserver",
+					ResourceGroup: "foo-group",
+					DBName:        "mydb",
+					Roles:         []string{"role1", "role2", "role3"},
+					AADID:         "aadid",
+					Username:      "username",
+				},
+			}))
+		})
+
+		It("can incorporate annotations when upgrading", func() {
+			// Server-level roles should just be carried forwards.
+			// Database roles need to be applied to the ones in the annotation.
+			v1 := MySQLAADUser{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "default",
+					Annotations: map[string]string{
+						"azure.microsoft.com/convert-stash": `{"databaseRoles":{"mydb":["role1","role2","role3"],"otherdb":["anotherrole"]},"roles":["somekindofsuperuser"]}`,
+					},
+				},
+				Spec: MySQLAADUserSpec{
+					Server:        "myserver",
+					ResourceGroup: "foo-group",
+					DBName:        "mydb",
+					Roles:         []string{"role3", "role4"},
+					AADID:         "aadid",
+					Username:      "username",
+				},
+			}
+			var v2 v1alpha2.MySQLAADUser
+			Expect(v1.ConvertTo(&v2)).To(Succeed())
+			Expect(v2).To(Equal(v1alpha2.MySQLAADUser{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "foo",
+					Namespace: "default",
+				},
+				Spec: v1alpha2.MySQLAADUserSpec{
+					Server:        "myserver",
+					ResourceGroup: "foo-group",
+					Roles:         []string{"somekindofsuperuser"},
+					DatabaseRoles: map[string][]string{
+						"mydb":    {"role3", "role4"},
+						"otherdb": {"anotherrole"},
+					},
+					AADID:    "aadid",
+					Username: "username",
+				},
+			}))
 		})
 
 	})
