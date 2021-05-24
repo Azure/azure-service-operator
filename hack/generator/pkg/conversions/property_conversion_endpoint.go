b9e9b1659c627182f44a8696031d73c4939aa7ee
Bevan Arps <bevan.arps@microsoft.com>
2021-05-24 18:42:26 +1200

    Rename to PropertyConversionEndpoint for clarity

--- hack/generator/pkg/conversions/property_conversion_endpoint.go
+++ hack/generator/pkg/conversions/property_conversion_endpoint.go
@@ -10,9 +10,9 @@ import (
 	"github.com/gobuffalo/flect"
 )
 
-// StorageConversionEndpoint represents either a source or a destination target for a storage conversion
+// PropertyConversionEndpoint represents either a source or a destination target for a storage conversion
 // In simple cases these will be fields, but can also represent indexed members of slices and maps.
-type StorageConversionEndpoint struct {
+type PropertyConversionEndpoint struct {
 	// theType is the Type of the value accessible via this endpoint
 	theType astmodel.Type
 	// name is the name of the underlying property, used to generate useful local identifiers
@@ -24,35 +24,35 @@ type StorageConversionEndpoint struct {
 func NewStorageConversionEndpoint(
 	theType astmodel.Type,
 	name string,
-	knownLocals *astmodel.KnownLocalsSet) *StorageConversionEndpoint {
-	return &StorageConversionEndpoint{
+	knownLocals *astmodel.KnownLocalsSet) *PropertyConversionEndpoint {
+	return &PropertyConversionEndpoint{
 		theType:     theType,
 		name:        name,
 		knownLocals: knownLocals,
 	}
 }
 
-func (endpoint *StorageConversionEndpoint) Name() string {
+func (endpoint *PropertyConversionEndpoint) Name() string {
 	return endpoint.name
 }
 
 // Type returns the type of this endpoint
 //TODO: Remove ?
-func (endpoint *StorageConversionEndpoint) Type() astmodel.Type {
+func (endpoint *PropertyConversionEndpoint) Type() astmodel.Type {
 	return endpoint.theType
 }
 
 // CreateLocal creates an identifier for a local variable using one of the supplied suffixes if
 // possible. If all of those suffixes have been used, integer suffixes will be used
 // Each call will return a unique identifier
-func (endpoint *StorageConversionEndpoint) CreateLocal(suffix ...string) string {
+func (endpoint *PropertyConversionEndpoint) CreateLocal(suffix ...string) string {
 	singular := flect.Singularize(endpoint.name)
 	return endpoint.knownLocals.CreateLocal(singular, suffix...)
 }
 
 // WithType creates a new endpoint with a different type
-func (endpoint *StorageConversionEndpoint) WithType(theType astmodel.Type) *StorageConversionEndpoint {
-	return &StorageConversionEndpoint{
+func (endpoint *PropertyConversionEndpoint) WithType(theType astmodel.Type) *PropertyConversionEndpoint {
+	return &PropertyConversionEndpoint{
 		theType:     theType,
 		name:        endpoint.name,
 		knownLocals: endpoint.knownLocals,
