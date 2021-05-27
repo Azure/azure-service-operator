e465e3b508ad4f8407142c378761262265561350
George Pollard <gpollard@microsoft.com>
2021-05-27 22:44:18 +0000

    Add ServiceBus topic CRUD test

--- hack/generated/controllers/helpers_test.go
+++ hack/generated/controllers/helpers_test.go
@@ -24,3 +24,26 @@ func remainingTime(t *testing.T) time.Duration {
 
 	return DefaultResourceTimeout
 }
+
+type subtest struct {
+	name string
+	test func(t *testing.T)
+}
+
+func RunParallelSubtests(t *testing.T, tests ...subtest) {
+	// this looks super weird but is correct.
+	// parallel subtests do not run until their parent test completes,
+	// and then the parent test does not finish until all its subtests finish.
+	// so "subtests" will run and complete, then all the subtests will run
+	// in parallel, and then "subtests" will finish. ¯\_(ツ)_/¯
+	// See: https://blog.golang.org/subtests#TOC_7.2.
+	t.Run("subtests", func(t *testing.T) {
+		for _, test := range tests {
+			test := test
+			t.Run(test.name, func(t *testing.T) {
+				t.Parallel()
+				test.test(t)
+			})
+		}
+	})
+}
