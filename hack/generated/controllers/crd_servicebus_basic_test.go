dae753e045219fb7ac769f5d1d51ea4bab85c04b
George Pollard <gpollard@microsoft.com>
2021-05-31 09:07:43 +1200

    Add v2 Servicebus Topic CRUD test (#1522)
    Adds a Servicebus Standard SKU namespace test (topics cannot be created in Basic SKU) and subtests to create Queues & Topics. 
    
    Also adds a helper to run subtests in parallel and wait for them to finish.
    
    Refs: #1192, #1175

--- hack/generated/controllers/crd_servicebus_basic_test.go
+++ hack/generated/controllers/crd_servicebus_basic_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/Azure/azure-service-operator/hack/generated/pkg/testcommon"
 )
 
-func Test_ServiceBus_Namespace_CRUD(t *testing.T) {
+func Test_ServiceBus_Basic_CRUD(t *testing.T) {
 	t.Parallel()
 
 	g := NewGomegaWithT(t)
@@ -42,24 +42,23 @@ func Test_ServiceBus_Namespace_CRUD(t *testing.T) {
 		},
 	}
 
-	err = testContext.KubeClient.Create(ctx, namespace)
-	g.Expect(err).ToNot(HaveOccurred())
-
-	// It should be created in Kubernetes
-	g.Eventually(namespace).Should(testContext.Match.BeProvisioned(ctx))
-
-	// Run sub-tests
-	t.Run("Queue CRUD", func(t *testing.T) {
-		ServiceBus_Queue_CRUD(t, testContext, namespace.ObjectMeta)
-	})
+	// Create
+	g.Expect(testContext.KubeClient.Create(ctx, namespace)).To(Succeed())
+	g.Eventually(namespace, remainingTime(t)).Should(testContext.Match.BeProvisioned(ctx))
 
 	g.Expect(namespace.Status.Id).ToNot(BeNil())
 	armId := *namespace.Status.Id
 
+	RunParallelSubtests(t,
+		subtest{
+			name: "Queue CRUD",
+			test: func(t *testing.T) { ServiceBus_Queue_CRUD(t, testContext, namespace.ObjectMeta) },
+		},
+	)
+
 	// Delete
-	err = testContext.KubeClient.Delete(ctx, namespace)
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Eventually(namespace).Should(testContext.Match.BeDeleted(ctx))
+	g.Expect(testContext.KubeClient.Delete(ctx, namespace)).To(Succeed())
+	g.Eventually(namespace, remainingTime(t)).Should(testContext.Match.BeDeleted(ctx))
 
 	// Ensure that the resource was really deleted in Azure
 	exists, retryAfter, err := testContext.AzureClient.HeadResource(ctx, armId, "2018-01-01-preview")
@@ -70,7 +69,6 @@ func Test_ServiceBus_Namespace_CRUD(t *testing.T) {
 
 func ServiceBus_Queue_CRUD(t *testing.T, testContext testcommon.KubePerTestContext, sbNamespace metav1.ObjectMeta) {
 	ctx := context.Background()
-
 	g := NewGomegaWithT(t)
 
 	queue := &servicebus.NamespacesQueue{
@@ -82,17 +80,15 @@ func ServiceBus_Queue_CRUD(t *testing.T, testContext testcommon.KubePerTestConte
 	}
 
 	// Create
-	err := testContext.KubeClient.Create(ctx, queue)
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Eventually(queue).Should(testContext.Match.BeProvisioned(ctx))
+	g.Expect(testContext.KubeClient.Create(ctx, queue)).To(Succeed())
+	g.Eventually(queue, remainingTime(t)).Should(testContext.Match.BeProvisioned(ctx))
 
 	g.Expect(queue.Status.Id).ToNot(BeNil())
 
-	// Just a basic assertion on a property
+	// a basic assertion on a property
 	g.Expect(queue.Status.Properties.SizeInBytes).ToNot(BeNil())
 	g.Expect(*queue.Status.Properties.SizeInBytes).To(Equal(0))
 
-	err = testContext.KubeClient.Delete(ctx, queue)
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Eventually(queue).Should(testContext.Match.BeDeleted(ctx))
+	g.Expect(testContext.KubeClient.Delete(ctx, queue)).To(Succeed())
+	g.Eventually(queue, remainingTime(t)).Should(testContext.Match.BeDeleted(ctx))
 }
