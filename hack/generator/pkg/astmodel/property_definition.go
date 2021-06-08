Azure/k8s-infra

Add flatten property

hack/generator/pkg/astmodel/property_definition.go
@@ -23,6 +23,7 @@ type PropertyDefinition struct {
 	propertyType Type
 	description  string
 	isRequired   bool
+	flatten      bool // maps to x-ms-flatten: should the propertyType be flattened into the parent
 	tags         map[string][]string
 }
 
@@ -55,8 +56,23 @@ func (property *PropertyDefinition) PropertyType() Type {
 
 // SetRequired sets if the property is required or not
 func (property *PropertyDefinition) SetRequired(required bool) *PropertyDefinition {
+	if required == property.isRequired {
+		return property
+	}
+
 	result := *property
 	result.isRequired = required
+	return &result
+}
+
+// SetFlatten sets if the property should be flattened or not
+func (property *PropertyDefinition) SetFlatten(flatten bool) *PropertyDefinition {
+	if flatten == property.flatten {
+		return property
+	}
+
+	result := *property
+	result.flatten = flatten
 	return &result
 }
 

hack/generator/pkg/astmodel/property_definition_test.go
@@ -501,3 +501,31 @@ func TestPropertyDefinition_Equals_WhenGivenPropertyDefinition_ReturnsExpectedRe
 		})
 	}
 }
+
+func TestSettingSameRequiredValueDoesNotAllocateNewPropertyDefinition(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	strProperty := createStringProperty("FullName", "Full Legal Name")
+	strPropertyRequired := strProperty.SetRequired(true)
+
+	// safety check
+	g.Expect(strProperty).ToNot(Equal(strPropertyRequired))
+
+	// actual asserts
+	g.Expect(strProperty.SetRequired(false)).To(BeIdenticalTo(strProperty))
+	g.Expect(strPropertyRequired.SetRequired(true)).To(BeIdenticalTo(strPropertyRequired))
+}
+
+func TestSettingSameFlattenValueDoesNotAllocateNewPropertyDefinition(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	strProperty := createStringProperty("FullName", "Full Legal Name")
+	strPropertyFlatten := strProperty.SetFlatten(true)
+
+	// safety check
+	g.Expect(strProperty).ToNot(Equal(strPropertyFlatten))
+
+	// actual asserts
+	g.Expect(strProperty.SetFlatten(false)).To(BeIdenticalTo(strProperty))
+	g.Expect(strPropertyFlatten.SetFlatten(true)).To(BeIdenticalTo(strPropertyFlatten))
+}

Azure/azure-service-operator

hack/generator/pkg/astmodel/property_definition.go
@@ -24,6 +24,7 @@ type PropertyDefinition struct {
 	propertyType                     Type
 	description                      string
 	hasKubebuilderRequiredValidation bool
+	flatten                          bool // maps to x-ms-flatten: should the propertyType be flattened into the parent
 	tags                             map[string][]string
 }
 
@@ -58,8 +59,23 @@ func (property *PropertyDefinition) PropertyType() Type {
 // Note that this does not in any way change the underlying type that this PropertyDefinition points to, unlike
 // MakeRequired and MakeOptional.
 func (property *PropertyDefinition) WithKubebuilderRequiredValidation(required bool) *PropertyDefinition {
+	if required == property.hasKubebuilderRequiredValidation {
+		return property
+	}
+
 	result := *property
 	result.hasKubebuilderRequiredValidation = required
+	return &result
+}
+
+// SetFlatten sets if the property should be flattened or not
+func (property *PropertyDefinition) SetFlatten(flatten bool) *PropertyDefinition {
+	if flatten == property.flatten {
+		return property
+	}
+
+	result := *property
+	result.flatten = flatten
 	return &result
 }
 

hack/generator/pkg/astmodel/property_definition_test.go
@@ -520,3 +520,31 @@ func TestPropertyDefinition_Equals_WhenGivenPropertyDefinition_ReturnsExpectedRe
 		})
 	}
 }
+
+func TestSettingSameRequiredValueDoesNotAllocateNewPropertyDefinition(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	strProperty := createStringProperty("FullName", "Full Legal Name")
+	strPropertyRequired := strProperty.WithKubebuilderRequiredValidation(true)
+
+	// safety check
+	g.Expect(strProperty).ToNot(Equal(strPropertyRequired))
+
+	// actual asserts
+	g.Expect(strProperty.WithKubebuilderRequiredValidation(false)).To(BeIdenticalTo(strProperty))
+	g.Expect(strPropertyRequired.WithKubebuilderRequiredValidation(true)).To(BeIdenticalTo(strPropertyRequired))
+}
+
+func TestSettingSameFlattenValueDoesNotAllocateNewPropertyDefinition(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	strProperty := createStringProperty("FullName", "Full Legal Name")
+	strPropertyFlatten := strProperty.SetFlatten(true)
+
+	// safety check
+	g.Expect(strProperty).ToNot(Equal(strPropertyFlatten))
+
+	// actual asserts
+	g.Expect(strProperty.SetFlatten(false)).To(BeIdenticalTo(strProperty))
+	g.Expect(strPropertyFlatten.SetFlatten(true)).To(BeIdenticalTo(strPropertyFlatten))
+}