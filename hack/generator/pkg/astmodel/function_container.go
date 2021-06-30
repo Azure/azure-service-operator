03fba1bc041a6b9bc62f13cc65884ce55d1b684f
Bevan Arps <bevan.arps@microsoft.com>
2021-06-30 15:55:41 +1200

    Move AsFunctionContainer() into astmodel

--- hack/generator/pkg/astmodel/function_container.go
+++ hack/generator/pkg/astmodel/function_container.go
@@ -11,4 +11,19 @@ type FunctionContainer interface {
 	// Functions returns all the function implementations
 	// A sorted slice is returned to preserve immutability and provide determinism
 	Functions() []Function
+
+	// HasFunctionWithName determines if this resource has a function with the given name
+	HasFunctionWithName(name string) bool
+}
+
+// AsFunctionContainer converts a type into a function container
+func AsFunctionContainer(theType Type) (FunctionContainer, bool) {
+	switch t := theType.(type) {
+	case FunctionContainer:
+		return t, true
+	case MetaType:
+		return AsFunctionContainer(t.Unwrap())
+	default:
+		return nil, false
+	}
 }
