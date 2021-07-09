efb26721fc86002092713782413366a20bba7e88
Bevan Arps <bevan.arps@microsoft.com>
2021-07-09 12:10:04 +1200

    Use %s for Status types

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -255,12 +255,13 @@ func EnsureInstanceWithResult(ctx context.Context, t *testing.T, tc TestContext,
 
 		statused := ConvertToStatus(instance)
 		// if we expect this resource to end up with provisioned == true then failedProvisioning == true is unrecoverable
+		// TODO: Implement fmt.Stringer on Status types
 		if provisioned && statused.Status.FailedProvisioning {
 			if strings.Contains(statused.Status.Message, "already exists") || strings.Contains(statused.Status.Message, "AlreadyExists") {
 				t.Log("")
 				t.Log("-------")
 				t.Log("unexpected failed provisioning encountered")
-				t.Logf("%+v\n", statused.Status)
+				t.Logf("%s\n", statused.Status)
 				t.Logf("current time %s\n", time.Now().Format("2006-01-02 15:04:05"))
 				t.Log("-------")
 				t.Log("")
