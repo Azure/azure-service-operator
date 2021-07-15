f096d1de774891d542f7e12a686ca9fc6b56a728
Matthew Christopher <matthchr@users.noreply.github.com>
2021-07-15 16:21:16 -0700

    Add missed reference properties
      * Improve heuristic that detects references.
      * Fix panic when a reference is also flattened.

--- hack/generator/pkg/astmodel/armconversion/convert_to_arm_function_builder.go
+++ hack/generator/pkg/astmodel/armconversion/convert_to_arm_function_builder.go
@@ -259,7 +259,14 @@ func (builder *convertToARMBuilder) flattenedPropertyHandler(
 	// Copy each from-prop into the to-prop
 	for _, fromProp := range fromProps {
 		// find the corresponding inner property on the to-prop type
-		toSubProp, ok := toPropObjType.Property(fromProp.PropertyName())
+		// TODO: If this property is an ARM reference we need a bit of special handling.
+		// TODO: See https://github.com/Azure/azure-service-operator/issues/1651 for possible improvements to this.
+		toSubPropName := fromProp.PropertyName()
+		if values, ok := fromProp.Tag(astmodel.ARMReferenceTag); ok {
+			toSubPropName = astmodel.PropertyName(values[0])
+		}
+
+		toSubProp, ok := toPropObjType.Property(toSubPropName)
 		if !ok {
 			panic(fmt.Sprintf("unable to find expected property %s inside property %s", fromProp.PropertyName(), toPropName))
 		}
