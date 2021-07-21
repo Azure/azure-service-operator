21c1b2991d07cef5784c8ac1999955ab37a8cef3
Bevan Arps <bevan.arps@microsoft.com>
2021-07-22 11:26:34 +1200

    Update method names in interfaces

--- hack/generated/pkg/genruntime/convertible_spec.go
+++ hack/generated/pkg/genruntime/convertible_spec.go
@@ -6,9 +6,9 @@
 package genruntime
 
 type ConvertibleSpec interface {
-	// ConvertToSpec will populate the passed Spec by copying over all available information from this one
-	ConvertToSpec(destination ConvertibleSpec) error
+	// ConvertSpecTo will populate the passed Spec by copying over all available information from this one
+	ConvertSpecTo(destination ConvertibleSpec) error
 
-	// ConvertFromSpec will populate this spec by copying over all available information from the passed one
-	ConvertFromSpec(source ConvertibleSpec) error
+	// ConvertSpecFrom will populate this spec by copying over all available information from the passed one
+	ConvertSpecFrom(source ConvertibleSpec) error
 }
--- hack/generated/pkg/genruntime/convertible_status.go
+++ hack/generated/pkg/genruntime/convertible_status.go
@@ -6,10 +6,9 @@
 package genruntime
 
 type ConvertibleStatus interface {
-	// ConvertToStatus will populate the passed Status by copying over all available information from this one
-	ConvertToStatus(destination ConvertibleStatus) error
+	// ConvertStatusTo will populate the passed Status by copying over all available information from this one
+	ConvertStatusTo(destination ConvertibleStatus) error
 
-	// ConvertFromStatus will populate this status by copying over all available information from the passed one
-	ConvertFromStatus(source ConvertibleStatus) error
+	// ConvertStatusFrom will populate this status by copying over all available information from the passed one
+	ConvertStatusFrom(source ConvertibleStatus) error
 }
-
