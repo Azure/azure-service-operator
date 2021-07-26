f2afafa3decb9e8d7b46f76ef108492900ccb42d
Bevan Arps <bevan.arps@microsoft.com>
2021-07-26 16:27:23 +1200

    Refactor WritableConversinEndpoint

--- hack/generator/pkg/conversions/writable_conversion_endpoint.go
+++ hack/generator/pkg/conversions/writable_conversion_endpoint.go
@@ -14,7 +14,7 @@ import (
 	"github.com/Azure/azure-service-operator/hack/generator/pkg/astmodel"
 )
 
-// ReadableConversionEndpoint is an endpoint that includes a way to generate statements to write the final value
+// WritableConversionEndpoint is an endpoint that includes a way to generate statements to write the final value
 type WritableConversionEndpoint struct {
 	endpoint *TypedConversionEndpoint
 	// writer is a function that accepts an expression for the containing instance, another expression for the value to
@@ -26,22 +26,44 @@ type WritableConversionEndpoint struct {
 
 var _ fmt.Stringer = WritableConversionEndpoint{}
 
-// MakeWritableConversionEndpointForProperty creates a WritableConversionEndpoint for a specific property
-func MakeWritableConversionEndpointForProperty(
-	prop *astmodel.PropertyDefinition,
+// MakeWritableConversionEndpointWritingProperty creates a WritableConversionEndpoint for a specific property
+func MakeWritableConversionEndpointWritingProperty(
+	propertyName astmodel.PropertyName,
+	propertyType astmodel.Type,
 	knownLocals *astmodel.KnownLocalsSet,
 ) WritableConversionEndpoint {
-	propertyName := string(prop.PropertyName())
+	name := string(propertyName)
 	return WritableConversionEndpoint{
-		endpoint: NewStorageConversionEndpoint(prop.PropertyType(), propertyName, knownLocals),
+		endpoint: NewStorageConversionEndpoint(propertyType, name, knownLocals),
 		writer: func(destination dst.Expr, value dst.Expr) []dst.Stmt {
 			return []dst.Stmt{
 				astbuilder.SimpleAssignment(
-					astbuilder.Selector(destination, propertyName),
+					astbuilder.Selector(destination, name),
 					value),
 			}
 		},
 		description: fmt.Sprintf("write property %s", propertyName),
+	}
+}
+
+// MakeWritableConversionEndpointWritingBagItem creates a WritableConversionEndpoint that writes an item into a
+// property  bag
+func MakeWritableConversionEndpointWritingBagItem(
+	itemName string,
+	itemType astmodel.Type,
+	knownLocals *astmodel.KnownLocalsSet,
+) WritableConversionEndpoint {
+	return WritableConversionEndpoint{
+		endpoint: NewStorageConversionEndpoint(NewBagItemType(itemType), itemName, knownLocals),
+		writer: func(destination dst.Expr, value dst.Expr) []dst.Stmt {
+			return []dst.Stmt{
+				astbuilder.SimpleAssignment(
+					astbuilder.Selector(destination, itemName),
+					token.ASSIGN,
+					value),
+			}
+		},
+		description: fmt.Sprintf("write %s to property bag", itemName),
 	}
 }
 
@@ -54,7 +76,7 @@ func (w WritableConversionEndpoint) Write(destination dst.Expr, value dst.Expr) 
 	return w.writer(destination, value)
 }
 
-// Endpoint() provides access to the endpoint we write
+// Endpoint provides access to the endpoint we write
 func (w WritableConversionEndpoint) Endpoint() *TypedConversionEndpoint {
 	return w.endpoint
 }
