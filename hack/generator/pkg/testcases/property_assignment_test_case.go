3283c34e30178339d0b741500e45ff986d73a63b
Bevan Arps <bevan.arps@microsoft.com>
2021-08-11 19:47:46 +1200

    Implement PropertyAssignmentTestCase and test

--- hack/generator/pkg/testcases/property_assignment_test_case.go
+++ hack/generator/pkg/testcases/property_assignment_test_case.go
@@ -221,6 +221,7 @@ func (p *PropertyAssignmentTestCase) createTestMethod(
 		resultId     = "result"
 	)
 
+	//jsonPackage := codegenContext.MustGetImportedPackageName(astmodel.JsonReference)
 	cmpPackage := codegenContext.MustGetImportedPackageName(astmodel.CmpReference)
 	cmpoptsPackage := codegenContext.MustGetImportedPackageName(astmodel.CmpOptsReference)
 	prettyPackage := codegenContext.MustGetImportedPackageName(astmodel.PrettyReference)
