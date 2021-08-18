b081f0331b4edaf624f6078c12b2887c4e738aa1
Bevan Arps <bevan.arps@microsoft.com>
2021-08-18 14:58:42 +1200

    Add test for JSON serialization pipeline stage

---
+++ hack/generator/pkg/codegen/pipeline/json_serialization_test_cases_test.go
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package pipeline
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/Azure/azure-service-operator/hack/generator/pkg/astmodel"
+	"github.com/Azure/azure-service-operator/hack/generator/pkg/test"
+)
+
+func TestInjectJsonSerializationTests(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	idFactory := astmodel.NewIdentifierFactory()
+	// Test Resource V1
+
+	specV1 := test.CreateSpec(
+		test.Pkg2020,
+		"Person",
+		test.FullNameProperty,
+		test.FamilyNameProperty,
+		test.KnownAsProperty)
+	statusV1 := test.CreateStatus(test.Pkg2020, "Person")
+	resourceV1 := test.CreateResource(test.Pkg2020, "Person", specV1, statusV1)
+
+	// Test Resource V2
+
+	specV2 := test.CreateSpec(
+		test.Pkg2021,
+		"Person",
+		test.FullNameProperty,
+		test.FamilyNameProperty,
+		test.KnownAsProperty,
+		test.ResidentialAddress2021,
+		test.PostalAddress2021)
+	statusV2 := test.CreateStatus(test.Pkg2021, "Person")
+	resourceV2 := test.CreateResource(test.Pkg2021, "Person", specV2, statusV2)
+
+	types := make(astmodel.Types)
+	types.AddAll(resourceV1, specV1, statusV1, resourceV2, specV2, statusV2, test.Address2021)
+
+	state := NewState().WithTypes(types)
+
+	finalState, err := RunTestPipeline(
+		state,
+		CreateConversionGraph(),
+		CreateStorageTypes(),
+		InjectPropertyAssignmentFunctions(idFactory),
+		InjectJsonSerializationTests(idFactory))
+	g.Expect(err).To(Succeed())
+
+	/*
+	 * When verifying these golden tests, check the _test files contain the expected tests, test runners, and generator
+	 * factory methods.
+	 */
+	test.AssertPackagesGenerateExpectedCode(t, finalState.Types())
+}
