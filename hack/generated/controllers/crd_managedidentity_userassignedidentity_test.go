2a816c813ed3b4984f73da424f107e0f220581b8
Matthew Christopher <matthchr@users.noreply.github.com>
2021-09-14 15:01:24 -0700

    Use a standard pattern for most recording tests
    The pattern is <service>_<resource>_test.go for the filename, and
    Test_<service>_<resource>_CRUD for the test name. In places where
    the "common name" of the service and the actual ARM name are different,
    I used the common name as it usually reads a bit clearer (AKS instead of
    containerservice, CosmosDB instead of DocumentDB).

--- hack/generated/controllers/crd_managedidentity_userassignedidentity_test.go
+++ hack/generated/controllers/crd_managedidentity_userassignedidentity_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Azure/azure-service-operator/hack/generated/pkg/testcommon"
 )
 
-func Test_ManagedIdentity_CRUD(t *testing.T) {
+func Test_ManagedIdentity_UserAssignedIdentity_CRUD(t *testing.T) {
 	t.Parallel()
 
 	tc := globalTestContext.ForTest(t)
