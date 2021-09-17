581b71d3d26275a7840343ba40cb4fb163c3dca3
Christian Muirhead <christian.muirhead@microsoft.com>
2021-09-17 16:09:40 +1200

    operator bundle: Remove the cert volume and mount from the pod (#1793)
    * Remove the cert volume and mount from the operator bundle deployment
    
    These consume the certificate generated by cert-manager, but OLM will
    generate its own and hook them into the pod. This issue was preventing
    the operator from deploying correctly on OCP 4.6 (because the secret
    referred to in the mount doesn't exist and k8s 1.19 wouldn't start the pod).
    
    * Remove the conversion section from multiversion CRDs in the bundle
    
    This will be fully generated by OLM from the conversion
    webhookdefinitions in the CSV when the operator is installed.
    
    * Update previous bundle version in makefile
    
    1.0.28631 is landed in both the operatorhub and openshift bundle
    repos.

--- scripts/inject-container-reference.sh
+++ scripts/inject-container-reference.sh
@@ -11,3 +11,9 @@ sed -i "s!controller:latest!${container_reference}!g" $cluster_version_file
 
 # Insert containerImage and createdAt into metadata.annotations.
 yq eval -i ".metadata.annotations.containerImage = \"${container_reference}\" | .metadata.annotations.createdAt = \"${now}\"" $cluster_version_file
+
+# Remove cert volumes and volume mounts from the CSV deployment - the
+# ones here are for cert-manager, OLM will set its own to get the
+# webhook certificates installed in the pod.
+yq eval -i 'del(.spec.install.spec.deployments[0].spec.template.spec.containers[] | select(.name == "manager").volumeMounts)' $cluster_version_file
+yq eval -i 'del(.spec.install.spec.deployments[0].spec.template.spec.volumes)' $cluster_version_file
