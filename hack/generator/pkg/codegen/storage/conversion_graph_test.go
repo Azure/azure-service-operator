cc10b7db8f17090111dd6f59418fc12b3abbb374
Bevan Arps <bevan.arps@microsoft.com>
2021-09-30 10:53:00 +1300

    Modify ConversionGraph to support hub types not in the most recent package

--- hack/generator/pkg/codegen/storage/conversion_graph_test.go
+++ hack/generator/pkg/codegen/storage/conversion_graph_test.go
@@ -46,47 +46,70 @@ func TestConversionGraph_WithTwoUnrelatedReferences_HasExpectedTransitions(t *te
 func TestConversionGraph_GivenTypeName_ReturnsExpectedHubTypeName(t *testing.T) {
 	g := NewGomegaWithT(t)
 
+	// Create some resources to use for testing.
+	// Need three versions of the Person resource, plus the storage variants of those
+	person2020 := test.CreateSimpleResource(test.Pkg2020, "Person")
+	person2020s := test.CreateSimpleResource(test.Pkg2020s, "Person")
+	person2021 := test.CreateSimpleResource(test.Pkg2021, "Person")
+	person2021s := test.CreateSimpleResource(test.Pkg2021s, "Person")
+	person2022 := test.CreateSimpleResource(test.Pkg2022, "Person")
+	person2022s := test.CreateSimpleResource(test.Pkg2022s, "Person")
+
+	// Need two versions of the Address resource, plus the storage variants of those
+	address2020 := test.CreateSimpleResource(test.Pkg2020, "Address")
+	address2020s := test.CreateSimpleResource(test.Pkg2020s, "Address")
+	address2021 := test.CreateSimpleResource(test.Pkg2021, "Address")
+	address2021s := test.CreateSimpleResource(test.Pkg2021s, "Address")
+
+	// Create our set of types
+	types := make(astmodel.Types)
+	types.AddAll(person2020, person2021, person2022, address2020, address2021)
+	types.AddAll(person2020s, person2021s, person2022s, address2020s, address2021s)
+
+	// Create a builder use it to configure a graph to test
 	builder := NewConversionGraphBuilder()
-	builder.Add(test.Pkg2020)
-	builder.Add(test.Pkg2021)
-	builder.Add(test.Pkg2022)
+	builder.Add(person2020.Name().PackageReference)
+	builder.Add(person2021.Name().PackageReference)
+	builder.Add(person2022.Name().PackageReference)
 
 	graph, err := builder.Build()
 	g.Expect(err).To(Succeed())
 
-	pkg2020storage, ok := graph.LookupTransition(test.Pkg2020)
-	g.Expect(ok).To(BeTrue())
+	// We expect all person resources to use this hub
+	personHub := person2022s.Name()
 
-	pkg2021storage, ok := graph.LookupTransition(test.Pkg2021)
-	g.Expect(ok).To(BeTrue())
-
-	pkg2022storage, ok := graph.LookupTransition(test.Pkg2022)
-	g.Expect(ok).To(BeTrue())
+	// Address resources use this as the hub because Address doesn't exist in Pkg2022
+	addressHub := address2021s.Name()
 
 	cases := []struct {
-		name     string
-		start    astmodel.PackageReference
-		expected astmodel.PackageReference
+		name         string
+		start        astmodel.TypeName
+		expectedName astmodel.TypeName
+		expectedOk   bool
 	}{
-		{"Hub type resolves to self", pkg2022storage, pkg2022storage},
-		{"Directly linked api resolves", test.Pkg2021, pkg2022storage},
-		{"Directly linked storage resolves", pkg2021storage, pkg2022storage},
-		{"Doubly linked api resolves", test.Pkg2021, pkg2022storage},
-		{"Indirectly linked api resolves", test.Pkg2020, pkg2022storage},
-		{"Indirectly linked storage resolves", pkg2020storage, pkg2022storage},
+		{"Hub type returns nothing", personHub, astmodel.TypeName{}, false},
+		{"Directly linked api resolves", person2022.Name(), personHub, true},
+		{"Directly linked storage resolves", person2021s.Name(), personHub, true},
+		{"Doubly linked api resolves", person2021.Name(), personHub, true},
+		{"Indirectly linked api resolves", person2020.Name(), personHub, true},
+		{"Indirectly linked storage resolves", person2020s.Name(), personHub, true},
+		{"Hub type returns nothing even when resource does not exist in latest package", addressHub, astmodel.TypeName{}, false},
+		{"Directly linked api resolves when resource does not exist in latest package", address2021.Name(), addressHub, true},
+		{"Indirectly linked api resolves when resource does not exist in latest package", address2020.Name(), addressHub, true},
 	}
 
 	t.Parallel()
 	for _, c := range cases {
 		c := c
 		t.Run(c.name, func(t *testing.T) {
+			g := NewGomegaWithT(t)
 			t.Parallel()
 
-			startType := astmodel.MakeTypeName(c.start, "Person")
-			expectedType := astmodel.MakeTypeName(c.expected, "Person")
-
-			actual := graph.FindHubTypeName(startType)
-			g.Expect(actual).To(Equal(expectedType))
+			actual, ok := graph.FindHub(c.start, types)
+			g.Expect(ok).To(Equal(c.expectedOk))
+			if ok {
+				g.Expect(actual).To(Equal(c.expectedName))
+			}
 		})
 	}
 }
