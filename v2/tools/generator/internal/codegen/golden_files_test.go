f50f2ee587f073caa065209b356ac52a39aac547
Bevan Arps <bevan.arps@microsoft.com>
2021-10-16 08:41:54 +1300

    Introduce InjectPropertyAssignmentFunctionsStage

--- v2/tools/generator/internal/codegen/golden_files_test.go
+++ v2/tools/generator/internal/codegen/golden_files_test.go
@@ -149,7 +149,6 @@ func NewTestCodeGenerator(testName string, path string, t *testing.T, testConfig
 			pipeline.DeleteGeneratedCodeStageID,
 			pipeline.CheckForAnyTypeStageID,
 			pipeline.CreateStorageTypesStageID,
-			pipeline.InjectPropertyAssignmentFunctionsStageID,
 			// TODO: Once the stage is enabled in the pipeline, we may need to remove it here for testing
 			// pipeline.InjectHubFunctionStageID,
 			// pipeline.ImplementConvertibleInterfaceStageId,
