3804cb67bb1004ec4194e68218cb1915d7df4069
Matthew Christopher <matthchr@microsoft.com>
2022-01-12 13:05:43 -0800

    Add FindSpecConnectedTypes and FindStatusConnectedTypes (#2030)

--- v2/tools/generator/internal/astmodel/types_test.go
+++ v2/tools/generator/internal/astmodel/types_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	pkg                = MakeExternalPackageReference("foo")
+	pkg                = makeTestLocalPackageReference("foo", "2020-01-01")
 	alphaDefinition    = createTestDefinition("alpha", StringType)
 	betaDefinition     = createTestDefinition("beta", StringType)
 	gammaDefinition    = createTestDefinition("gamma", StringType)
@@ -259,6 +259,64 @@ func TestFindStatusTypes(t *testing.T) {
 }
 
 /*
+ * FindSpecConnectedTypes() tests
+ */
+
+func TestFindSpecConnectedTypes(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	// Define a test resource
+	nameInfo := NewTestObject("NameInfo", fullNameProperty, knownAsProperty)
+	nameInfoProperty := NewPropertyDefinition("NameInfo", "nameInfo", nameInfo.Name())
+	spec := createTestSpec("PersonAugmented", nameInfoProperty)
+
+	nameInfoStatus := NewTestObject("NameInfoStatus", fullNameProperty, knownAsProperty)
+	nameInfoStatusProperty := NewPropertyDefinition("NameInfo", "nameInfo", nameInfoStatus.Name())
+	status := createTestStatus("PersonAugmented", nameInfoStatusProperty)
+
+	resource := createTestResource("PersonAugmented", spec, status)
+
+	types := make(Types)
+	types.AddAll(resource, status, spec, nameInfo, nameInfoStatus)
+
+	specs, err := FindSpecConnectedTypes(types)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	g.Expect(specs).To(HaveLen(2))
+	g.Expect(specs.Contains(spec.Name())).To(BeTrue())
+	g.Expect(specs.Contains(nameInfo.Name())).To(BeTrue())
+}
+
+/*
+ * FindStatusConnectedTypes() tests
+ */
+
+func TestFindStatusConnectedTypes(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	// Define a test resource
+	nameInfo := NewTestObject("NameInfo", fullNameProperty, knownAsProperty)
+	nameInfoProperty := NewPropertyDefinition("NameInfo", "nameInfo", nameInfo.Name())
+	spec := createTestSpec("PersonAugmented", nameInfoProperty)
+
+	nameInfoStatus := NewTestObject("NameInfoStatus", fullNameProperty, knownAsProperty)
+	nameInfoStatusProperty := NewPropertyDefinition("NameInfo", "nameInfo", nameInfoStatus.Name())
+	status := createTestStatus("PersonAugmented", nameInfoStatusProperty)
+
+	resource := createTestResource("PersonAugmented", spec, status)
+
+	types := make(Types)
+	types.AddAll(resource, status, spec, nameInfo, nameInfoStatus)
+
+	statuses, err := FindStatusConnectedTypes(types)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	g.Expect(statuses).To(HaveLen(2))
+	g.Expect(statuses.Contains(status.Name())).To(BeTrue())
+	g.Expect(statuses.Contains(nameInfoStatus.Name())).To(BeTrue())
+}
+
+/*
  * ResolveResourceSpecAndStatus() tests
  */
 
@@ -326,10 +384,11 @@ func createTestSpec(
 }
 
 // createTestStatus makes a status for testing
-func createTestStatus(name string) TypeDefinition {
-	statusProperty := NewPropertyDefinition("Status", "status", StringType)
+func createTestStatus(
+	name string,
+	properties ...*PropertyDefinition) TypeDefinition {
 	statusName := MakeTypeName(pkg, name+"_Status")
 	return MakeTypeDefinition(
 		statusName,
-		NewObjectType().WithProperties(statusProperty))
+		NewObjectType().WithProperties(properties...))
 }
