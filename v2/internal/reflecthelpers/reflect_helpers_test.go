3bd7dc0bfc51cfef3c4da858054f29836709c4d5
Matthew Christopher <matthchr@microsoft.com>
2022-01-13 10:24:40 -0800

    Refactor reflect_helpers a bit to support other types (#2032)

--- v2/internal/reflecthelpers/reflect_helpers_test.go
+++ v2/internal/reflecthelpers/reflect_helpers_test.go
@@ -6,40 +6,59 @@
 package reflecthelpers_test
 
 import (
-	"context"
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	//nolint:staticcheck // ignoring deprecation (SA1019) to unblock CI builds
 	"github.com/Azure/azure-service-operator/v2/internal/reflecthelpers"
-	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
-
-	// TODO: Do we want to use a sample object rather than a code generated one?
-	batch "github.com/Azure/azure-service-operator/v2/api/batch/v1alpha1api20210101"
 
 	. "github.com/onsi/gomega"
 )
 
+type ResourceWithReferences struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+	Spec              ResourceWithReferencesSpec `json:"spec,omitempty"`
+}
+
+type ResourceWithReferencesSpec struct {
+	Owner genruntime.KnownResourceReference `json:"owner"`
+
+	AzureName string `json:"azureName"`
+
+	Ref *ResourceReference `json:"ref,omitempty"`
+
+	Location string `json:"location,omitempty"`
+}
+
+type ResourceReference struct {
+	Reference genruntime.ResourceReference `armReference:"Id" json:"reference"`
+}
+
 func Test_FindReferences(t *testing.T) {
 	g := NewGomegaWithT(t)
-	ctx := context.Background()
 
-	scheme, err := testcommon.CreateScheme()
-	g.Expect(err).ToNot(HaveOccurred())
-
-	testClient := testcommon.CreateClient(scheme)
-
-	rg := testcommon.CreateResourceGroup()
-	g.Expect(testClient.Create(ctx, rg)).To(Succeed())
-
-	account := testcommon.CreateDummyResource()
 	ref := genruntime.ResourceReference{ARMID: "test"}
-	account.Spec.KeyVaultReference = &batch.KeyVaultReference{
-		Reference: ref,
-	}
-	g.Expect(testClient.Create(ctx, account)).To(Succeed())
 
-	refs, err := reflecthelpers.FindResourceReferences(account)
+	res := ResourceWithReferences{
+		Spec: ResourceWithReferencesSpec{
+			AzureName: "azureName",
+			Location:  "westus",
+			Owner: genruntime.KnownResourceReference{
+				Name: "myrg",
+			},
+			Ref: &ResourceReference{
+				Reference: ref,
+			},
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-group",
+			Namespace: "test-namespace",
+		},
+	}
+
+	refs, err := reflecthelpers.FindResourceReferences(res)
 	g.Expect(err).ToNot(HaveOccurred())
 	g.Expect(refs).To(HaveLen(1))
 	g.Expect(refs).To(HaveKey(ref))
