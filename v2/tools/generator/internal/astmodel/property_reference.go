d8f3b34a3dd8e1f9e437ac4e44b2eb15551b8e09
Bevan Arps <bevan.arps@microsoft.com>
2022-01-18 14:30:30 +1300

    Define value type PropertyReference

---
+++ v2/tools/generator/internal/astmodel/property_reference.go
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package astmodel
+
+import (
+	"fmt"
+)
+
+// PropertyReference is a fully qualified reference to where a property may be found
+type PropertyReference struct {
+	declaringType TypeName     // which type declares the property
+	property      PropertyName // the name of the property
+}
+
+// EmptyPropertyReference is a convenience constant for when we have no value
+var EmptyPropertyReference = PropertyReference{}
+
+// MakePropertyReference creates a new property reference identifying both the containing type and the property name.
+func MakePropertyReference(declaringType TypeName, property PropertyName) PropertyReference {
+	return PropertyReference{
+		declaringType: declaringType,
+		property:      property,
+	}
+}
+
+// DeclaringType returns the type name of the type declaring the property
+func (ref PropertyReference) DeclaringType() TypeName {
+	return ref.declaringType
+}
+
+// Property returns the actual name of the property
+func (ref PropertyReference) Property() PropertyName {
+	return ref.property
+}
+
+// IsEmpty returns true if the reference is empty, false otherwise.
+func (ref PropertyReference) IsEmpty() bool {
+	return ref == EmptyPropertyReference
+}
+
+// String returns this property reference
+func (ref PropertyReference) String() string {
+	g, v, _ := ref.declaringType.PackageReference.GroupVersion()
+	return fmt.Sprintf("%s/%s/%s.%s", g, v, ref.declaringType.Name(), ref.property)
+}
---
+++ v2/tools/generator/internal/astmodel/property_reference_test.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package astmodel
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestPropertyReference_IsEmpty_ReturnsExpectedResult(t *testing.T) {
+	pkg := makeTestLocalPackageReference("Demo", "v1")
+	declaringType := MakeTypeName(pkg, "Person")
+
+	cases := []struct {
+		name          string
+		declaringType TypeName
+		property      PropertyName
+		expected      bool
+	}{
+		{"Property name present means not empty", EmptyTypeName, "property", false},
+		{"Declaring type present means not empty", declaringType, "", false},
+		{"Fully populated means not empty", declaringType, "property", false},
+		{"Totally empty not empty", EmptyTypeName, "", true},
+	}
+
+	t.Parallel()
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewGomegaWithT(t)
+			ref := MakePropertyReference(c.declaringType, c.property)
+			g.Expect(ref.IsEmpty()).To(Equal(c.expected))
+		})
+	}
+}
+
+func TestPropertyReference_String_ReturnsExpectedResult(t *testing.T) {
+	g := NewGomegaWithT(t)
+	pkg := makeTestLocalPackageReference("Demo", "v1")
+	declaringType := MakeTypeName(pkg, "Person")
+	property := PropertyName("FullName")
+
+	ref := MakePropertyReference(declaringType, property)
+	str := ref.String()
+
+	g.Expect(str).To(Equal("Demo/v1/Person.FullName"))
+}
