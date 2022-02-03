abd9a67c7dcb600c38b3d612874e5c5c48e6d57b
Bevan Arps <bevan.arps@microsoft.com>
2022-02-04 07:14:26 +1300

    Tidy up configuration code (#2063)
    Co-authored-by: Christian Muirhead <christian.muirhead@microsoft.com>

--- v2/tools/generator/internal/config/group_configuration.go
+++ v2/tools/generator/internal/config/group_configuration.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
 
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astmodel"
 )
@@ -37,93 +38,9 @@ func NewGroupConfiguration(name string) *GroupConfiguration {
 	}
 }
 
-// TypeRename looks up a rename for the specified type, returning the new name and true if found, or empty string
-// and false if not.
-func (gc *GroupConfiguration) TypeRename(name astmodel.TypeName) (string, error) {
-	version, err := gc.findVersion(name)
-	if err != nil {
-		return "", err
-	}
-
-	rename, err := version.TypeRename(name.Name())
-	if err != nil {
-		return "", errors.Wrapf(
-			err,
-			"configuration of group %s",
-			gc.name)
-	}
-
-	return rename, nil
-}
-
-// FindUnusedTypeRenames returns a slice listing any unused type rename configuration
-func (gc *GroupConfiguration) FindUnusedTypeRenames() []string {
-	return gc.collectErrors((*VersionConfiguration).FindUnusedTypeRenames)
-}
-
-// ARMReference looks up a property to determine whether it may be an ARM reference or not.
-func (gc *GroupConfiguration) ARMReference(name astmodel.TypeName, property astmodel.PropertyName) (bool, error) {
-	version, err := gc.findVersion(name)
-	if err != nil {
-		return false, err
-	}
-
-	armReference, err := version.ARMReference(name.Name(), property)
-	if err != nil {
-		return false, errors.Wrapf(
-			err,
-			"configuration of group %s",
-			gc.name)
-	}
-
-	return armReference, nil
-}
-
-// FindUnusedARMReferences returns a slice listing any unused ARMReference configuration
-func (gc *GroupConfiguration) FindUnusedARMReferences() []string {
-	return gc.collectErrors((*VersionConfiguration).FindUnusedARMReferences)
-}
-
-// collectErrors iterates over all our versions, collecting any errors provided by the source func, and annotating
-// each one with the source group.
-func (gc *GroupConfiguration) collectErrors(source func(v *VersionConfiguration) []string) []string {
-	var result []string
-
-	// All our versions are listed twice, under two different keys, so we hedge against processing them multiple times
-	versionsSeen := astmodel.MakeStringSet()
-	for _, vc := range gc.versions {
-		if versionsSeen.Contains(vc.name) {
-			continue
-		}
-
-		versionsSeen.Add(vc.name)
-		result = appendWithPrefix(result, fmt.Sprintf("group %s ", gc.name), source(vc)...)
-	}
-
-	return result
-}
-
-// IsSecret looks up a property to determine whether it is a secret or not
-func (gc *GroupConfiguration) IsSecret(name astmodel.TypeName, property astmodel.PropertyName) (bool, error) {
-	version, err := gc.findVersion(name)
-	if err != nil {
-		return false, err
-	}
-
-	isSecret, err := version.IsSecret(name.Name(), property)
-	if err != nil {
-		return false, errors.Wrapf(
-			err,
-			"configuration of group %s",
-			gc.name)
-	}
-
-	return isSecret, nil
-}
-
 // Add includes configuration for the specified version as a part of this group configuration
 // In addition to indexing by the name of the version, we also index by the local-package-name and storage-package-name
-// of the version so we can do lookups via TypeName. All indexing is lower-case to allow case-insensitive lookups (this
+// of the version, so we can do lookups via TypeName. All indexing is lower-case to allow case-insensitive lookups (this
 // makes our configuration more forgiving).
 func (gc *GroupConfiguration) Add(version *VersionConfiguration) *GroupConfiguration {
 	pkg := astmodel.CreateLocalPackageNameFromVersion(version.name)
@@ -132,6 +49,42 @@ func (gc *GroupConfiguration) Add(version *VersionConfiguration) *GroupConfigura
 	gc.versions[strings.ToLower(pkg)] = version
 	gc.versions[strings.ToLower(str)] = version
 	return gc
+}
+
+// visitVersion invokes the provided visitor on the specified version if present.
+// Returns a NotConfiguredError if the version is not found; otherwise whatever error is returned by the visitor.
+func (gc *GroupConfiguration) visitVersion(
+	name astmodel.TypeName,
+	visitor *configurationVisitor) error {
+
+	vc, err := gc.findVersion(name)
+	if err != nil {
+		return err
+	}
+
+	return visitor.visitVersion(vc)
+}
+
+// visitVersions invokes the provided visitor on all versions.
+func (gc *GroupConfiguration) visitVersions(visitor *configurationVisitor) error {
+	var errs []error
+
+	// All our versions are listed under multiple keys, so we hedge against processing them multiple times
+	versionsSeen := astmodel.MakeStringSet()
+	for _, v := range gc.versions {
+		if versionsSeen.Contains(v.name) {
+			continue
+		}
+
+		errs = append(errs, visitor.visitVersion(v))
+		versionsSeen.Add(v.name)
+	}
+
+	// Both errors.Wrapf() and kerrors.NewAggregate() return nil if nothing went wrong
+	return errors.Wrapf(
+		kerrors.NewAggregate(errs),
+		"group %s",
+		gc.name)
 }
 
 // findVersion uses the provided TypeName to work out which nested VersionConfiguration should be used
