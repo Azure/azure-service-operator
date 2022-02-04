09ae7f35b8d434fe87b978c29f3d9c862b9a0758
Bevan Arps <bevan.arps@microsoft.com>
2022-02-04 13:59:35 +1300

    Remove use of export filters

--- v2/tools/generator/internal/codegen/pipeline/apply_export_filters.go
+++ v2/tools/generator/internal/codegen/pipeline/apply_export_filters.go
@@ -7,9 +7,7 @@ package pipeline
 
 import (
 	"context"
-	"fmt"
 
-	"github.com/pkg/errors"
 	"k8s.io/klog/v2"
 
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astmodel"
@@ -32,38 +30,6 @@ func ApplyExportFilters(configuration *config.Configuration) Stage {
 func filterTypes(
 	configuration *config.Configuration,
 	state *State) (*State, error) {
-
-	// Original Approach
-
-	newDefinitions := make(astmodel.Types)
-	filterer := configuration.BuildExportFilterer(state.types)
-
-	oldRenames := make(map[astmodel.TypeName]astmodel.TypeName)
-	for _, def := range state.types {
-		defName := def.Name()
-		shouldExport, newName, reason := filterer(defName)
-
-		switch shouldExport {
-		case config.Skip:
-			klog.V(3).Infof("Skipping %s because %s", defName, reason)
-
-		case config.Export:
-			if reason == "" {
-				klog.V(3).Infof("Exporting %s", defName)
-			} else {
-				klog.V(2).Infof("Exporting %s because %s", defName, reason)
-			}
-
-			newDefinitions[def.Name()] = def
-			if newName != nil {
-				oldRenames[defName] = *newName
-			}
-		default:
-			panic(fmt.Sprintf("unhandled shouldExport case %q", shouldExport))
-		}
-	}
-
-	// New Approach
 
 	defs := make(astmodel.Types)
 	types := state.Types()
@@ -99,39 +65,6 @@ func filterTypes(
 		}
 	}
 
-	// Verify consistency
-	missing := astmodel.FindResourceTypes(newDefinitions.Except(defs))
-	if len(missing) > 0 {
-		// Some expected types missing
-		for name := range missing {
-			klog.Errorf("Type %s is missing", name)
-		}
-
-		return nil, errors.Errorf("Found %d missing types", len(missing))
-	}
-
-	extras := astmodel.FindResourceTypes(defs.Except(newDefinitions))
-	if len(extras) > 0 {
-		// Some unexpected types present
-		for name := range extras {
-			klog.Errorf("Type %s is unexpected", name)
-		}
-
-		return nil, errors.Errorf("Found %d unexpected types", len(extras))
-	}
-
-	count := 0
-	for o, n := range oldRenames {
-		if _, ok := renames[o]; !ok {
-			klog.Errorf("Missing rename of %s to %s", o, n)
-			count++
-		}
-	}
-
-	if count > 0 {
-		return nil, errors.Errorf("Found %d unsupported renames", count)
-	}
-
 	if err := configuration.ObjectModelConfiguration.VerifyExportConsumed(); err != nil {
 		return nil, err
 	}
@@ -142,13 +75,7 @@ func filterTypes(
 
 	// Now apply all the renames
 	renamingVisitor := astmodel.NewRenamingVisitor(renames)
-	result, err := renamingVisitor.RenameAll(newDefinitions)
-	if err != nil {
-		return nil, err
-	}
-
-	// Ensure that the export filters had no issues
-	err = configuration.GetExportFiltersError()
+	result, err := renamingVisitor.RenameAll(defs)
 	if err != nil {
 		return nil, err
 	}
