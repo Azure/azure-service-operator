760ca9732d396eacf686e1656f061ee3944fe9f2
Harshdeep Singh <38904804+super-harsh@users.noreply.github.com>
2022-02-08 09:04:13 +1300

    Update v2/pkg/genruntime/resource_extension.go to edit the comment
    Co-authored-by: Matthew Christopher <matthchr@microsoft.com>

--- v2/pkg/genruntime/resource_extension.go
+++ v2/pkg/genruntime/resource_extension.go
@@ -9,7 +9,7 @@ package genruntime
 This file contains manual implementations to reduce code bloat in generated code.
 */
 
-// ResourceExtension is defines extended functionality of a resource used by the reconciler
+// ResourceExtension defines extended functionality of a resource used by the reconciler
 type ResourceExtension interface {
 	// GetExtendedResources Returns the KubernetesResource slice for Resource versions
 	GetExtendedResources() []KubernetesResource
