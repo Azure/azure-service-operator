19eb9dbff68ea4058f1a77232665299e9924241c
Harshdeep Singh <harshdsingh@microsoft.com>
2022-02-22 23:45:49 +1300

    Fix directory in gen-helm-manifest.sh

--- scripts/generate-helm-manifest.sh
+++ scripts/generate-helm-manifest.sh
@@ -18,9 +18,9 @@ mkdir "$DIR"charts/azure-service-operator/templates/generated
 mkdir "$DIR"charts/azure-service-operator/crds # create dirs for generated files
 kustomize build "$DIR"config/default -o "$DIR"charts/azure-service-operator/templates/generated
 find "$DIR"charts/azure-service-operator/templates/generated/*_customresourcedefinition_* -exec mv '{}' "$DIR"charts/azure-service-operator/crds \; # move CRD definitions to crd folder
-rm charts/azure-service-operator/templates/generated/*_namespace_* # remove namespace as we will let Helm manage it
+rm "$DIR"charts/azure-service-operator/templates/generated/*_namespace_* # remove namespace as we will let Helm manage it
 sed -i '' "s@$LOCAL_REGISTRY_CONTROLLER_DOCKER_IMAGE@{{.Values.image.repository}}@g" "$DIR"charts/azure-service-operator/templates/generated/*_deployment_* # Replace hardcoded ASO image
-grep -E {{.KUBE_RBAC_PROXY}} "$DIR"charts/azure-service-operator/templates/generated/*_deployment_* # Ensure that what we're about to try to replace actually exists (if it doesn't we want to fail)
+grep -E $KUBE_RBAC_PROXY "$DIR"charts/azure-service-operator/templates/generated/*_deployment_* # Ensure that what we're about to try to replace actually exists (if it doesn't we want to fail)
 sed -i '' "s@$KUBE_RBAC_PROXY.*@{{.Values.image.kubeRBACProxy}}@g" "$DIR"charts/azure-service-operator/templates/generated/*_deployment_*
 sed -i '' "s@cert-manager.io/.*@{{.Values.certManagerResourcesAPIVersion}}@g" "$DIR"charts/azure-service-operator/templates/generated/*cert-manager.io*
 find "$DIR"charts/azure-service-operator/templates/generated/ -type f -exec sed -i '' "s@azureserviceoperator-system@{{.Release.Namespace}}@g" {} \;
