1a528216ab9b679f951eba06523ec16dbf28ff6b
Harshdeep Singh <harshdsingh@microsoft.com>
2022-02-28 10:04:31 +1300

    Add crossplane resource type check in doc.go file creation

--- v2/tools/generator/internal/astmodel/package_definition.go
+++ v2/tools/generator/internal/astmodel/package_definition.go
@@ -68,8 +68,8 @@ func (p *PackageDefinition) EmitDefinitions(outputDir string, generatedPackages 
 			return 0, err
 		}
 
-		// If package contains resources and is not Storage package, then we generate doc file for the package
-		if !strings.HasSuffix(p.PackageName, StoragePackageSuffix) {
+		// If package contains ARM resources is not Storage package, then we generate doc file for the package
+		if !strings.HasSuffix(p.PackageName, StoragePackageSuffix) && !strings.Contains(outputDir, "crossplane") {
 			if err = emitDocFile(p, outputDir); err != nil {
 				return 0, err
 			}
