721906d0078b8138a6c5cb664bcb4de5b950410c
Bevan Arps <bevan.arps@microsoft.com>
2022-03-04 14:36:03 +1300

    Update stage_test.go

--- v2/tools/generator/internal/codegen/pipeline/stage_test.go
+++ v2/tools/generator/internal/codegen/pipeline/stage_test.go
@@ -7,7 +7,9 @@ package pipeline
 
 import (
 	"context"
+	"testing"
 
+	. "github.com/onsi/gomega"
 	"github.com/pkg/errors"
 )
 
@@ -25,4 +27,73 @@ func RunTestPipeline(state *State, stages ...*Stage) (*State, error) {
 	}
 
 	return resultState, nil
+}
+
+/*
+ * checkPreconditions Tests
+ */
+
+func TestStagePreconditions_GivenNoPrerequisites_ReturnsNoError(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testStage := NewFakeStage("test")
+	state := NewState()
+	g.Expect(testStage.checkPreconditions(state)).To(BeNil())
+}
+
+const (
+	firstStageId = "firstStage"
+	testStageId  = "testStage"
+	lastStageId  = "lastStage"
+)
+
+func TestStagePreconditions_GivenSatisfiedPrerequisites_ReturnsNoError(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testStage := NewFakeStage(testStageId)
+	testStage.RequiresPrerequisiteStages(firstStageId)
+
+	state := NewState().WithSeenStage(firstStageId)
+	g.Expect(testStage.checkPreconditions(state)).To(BeNil())
+}
+
+func TestStagePreconditions_GivenUnsatisfiedPrerequisites_ReturnsError(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testStage := NewFakeStage(testStageId)
+	testStage.RequiresPrerequisiteStages(firstStageId)
+
+	state := NewState()
+	err := testStage.checkPreconditions(state)
+
+	g.Expect(err).NotTo(BeNil())
+	g.Expect(err.Error()).To(ContainSubstring(testStageId))
+	g.Expect(err.Error()).To(ContainSubstring(firstStageId))
+}
+
+func TestStagePreconditions_WhenSatisfiedTooEarly_ReturnsError(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testStage := NewFakeStage(testStageId)
+	testStage.RequiresPostrequisiteStages(lastStageId)
+
+	state := NewState().WithSeenStage(lastStageId)
+	err := testStage.checkPreconditions(state)
+
+	g.Expect(err).NotTo(BeNil())
+	g.Expect(err.Error()).To(ContainSubstring(testStageId))
+	g.Expect(err.Error()).To(ContainSubstring(lastStageId))
+}
+
+func NewFakeStage(id string) *Stage {
+	return NewStage(
+		id,
+		"Stage "+id,
+		func(ctx context.Context, state *State) (*State, error) {
+			return state, nil
+		})
 }
