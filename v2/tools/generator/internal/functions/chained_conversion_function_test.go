d8d1c20be429fe81e7aa471136d0b5cc915fbc27
Bevan Arps <bevan.arps@microsoft.com>
2022-04-01 11:01:25 +1300

    Fix code generation for object conversion with shared sub-objects (#2201)
    Co-authored-by: Matthew Christopher <matthchr@microsoft.com>

--- v2/tools/generator/internal/functions/chained_conversion_function_test.go
+++ v2/tools/generator/internal/functions/chained_conversion_function_test.go
@@ -44,7 +44,7 @@ func TestGolden_NewSpecChainedConversionFunction_Conversion_GeneratesExpectedCod
 	defs.AddAll(personSpec2020)
 	defs.AddAll(personSpec2021)
 
-	conversionContext := conversions.NewPropertyConversionContext(defs, idFactory, nil /* ObjectModelConfiguration*/)
+	conversionContext := conversions.NewPropertyConversionContext(defs, idFactory)
 	propertyAssignTo, err := NewPropertyAssignmentFunction(
 		personSpec2020, personSpec2021, conversionContext, conversions.ConvertTo)
 	g.Expect(err).To(Succeed())
@@ -89,7 +89,7 @@ func TestGolden_NewStatusChainedConversionFunction_Conversion_GeneratesExpectedC
 	defs.AddAll(personStatus2020)
 	defs.AddAll(personStatus2021)
 
-	conversionContext := conversions.NewPropertyConversionContext(defs, idFactory, nil /* ObjectModelConfiguration*/)
+	conversionContext := conversions.NewPropertyConversionContext(defs, idFactory)
 	propertyAssignTo, err := NewPropertyAssignmentFunction(
 		personStatus2020, personStatus2021, conversionContext, conversions.ConvertTo)
 	g.Expect(err).To(Succeed())
