3688f8d446f1add6b37caf4b96471ac9d396205c
Christian Muirhead <christian.muirhead@microsoft.com>
2022-04-04 12:41:39 +1200

    Generate multitenant deployment yaml files automatically (#2188)
    The multitenant deployment files can be generated by running the `controller:make-multitenant-files` task. They'll be generated and attached to new releases.
    
    Also adds an integration test to validate that we can create resources in a multitenant-deployed cluster that is run in the PR validation CI.
    
    * Add controller:kind-create-multitenant-cluster task
    
    This applies the generated multitenant files to a new kind cluster and
    creates a secret for the tenant operator, so that it's ready for testing.
    
    * Add test for confirming that multitenant mode works
    
    This is a very simple integration test that creates a resource
    group (confirming that defaulting webhooks are working in this
    configuration) and a vnet (confirming that conversion webhooks are
    working as well).

---
+++ scripts/wait-for-ca-bundles.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+function all_crds_have_cabundle() {
+    for crd in $(kubectl api-resources -o name | grep '\.azure\.com'); do
+        cabundle=$(kubectl get crd "$crd" -o jsonpath='{.spec.conversion.webhook.clientConfig.caBundle}')
+        if [ -z "$cabundle" ]; then
+            echo "$crd has no CA bundle"
+            return 1
+        fi
+    done
+    return 0
+}
+
+# Wait for all CRDs to have CA bundles set, or 30s.
+SECONDS=0
+
+until all_crds_have_cabundle; do
+    if (( SECONDS > 60 )); then
+        echo "Timed out waiting for all CRDs to have CA bundles"
+        exit 1
+    fi
+
+    echo "Waiting 5s"
+    sleep 5
+done
+
+echo "All CRDs have CA bundles"
+exit 0
