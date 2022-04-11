e652f93aabd694b16076e6900548c2a737ce6177
Bevan Arps <bevan.arps@microsoft.com>
2022-04-12 11:47:20 +1200

    Use destination variable for envtest binaries

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -82,8 +82,8 @@ arch=$(go env GOARCH)
 K8S_VERSION=1.23.3
 echo "Installing envtest binaries (kubectl, etcd, kube-apiserver) for ${K8S_VERSION} ($os $arch)…"
 curl -sSLo envtest-bins.tar.gz "https://go.kubebuilder.io/test-tools/${K8S_VERSION}/${os}/${arch}"
-mkdir -p "/usr/local/kubebuilder"
-tar -C /usr/local/kubebuilder --strip-components=1 -zvxf envtest-bins.tar.gz
+mkdir -p "$KUBEBUILDER_DEST"
+tar -C "$KUBEBUILDER_DEST" --strip-components=1 -zvxf envtest-bins.tar.gz
 rm envtest-bins.tar.gz
 
 # Install helm
