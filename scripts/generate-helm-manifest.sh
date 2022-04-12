f2e99159e06eaf97a98799859ee482befee90d26
Harshdeep Singh <harshdsingh@microsoft.com>
2022-04-13 10:44:40 +1200

    Move crds from template to  folder to avoid deletion; Add comments

--- scripts/generate-helm-manifest.sh
+++ scripts/generate-helm-manifest.sh
@@ -14,8 +14,8 @@ DIR=$5
 echo "Generating helm chart manifest"
 sed -i "s@\($PUBLIC_REGISTRY\)\(.*\)@\1azureserviceoperator:$VERSION@g" "$DIR"charts/azure-service-operator/values.yaml
 rm -rf "$DIR"charts/azure-service-operator/templates/generated # remove generated files
-rm -rf "$DIR"charts/azure-service-operator/charts/azure-service-operator-resources/templates
-mkdir "$DIR"charts/azure-service-operator/charts/azure-service-operator-resources/templates # create dirs for generated files
+rm -rf "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/crds
+mkdir "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/crds # create dirs for generated files
 mkdir "$DIR"charts/azure-service-operator/templates/generated
 kustomize build "$DIR"config/default -o "$DIR"charts/azure-service-operator/templates/generated
 rm "$DIR"charts/azure-service-operator/templates/generated/*_namespace_* # remove namespace as we will let Helm manage it
@@ -28,15 +28,16 @@ sed -i '/metrics-addr/i \  \ {{if .Values.metrics.enable}}' "$DIR"charts/azure-s
 sed -i "1,/metrics-addr=.*/s/\(metrics-addr=\)\(.*\)/\1{{.Values.metrics.address | default \"127.0.0.1:8080\" }}/g" "$DIR"charts/azure-service-operator/templates/generated/*_deployment_*
 sed -i '/metrics-addr/a \  \ {{ end }}' "$DIR"charts/azure-service-operator/templates/generated/*_deployment_* # End metrics flow control
 sed -i "1,/version:.*/s/\(version: \)\(.*\)/\1$VERSION/g" "$DIR"charts/azure-service-operator/Chart.yaml  # find version key and update the value with the current version for main chart
-find "$DIR"charts/azure-service-operator/templates/generated/ -type f -exec sed -i "s@azureserviceoperator-system@{{.Release.Namespace}}@g" {} \;
 
-#Azure-Service-Operator-Resources actions
-find "$DIR"charts/azure-service-operator/templates/generated/*_customresourcedefinition_* -exec mv '{}' "$DIR"charts/azure-service-operator/charts/azure-service-operator-resources/templates \; # move CRD definitions to resources chart folder
-sed -i "1,/version:.*/s/\(version: \)\(.*\)/\1$VERSION/g" "$DIR"charts/azure-service-operator/charts/azure-service-operator-resources/Chart.yaml  # find version key and update the value with the current version for resources chart
+# Azure-Service-Operator-crds actions
+# We had to split charts here here as with a single chart, we were running into the max size issue with helm
+# See https://github.com/helm/helm/issues/9788
+find "$DIR"charts/azure-service-operator/templates/generated/*_customresourcedefinition_* -exec mv '{}' "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/crds \; # move CRD definitions to crds chart folder
+sed -i "1,/version:.*/s/\(version: \)\(.*\)/\1$VERSION/g" "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/Chart.yaml  # find version key and update the value with the current version for crds chart
 
 # Helm chart packaging, indexing and updating dependencies
 echo "Packaging helm charts"
-helm package "$DIR"charts/azure-service-operator/charts/azure-service-operator-resources -d "$DIR"charts # package the CRD helm files into a tar file
-helm template "$DIR"charts/azure-service-operator --dependency-update > /dev/null #Update the resources dependency
+helm package "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds -d "$DIR"charts # package the CRD helm files into a tar file
+helm template "$DIR"charts/azure-service-operator --dependency-update > /dev/null # Update the crds dependency
 helm package "$DIR"charts/azure-service-operator -d "$DIR"charts # package the ASOv2 helm files into a tar file
 helm repo index "$DIR"charts # update index.yaml for Helm Repository
