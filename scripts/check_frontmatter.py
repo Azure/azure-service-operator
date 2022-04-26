1f6d2cf251065bad319e78d5b910180346bb711a
Bevan Arps <bevan.arps@microsoft.com>
2022-04-26 17:01:35 +1200

    Add a task to check for FrontMatter

---
+++ scripts/check_frontmatter.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import itertools
+import os
+import re
+import sys
+
+
+number_of_lines = 8
+
+
+# Turn colors in this script off by setting the NO_COLOR variable in your
+# environment to any value:
+#
+# $ NO_COLOR=1 python3 check_headers.py
+if os.getenv('NO_COLOR') is None:
+    header = '\033[1;33m'
+    reset = '\033[0m'
+else:
+    header = ''
+    reset = ''
+
+
+def print_colorful(text):
+    print(f'{header}{text}{reset}')
+
+
+# Returns true if the file passes validation, false if it fails
+def is_file_compliant(regex, path):
+    if not path.endswith('.md'):
+        return True
+
+    with open(path, 'r') as f:
+        combined = ''.join(itertools.islice(f, number_of_lines))
+
+        if not regex.search(combined):
+            return False
+    
+    return True
+
+
+if __name__ == '__main__':
+    frontMatterRegex = r'(?s)^---\s*(.*)\s*---$'
+    regex = re.compile(frontMatterRegex, re.MULTILINE | re.IGNORECASE)
+    failed_files = []
+
+    print_colorful('==> Checking Markdown frontmatter <==')
+
+    for root, subdirs, files in os.walk('.'):
+        for filename in files:
+            path = os.path.join(root, filename)
+            if not is_file_compliant(regex, path):
+                failed_files.append(path)
+
+    if failed_files:
+        print('The following files are missing the required frontmatter:')
+        for name in failed_files:
+            print(f'\t{name}')
+
+    print_colorful('Done checking Markdown frontmatter')
+
+    if failed_files:
+        sys.exit(1)
