db7630d2b4d5941893e796d83598e3e18b68ac31
Harshdeep Singh <harshdsingh@microsoft.com>
2022-05-03 22:24:19 +0530

    Add support for setting up validation webhook for immutable probs

--- v2/tools/generator/internal/functions/kubernetes_admissions_validations.go
+++ v2/tools/generator/internal/functions/kubernetes_admissions_validations.go
@@ -7,11 +7,11 @@ package functions
 
 import (
 	"go/token"
-
-	"github.com/dave/dst"
+	"strings"
 
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astbuilder"
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astmodel"
+	"github.com/dave/dst"
 )
 
 func NewValidateResourceReferencesFunction(resource *astmodel.ResourceType, idFactory astmodel.IdentifierFactory) *ResourceFunction {
@@ -21,6 +21,15 @@ func NewValidateResourceReferencesFunction(resource *astmodel.ResourceType, idFa
 		idFactory,
 		validateResourceReferences,
 		astmodel.NewPackageReferenceSet(astmodel.GenRuntimeReference, astmodel.ReflectHelpersReference))
+}
+
+func NewValidateImmutablePropertiesFunction(resource *astmodel.ResourceType, idFactory astmodel.IdentifierFactory) *ResourceFunction {
+	return NewResourceFunction(
+		"validateImmutableProperties",
+		resource,
+		idFactory,
+		validateImmutablePropertiesFunction,
+		astmodel.NewPackageReferenceSet())
 }
 
 func validateResourceReferences(k *ResourceFunction, codeGenerationContext *astmodel.CodeGenerationContext, receiver astmodel.TypeName, methodName string) *dst.FuncDecl {
@@ -76,4 +85,75 @@ func validateResourceReferencesBody(codeGenerationContext *astmodel.CodeGenerati
 				dst.NewIdent("refs"))))
 
 	return body
+}
+
+func validateImmutablePropertiesFunction(resourceFn *ResourceFunction, codeGenerationContext *astmodel.CodeGenerationContext, receiver astmodel.TypeName, methodName string) *dst.FuncDecl {
+
+	receiverIdent := resourceFn.IdFactory().CreateReceiver(receiver.Name())
+	receiverType := receiver.AsType(codeGenerationContext)
+
+	runtimePackage := codeGenerationContext.MustGetImportedPackageName(astmodel.APIMachineryRuntimeReference)
+
+	fn := &astbuilder.FuncDetails{
+		Name:          methodName,
+		ReceiverIdent: receiverIdent,
+		ReceiverType: &dst.StarExpr{
+			X: receiverType,
+		},
+		Returns: []*dst.Field{
+			{
+				Type: dst.NewIdent("error"),
+			},
+		},
+		Body: validateImmutablePropertiesFunctionBody(receiver, codeGenerationContext, receiverIdent),
+	}
+
+	fn.AddParameter("old", astbuilder.QualifiedTypeName(runtimePackage, "Object"))
+	fn.AddComments("validates all immutable properties")
+
+	return fn.DefineFunc()
+}
+
+func validateImmutablePropertiesFunctionBody(receiver astmodel.TypeName, codeGenerationContext *astmodel.CodeGenerationContext, receiverIdent string) []dst.Stmt {
+
+	obj := dst.NewIdent("oldObj")
+
+	var validations []dst.Stmt
+
+	cast := astbuilder.TypeAssert(obj, dst.NewIdent("old"), astbuilder.Dereference(receiver.AsType(codeGenerationContext)))
+	checkAssert := astbuilder.ReturnIfNotOk(astbuilder.Nil())
+
+	azureNameRef := []string{"AzureName()"}
+	azureNameCheck := checkImmutableProperty(obj, receiverIdent, azureNameRef, codeGenerationContext)
+
+	resourceOwnerRef := []string{"Owner()", "Name"}
+	resourceOwnerCheck := checkImmutableProperty(obj, receiverIdent, resourceOwnerRef, codeGenerationContext)
+
+	validations = append(validations, azureNameCheck, resourceOwnerCheck)
+
+	return astbuilder.Statements(cast, checkAssert, validations, astbuilder.ReturnNoError())
+
+}
+
+func checkImmutableProperty(obj *dst.Ident, receiverIdent string, ref []string, codeGenerationContext *astmodel.CodeGenerationContext) *dst.IfStmt {
+	errorsPkg := codeGenerationContext.MustGetImportedPackageName(astmodel.GitHubErrorsReference)
+
+	oldIndent := astbuilder.Selector(obj, ref...)
+	newIndent := astbuilder.Selector(dst.NewIdent(receiverIdent), ref...)
+
+	errorMessage := astbuilder.StringLiteralf(
+		"update for '%s' is not allowed", getRefString(ref))
+
+	azureNameCheck := astbuilder.ReturnIfExpr(
+		astbuilder.AreNotEqual(oldIndent, newIndent),
+		astbuilder.CallQualifiedFunc(errorsPkg, "New", errorMessage))
+	azureNameCheck.Decorations().Before = dst.EmptyLine
+
+	return azureNameCheck
+}
+
+func getRefString(ref []string) string {
+
+	return strings.Join(ref, ".")
+
 }
