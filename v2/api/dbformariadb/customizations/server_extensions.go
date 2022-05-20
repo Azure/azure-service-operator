2b9e4feb8b16965490dff0c751c6829067115cf8
Bevan Arps <bevan.arps@microsoft.com>
2022-05-20 15:02:35 +1200

    Retrieve secrets

---
+++ v2/api/dbformariadb/customizations/server_extensions.go
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package customizations
+
+import (
+	"context"
+
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/go-logr/logr"
+	"github.com/pkg/errors"
+	v1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/conversion"
+
+	mariadb "github.com/Azure/azure-service-operator/v2/api/dbformariadb/v1beta20180601storage"
+	"github.com/Azure/azure-service-operator/v2/internal/genericarmclient"
+	. "github.com/Azure/azure-service-operator/v2/internal/logging"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/extensions"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/secrets"
+)
+
+var _ extensions.SecretsRetriever = &ServerExtension{}
+
+// RetrieveSecrets implements extensions.SecretsRetriever
+func (ext *ServerExtension) RetrieveSecrets(
+	ctx context.Context,
+	obj genruntime.ARMMetaObject,
+	armClient *genericarmclient.GenericClient,
+	log logr.Logger) ([]*v1.Secret, error) {
+
+	// This has to be the current storage version. It will need to be updated
+	// if the storage version changes.
+	typedObj, ok := obj.(*mariadb.Server)
+	if !ok {
+		return nil, errors.Errorf("cannot run on unknown resource type %T", obj)
+	}
+
+	// Type assert that we are the hub type. This should fail to compile if
+	// the hub type has been changed but this extension has not
+	var _ conversion.Hub = typedObj
+
+	hasSecrets := secretsSpecified(typedObj)
+	if !hasSecrets {
+		log.V(Debug).Info("No secrets retrieval to perform as operatorSpec is empty")
+		return nil, nil
+	}
+
+	secretSlice, err := secretsToWrite(typedObj)
+	if err != nil {
+		return nil, err
+	}
+
+	return secretSlice, nil
+}
+
+func secretsSpecified(obj *mariadb.Server) bool {
+	if obj.Spec.OperatorSpec == nil || obj.Spec.OperatorSpec.Secrets == nil {
+		return false
+	}
+
+	secrets := obj.Spec.OperatorSpec.Secrets
+	if secrets.FullyQualifiedDomainName != nil {
+		return true
+	}
+
+	return false
+}
+
+func secretsToWrite(obj *mariadb.Server) ([]*v1.Secret, error) {
+	operatorSpecSecrets := obj.Spec.OperatorSpec.Secrets
+	if operatorSpecSecrets == nil {
+		return nil, errors.Errorf("unexpected nil OperatorSpec")
+	}
+
+	collector := secrets.NewSecretCollector(obj.Namespace)
+	collector.AddSecretValue(operatorSpecSecrets.FullyQualifiedDomainName, to.String(obj.Status.FullyQualifiedDomainName))
+
+	return collector.Secrets(), nil
+}
