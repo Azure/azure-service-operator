efdff5ced0cf828040fc0f30ff34b20ec35df0ff
Bevan Arps <bevan.arps@microsoft.com>
2022-05-23 11:34:46 +1200

    Allow generating standalone error from TypoAdvisor

--- v2/tools/generator/internal/config/typo_advisor.go
+++ v2/tools/generator/internal/config/typo_advisor.go
@@ -35,18 +35,48 @@ func (advisor *TypoAdvisor) AddTerm(item string) {
 	advisor.terms.Add(item)
 }
 
+// HasTerms returns true if this advisor has any terms available
+func (advisor *TypoAdvisor) HasTerms() bool {
+	return len(advisor.terms) > 0
+}
+
+// Errorf creates a new error with advice, or a simple error if no advice possible
+func (advisor *TypoAdvisor) Errorf(typo string, format string, args ...interface{}) error {
+	advisor.lock.RLock()
+	defer advisor.lock.RUnlock()
+
+	if !advisor.HasTerms() || advisor.terms.Contains(typo) {
+		// Can't make any suggestions,
+		return errors.Errorf(format, args...)
+	}
+
+	msg := fmt.Sprintf(format, args...)
+
+	suggestion, err := edlib.FuzzySearch(
+		strings.ToLower(typo),
+		set.AsSortedSlice(advisor.terms),
+		edlib.Levenshtein)
+	if err != nil {
+		// Can't offer a suggestion
+		return errors.Errorf(
+			"%s (unable to provide suggestion: %s)",
+			msg,
+			err)
+	}
+
+	return errors.Errorf(
+		"%s (did you mean %s?)",
+		msg,
+		suggestion)
+}
+
 // Wrapf adds any guidance to the provided error, if possible
 func (advisor *TypoAdvisor) Wrapf(originalError error, typo string, format string, args ...interface{}) error {
 	advisor.lock.RLock()
 	defer advisor.lock.RUnlock()
 
-	if len(advisor.terms) == 0 {
-		// Can't make any suggestions
-		return originalError
-	}
-
-	if advisor.terms.Contains(typo) {
-		// No suggestion needed
+	if !advisor.HasTerms() || advisor.terms.Contains(typo) || originalError == nil {
+		// Can't make any suggestions, or don't need one
 		return originalError
 	}
 
