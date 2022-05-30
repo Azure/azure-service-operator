5d9275c22791d0057ea4455b86e154761f8c167c
Bevan Arps <bevan.arps@microsoft.com>
2022-05-31 09:26:02 +1200

    Add test to verify MultiMatcher does not short circuit

--- v2/tools/generator/internal/config/multi_matcher_test.go
+++ v2/tools/generator/internal/config/multi_matcher_test.go
@@ -67,3 +67,41 @@ func TestMultiMatcher_GivenDefinition_MatchesExpectedStrings(t *testing.T) {
 		})
 	}
 }
+
+func TestMultiMatcher_DoesNotShortCircuit(t *testing.T) {
+	t.Parallel()
+
+	/*
+	 * For nested matchers to correctly offer suggestions, the MultiMatcher may not short circuit evaluations.
+	 * Even once a match is found, later matchers must still be consulted so that they can accumulate candidates
+	 * to use for diagnostic generation.
+	 *
+	 * To ensure no short circuiting occurs, we create some multimatchers where every branch will match a given
+	 * probe value, and check that all branches were matched.
+	 */
+
+	cases := []struct {
+		name       string
+		definition string
+		probe      string
+	}{
+		{"Similar literals", "foo;Foo", "foo"},
+		{"Similar globs", "T*;*T", "that"},
+		{"Mixed types", "B*;Ba?;baz", "baz"},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+			g := NewGomegaWithT(t)
+
+			m := newMultiMatcher(c.definition)
+			g.Expect(m.Matches(c.probe)).To(BeTrue())
+			for _, nested := range m.matchers {
+				g.Expect(nested.WasMatched()).To(BeNil())
+			}
+		})
+	}
+
+}
