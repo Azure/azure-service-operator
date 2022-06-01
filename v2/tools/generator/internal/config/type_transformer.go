3cf7d39f1648dc35a52e0d8be79d0492d70dbee7
Bevan Arps <bevan.arps@microsoft.com>
2022-06-01 16:16:47 +1200

    Switch to using FieldMatcher

--- v2/tools/generator/internal/config/type_transformer.go
+++ v2/tools/generator/internal/config/type_transformer.go
@@ -199,7 +199,7 @@ func (target *TransformTarget) produceTargetType(
 				target.Name.String())
 		} else {
 			var err error
-			result, err = target.asPrimitiveType(target.Name)
+			result, err = target.asPrimitiveType(target.Name.String())
 			if err != nil {
 				return nil, err
 			}
