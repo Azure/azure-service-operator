932b28d494e3f5ed20df7f16cb67e807b8cc7811
Jianguo Ma <jiangma@microsoft.com>
2022-06-04 12:32:48 +0000

    add rest test and recording
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

---
+++ v2/internal/controllers/crd_containerinstance_containergroup_test.go
@@ -0,0 +1,78 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package controllers_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	containerinstance "github.com/Azure/azure-service-operator/v2/api/containerinstance/v1beta20211001"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
+)
+
+func Test_ContainerInstance_ContainerGroup_CRUD(t *testing.T) {
+	t.Parallel()
+
+	tc := globalTestContext.ForTest(t)
+
+	rg := tc.CreateTestResourceGroupAndWait()
+
+	name := tc.NoSpaceNamer.GenerateName("containergroup")
+	image := "mcr.microsoft.com/azuredocs/aci-helloworld"
+	port := int(80)
+	protocol := containerinstance.ContainerPortProtocolTCP
+	cpu := float64(1)
+	memoryInGB := float64(2)
+	osType := containerinstance.ContainerGroupsSpecPropertiesOsTypeLinux
+	restartPolicy := containerinstance.ContainerGroupsSpecPropertiesRestartPolicyAlways
+	ipAddressType := containerinstance.IpAddressTypePublic
+	portProtocol := containerinstance.PortProtocolTCP
+
+	// Create a ContainerGroup
+	cg := containerinstance.ContainerGroup{
+		ObjectMeta: tc.MakeObjectMetaWithName(name),
+		Spec: containerinstance.ContainerGroups_Spec{
+			Location: tc.AzureRegion,
+			Owner:    testcommon.AsOwner(rg),
+			Containers: []containerinstance.ContainerGroups_Spec_Properties_Containers{
+				{
+					Name:  &name,
+					Image: &image,
+					Ports: []containerinstance.ContainerPort{
+						{
+							Port:     &port,
+							Protocol: &protocol,
+						},
+					},
+					Resources: &containerinstance.ResourceRequirements{
+						Requests: &containerinstance.ResourceRequests{
+							Cpu:        &cpu,
+							MemoryInGB: &memoryInGB,
+						},
+					},
+				},
+			},
+			OsType:        &osType,
+			RestartPolicy: &restartPolicy,
+			IpAddress: &containerinstance.IpAddress{
+				Type: &ipAddressType,
+				Ports: []containerinstance.Port{
+					{
+						Port:     &port,
+						Protocol: &portProtocol,
+					},
+				},
+			},
+		},
+	}
+
+	tc.CreateResourcesAndWait(&cg)
+	defer tc.DeleteResourcesAndWait(&cg)
+
+	// Perform some assertions on the resources we just created
+	tc.Expect(cg.Status.Id).ToNot(BeNil())
+}
