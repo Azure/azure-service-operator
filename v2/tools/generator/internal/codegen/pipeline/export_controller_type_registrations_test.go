f0f4f7f9f1f7b83d73f6688e286fc1218a262f1d
Bevan Arps <bevan.arps@microsoft.com>
2022-06-09 11:18:21 +1200

    Create ensureIndexMethodNamesUnique() stub and add base tests

--- v2/tools/generator/internal/codegen/pipeline/export_controller_type_registrations_test.go
+++ v2/tools/generator/internal/codegen/pipeline/export_controller_type_registrations_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astmodel"
 )
 
-func TestEnsureIndexPropertyPathsUnique_ResolvesConflicts(t *testing.T) {
+func TestEnsureIndexMethodNamesUniqueResolvesConflicts(t *testing.T) {
 	t.Parallel()
 
 	type tc struct {
@@ -43,41 +43,6 @@ func TestEnsureIndexPropertyPathsUnique_ResolvesConflicts(t *testing.T) {
 				{"User.Created", "Created"},
 			},
 		},
-		{
-			"Nested properties that conflict get prefixes",
-			[]tc{
-				{"Administrator.UserName", "AdministratorUserName"},
-				{"Administrator.Password", "AdministratorPassword"},
-				{"Service.UserName", "ServiceUserName"},
-				{"Service.Password", "ServicePassword"},
-			},
-		},
-		{
-			"Parent properties that don't disambiguate are skipped",
-			[]tc{
-				{"Administrator.Properties.UserName", "AdministratorUserName"},
-				{"Administrator.Properties.Password", "AdministratorPassword"},
-				{"Service.Properties.UserName", "ServiceUserName"},
-				{"Service.Properties.Password", "ServicePassword"},
-			},
-		},
-		{
-			"Other properties that share parents are prefixed for consistency",
-			[]tc{
-				{"Administrator.UserName", "AdministratorUserName"},
-				{"Administrator.Password", "AdministratorPassword"},
-				{"Administrator.Expiry", "AdministratorExpiry"},
-				{"Service.UserName", "ServiceUserName"},
-				{"Service.Password", "ServicePassword"},
-			},
-		},
-		{
-			"Original conflict needs to be resolved",
-			[]tc{
-				{"obj.Spec.Properties.HDInsight.Properties.AdministratorAccount.Password", "HDInsightPassword"},
-				{"obj.Spec.Properties.VirtualMachine.Properties.AdministratorAccount.Password", "VirtualMachinePassword"},
-			},
-		},
 	}
 
 	for _, c := range cases {
@@ -85,13 +50,12 @@ func TestEnsureIndexPropertyPathsUnique_ResolvesConflicts(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			t.Parallel()
 			g := NewGomegaWithT(t)
-			factory := make(testChainFactory)
 			var chains []*propertyChain
 			for _, tc := range c.chains {
-				chains = append(chains, factory.createTestChain(tc.chain))
+				chains = append(chains, createTestChain(tc.chain))
 			}
 
-			ensureIndexPropertyPathsUnique(chains)
+			ensureIndexMethodNamesUnique(chains)
 
 			for i, chain := range chains {
 				g.Expect(chain.indexPropertyPath()).To(Equal(c.chains[i].expected))
@@ -100,29 +64,13 @@ func TestEnsureIndexPropertyPathsUnique_ResolvesConflicts(t *testing.T) {
 	}
 }
 
-// testChainFactory is a map of property chains to test chains, used to ensure the chains with a common prefix are
-// using references to the same root property chain
-type testChainFactory map[string]*propertyChain
-
-func (factory testChainFactory) createTestChain(chain string) *propertyChain {
-	// Return cached chain if present
-	if c, ok := factory[chain]; ok {
-		return c
-	}
-
+func createTestChain(chain string) *propertyChain {
+	props := strings.Split(chain, ".")
 	var result *propertyChain
-	if splitPoint := strings.LastIndex(chain, "."); splitPoint != -1 {
-		// We have a multipart chain, so create the parent chain first
-		parent := factory.createTestChain(chain[:splitPoint])
-		name := chain[splitPoint+1:]
-		prop := astmodel.NewPropertyDefinition(astmodel.PropertyName(name), strings.ToLower(name), astmodel.StringType)
-		result = parent.add(prop)
-	} else {
-		// Top level property
-		prop := astmodel.NewPropertyDefinition(astmodel.PropertyName(chain), strings.ToLower(chain), astmodel.StringType)
-		result = newPropertyChain().add(prop)
+	for _, prop := range props {
+		result = result.add(
+			astmodel.NewPropertyDefinition(astmodel.PropertyName(prop), strings.ToLower(prop), astmodel.StringType))
 	}
 
-	factory[chain] = result
 	return result
 }
