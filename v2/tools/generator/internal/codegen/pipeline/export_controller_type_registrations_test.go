bca1f6196f4e263ad0a5edae1dcc2bd0d1ebc4c4
Bevan Arps <bevan.arps@microsoft.com>
2022-06-09 12:46:20 +1200

    Add more tests

--- v2/tools/generator/internal/codegen/pipeline/export_controller_type_registrations_test.go
+++ v2/tools/generator/internal/codegen/pipeline/export_controller_type_registrations_test.go
@@ -14,7 +14,7 @@ import (
 	"github.com/Azure/azure-service-operator/v2/tools/generator/internal/astmodel"
 )
 
-func TestEnsureIndexMethodNamesUniqueResolvesConflicts(t *testing.T) {
+func TestEnsureIndexIndexPropertyPathsUnique_ResolvesConflicts(t *testing.T) {
 	t.Parallel()
 
 	type tc struct {
@@ -43,6 +43,41 @@ func TestEnsureIndexMethodNamesUniqueResolvesConflicts(t *testing.T) {
 				{"User.Created", "Created"},
 			},
 		},
+		{
+			"Nested properties that conflict get prefixes",
+			[]tc{
+				{"Administrator.UserName", "AdministratorUserName"},
+				{"Administrator.Password", "AdministratorPassword"},
+				{"Service.UserName", "ServiceUserName"},
+				{"Service.Password", "ServicePassword"},
+			},
+		},
+		{
+			"Parent properties that don't disambiguate are skipped",
+			[]tc{
+				{"Administrator.Properties.UserName", "AdministratorUserName"},
+				{"Administrator.Properties.Password", "AdministratorPassword"},
+				{"Service.Properties.UserName", "ServiceUserName"},
+				{"Service.Properties.Password", "ServicePassword"},
+			},
+		},
+		{
+			"Other properties that share parents are prefixed for consistency",
+			[]tc{
+				{"Administrator.UserName", "AdministratorUserName"},
+				{"Administrator.Password", "AdministratorPassword"},
+				{"Administrator.Expiry", "AdministratorExpiry"},
+				{"Service.UserName", "ServiceUserName"},
+				{"Service.Password", "ServicePassword"},
+			},
+		},
+		{
+			"Original conflict needs to be resolved",
+			[]tc{
+				{"obj.Spec.Properties.HDInsight.Properties.AdministratorAccount.Password", "HDInsightPassword"},
+				{"obj.Spec.Properties.VirtualMachine.Properties.AdministratorAccount.Password", "VirtualMachinePassword"},
+			},
+		},
 	}
 
 	for _, c := range cases {
@@ -50,12 +85,13 @@ func TestEnsureIndexMethodNamesUniqueResolvesConflicts(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			t.Parallel()
 			g := NewGomegaWithT(t)
+			factory := make(testChainFactory)
 			var chains []*propertyChain
 			for _, tc := range c.chains {
-				chains = append(chains, createTestChain(tc.chain))
+				chains = append(chains, factory.createTestChain(tc.chain))
 			}
 
-			ensureIndexMethodNamesUnique(chains)
+			ensureIndexPropertyPathsUnique(chains)
 
 			for i, chain := range chains {
 				g.Expect(chain.indexPropertyPath()).To(Equal(c.chains[i].expected))
@@ -64,13 +100,29 @@ func TestEnsureIndexMethodNamesUniqueResolvesConflicts(t *testing.T) {
 	}
 }
 
-func createTestChain(chain string) *propertyChain {
-	props := strings.Split(chain, ".")
-	var result *propertyChain
-	for _, prop := range props {
-		result = result.add(
-			astmodel.NewPropertyDefinition(astmodel.PropertyName(prop), strings.ToLower(prop), astmodel.StringType))
+// testChainFactory is a map of property chains to test chains, used to ensure the chains with a common prefix are
+// using references to the same root property chain
+type testChainFactory map[string]*propertyChain
+
+func (factory testChainFactory) createTestChain(chain string) *propertyChain {
+	// Return cached chain if present
+	if c, ok := factory[chain]; ok {
+		return c
 	}
 
+	var result *propertyChain
+	if splitPoint := strings.LastIndex(chain, "."); splitPoint != -1 {
+		// We have a multipart chain, so create the parent chain first
+		parent := factory.createTestChain(chain[:splitPoint])
+		name := chain[splitPoint+1:]
+		prop := astmodel.NewPropertyDefinition(astmodel.PropertyName(name), strings.ToLower(name), astmodel.StringType)
+		result = parent.add(prop)
+	} else {
+		// Top level property
+		prop := astmodel.NewPropertyDefinition(astmodel.PropertyName(chain), strings.ToLower(chain), astmodel.StringType)
+		result = newPropertyChain().add(prop)
+	}
+
+	factory[chain] = result
 	return result
 }
