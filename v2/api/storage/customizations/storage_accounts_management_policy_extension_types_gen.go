e1944e4420c26171e899c4374ead47cd138e2bb3
Matthew Christopher <matthchr@microsoft.com>
2022-06-26 13:17:02 -0700

    Re-add storagemanagement alpha version. (#2359)

--- v2/api/storage/customizations/storage_accounts_management_policy_extension_types_gen.go
+++ v2/api/storage/customizations/storage_accounts_management_policy_extension_types_gen.go
@@ -4,6 +4,8 @@
 package customizations
 
 import (
+	alpha20210401 "github.com/Azure/azure-service-operator/v2/api/storage/v1alpha1api20210401"
+	alpha20210401s "github.com/Azure/azure-service-operator/v2/api/storage/v1alpha1api20210401storage"
 	v20210401 "github.com/Azure/azure-service-operator/v2/api/storage/v1beta20210401"
 	v20210401s "github.com/Azure/azure-service-operator/v2/api/storage/v1beta20210401storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
@@ -15,6 +17,8 @@ type StorageAccountsManagementPolicyExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *StorageAccountsManagementPolicyExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
+		&alpha20210401.StorageAccountsManagementPolicy{},
+		&alpha20210401s.StorageAccountsManagementPolicy{},
 		&v20210401.StorageAccountsManagementPolicy{},
 		&v20210401s.StorageAccountsManagementPolicy{}}
 }
