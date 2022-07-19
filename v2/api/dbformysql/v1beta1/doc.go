e7403ffdc2e52c924cd6ccd81ffe93a1a3c1ac4a
Bevan Arps <bevan.arps@microsoft.com>
2022-07-19 04:41:48 +0000

    Fix package name

--- v2/api/dbformysql/v1beta1/doc.go
+++ v2/api/dbformysql/v1beta1/doc.go
@@ -3,6 +3,6 @@ Copyright (c) Microsoft Corporation.
 Licensed under the MIT license.
 */
 
-// Package v1beta20210501 contains hand crafted API Schema definitions for the dbformysql v1beta API group
+// Package v1beta contains hand crafted API Schema definitions for the dbformysql v1beta API group
 // +groupName=dbformysql.azure.com
-package v1beta
+package v1beta1
