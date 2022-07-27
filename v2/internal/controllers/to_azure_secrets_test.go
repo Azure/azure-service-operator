9471ec302d54e38d01850d3f72868254be4fce53
Harshdeep Singh <harshdsingh@microsoft.com>
2022-07-27 22:41:05 +1200

    Add test

--- v2/internal/controllers/to_azure_secrets_test.go
+++ v2/internal/controllers/to_azure_secrets_test.go
@@ -8,8 +8,8 @@ package controllers_test
 import (
 	"testing"
 
+	"github.com/Azure/azure-service-operator/v2/internal/config"
 	. "github.com/onsi/gomega"
-	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -59,8 +59,6 @@ func Test_MissingSecretKey_ReturnsError(t *testing.T) {
 	vnet := newVMVirtualNetwork(tc, testcommon.AsOwner(rg))
 	subnet := newVMSubnet(tc, testcommon.AsOwner(vnet))
 	networkInterface := newVMNetworkInterface(tc, testcommon.AsOwner(rg), subnet)
-
-	tc.RunParallelSubtests()
 	// Inefficient but avoids triggering the vnet/subnets problem.
 	// https://github.com/Azure/azure-service-operator/issues/1944
 	tc.CreateResourceAndWait(vnet)
@@ -83,8 +81,11 @@ func Test_UserSecretInDifferentNamespace_SecretNotFound(t *testing.T) {
 	t.Parallel()
 
 	tc := globalTestContext.ForTest(t)
+	secretName := tc.Namer.GenerateName("admin-pw")
+	nameSpaceName := "secret-namespace"
 
-	secretInDiffNamespace := createNamespaceAndSecret(tc, "secret-namespace", tc.Namer.GenerateName("vmsecret"))
+	createNamespaces(tc, nameSpaceName)
+	secretInDiffNamespace := createNamespacedSecret(tc, nameSpaceName, secretName)
 
 	rg := tc.CreateTestResourceGroupAndWait()
 
@@ -108,20 +109,80 @@ func Test_UserSecretInDifferentNamespace_SecretNotFound(t *testing.T) {
 	tc.DeleteResourcesAndWait(vm, networkInterface, subnet, vnet, rg)
 }
 
-func createNamespaceAndSecret(tc *testcommon.KubePerTestContext, namespaceName string, secretName string) genruntime.SecretReference {
-	ns := &corev1.Namespace{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: namespaceName,
-		},
-	}
-	// Creating a namespace here using CreateResourceUntracked as we don't want to register clean up for namespace.
-	tc.CreateResourceUntracked(ns)
+func Test_abc(t *testing.T) {
+	tc := globalTestContext.ForTest(t)
+	rg := tc.NewTestResourceGroup()
+	tc.DeleteResourceAndWait(rg)
 
+}
+
+func Test_UserSecretInDifferentNamespace_ShouldNotTriggerReconcile(t *testing.T) {
+	t.Parallel()
+	ns1 := "ns-1"
+	ns2 := "ns-2"
+
+	cfg, err := config.ReadFromEnvironment()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cfg.TargetNamespaces = []string{ns1, ns2}
+	tc := globalTestContext.ForTestWithConfig(t, cfg)
+
+	createNamespaces(tc, ns1, ns2)
+
+	secretName := tc.Namer.GenerateName("server-secret")
+
+	secretInCurrentNamespace := createNamespacedSecret(tc, ns1, secretName)
+	secretInDiffNamespace := createNamespacedSecret(tc, ns2, secretName)
+
+	rg := tc.NewTestResourceGroup()
+	rg.Namespace = ns1
+	tc.CreateResourceGroupAndWait(rg)
+
+	rg2 := tc.NewTestResourceGroup()
+	rg2.Namespace = ns2
+	tc.CreateResourceGroupAndWait(rg2)
+
+	server1, _ := newFlexibleServer(tc, testcommon.AsOwner(rg), secretInCurrentNamespace)
+	server1.ObjectMeta = metav1.ObjectMeta{
+		Name:      tc.Namer.GenerateName("mysql"),
+		Namespace: ns1,
+	}
+	tc.CreateResourceAndWait(server1)
+
+	resourceID := genruntime.GetResourceIDOrDefault(server1)
+	tc.Expect(resourceID).ToNot(BeEmpty())
+
+	// Deleting server1 here using AzureClient so that Operator does not know about the deletion
+	_, err = tc.AzureClient.DeleteByID(tc.Ctx, resourceID, server1.GetAPIVersion())
+	if err != nil {
+		return
+	}
+
+	server2, _ := newFlexibleServer(tc, testcommon.AsOwner(rg2), secretInDiffNamespace)
+	server2.ObjectMeta = metav1.ObjectMeta{
+		Name:      tc.Namer.GenerateName("mysql2"),
+		Namespace: ns2,
+	}
+
+	tc.CreateResourceAndWait(server2)
+
+	// Here we want to make sure that the server1 we deleted in Azure should not be created again with reconcile of secret
+	// in other namespace
+	_, err = tc.AzureClient.GetByID(tc.Ctx, resourceID, server1.GetAPIVersion(), server1)
+	tc.Expect(err).ToNot(BeNil())
+
+	tc.DeleteResourcesAndWait(server1, server2)
+	tc.DeleteResourcesAndWait(rg, rg2)
+
+}
+
+func createNamespacedSecret(tc *testcommon.KubePerTestContext, namespaceName string, secretName string) genruntime.SecretReference {
 	passwordKey := "password"
 	secret := &v1.Secret{
 		ObjectMeta: ctrl.ObjectMeta{
 			Name:      secretName,
-			Namespace: ns.Name,
+			Namespace: namespaceName,
 		},
 		StringData: map[string]string{
 			passwordKey: tc.Namer.GeneratePasswordOfLength(40),
@@ -130,9 +191,8 @@ func createNamespaceAndSecret(tc *testcommon.KubePerTestContext, namespaceName s
 
 	tc.CreateResource(secret)
 
-	secretRef := genruntime.SecretReference{
+	return genruntime.SecretReference{
 		Name: secret.Name,
 		Key:  passwordKey,
 	}
-	return secretRef
 }
