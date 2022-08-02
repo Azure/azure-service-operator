c8b29288a663c5b396310155934326680c11c012
Bevan Arps <bevan.arps@microsoft.com>
2022-08-03 09:12:59 +1200

    Add test

---
+++ v2/internal/controllers/crd_networking_privatednszone_test.go
@@ -0,0 +1,39 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package controllers_test
+
+import (
+	"testing"
+
+	network "github.com/Azure/azure-service-operator/v2/api/network/v1beta20180901"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
+	"github.com/Azure/go-autorest/autorest/to"
+	. "github.com/onsi/gomega"
+)
+
+func Test_Networking_PrivateDnsZone_CRUD(t *testing.T) {
+	t.Parallel()
+
+	tc := globalTestContext.ForTest(t)
+
+	rg := tc.CreateTestResourceGroupAndWait()
+
+	name := tc.Namer.GenerateName("pdz") + ".com"
+	zone := &network.PrivateDnsZone{
+		ObjectMeta: tc.MakeObjectMetaWithName(name),
+		Spec: network.PrivateDnsZones_Spec{
+			Location: to.StringPtr("global"),
+			Owner:    testcommon.AsOwner(rg),
+		},
+	}
+
+	tc.CreateResourceAndWait(zone)
+
+	tc.Expect(zone.Status.MaxNumberOfRecordSets).ToNot(BeNil())
+	tc.Expect(zone.Status.MaxNumberOfVirtualNetworkLinks).ToNot(BeNil())
+
+	tc.DeleteResourceAndWait(zone)
+}
