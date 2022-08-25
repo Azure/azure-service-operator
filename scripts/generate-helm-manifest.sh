b3898a0c6c8eddb54e8522d5d6b2bdb45634419c
Harshdeep Singh <harshdsingh@microsoft.com>
2022-08-25 16:04:10 +1200

    Cleanup

--- scripts/generate-helm-manifest.sh
+++ scripts/generate-helm-manifest.sh
@@ -10,7 +10,8 @@ PUBLIC_REGISTRY=$3
 VERSION=$4
 DIR=$5
 
-GEN_FILES_DIR="$DIR"charts/azure-service-operator/templates/generated
+ASO_CHART="$DIR"charts/azure-service-operator
+GEN_FILES_DIR="$ASO_CHART"/templates/generated
 IF_CLUSTER="{{- if or (eq .Values.multiTenant.enable false) (eq .Values.multiTenant.isCluster true) }}"
 IF_TENANT="{{- if or (eq .Values.multiTenant.enable false) (eq .Values.multiTenant.isCluster false) }}"
 
@@ -21,17 +22,17 @@ flow_control(){
   IF_CLAUSE=$3
   TARGET=$4
 
-sed -i "/$MATCHER_IF/i \  \ $IF_CLAUSE" "$TARGET"
-sed -i "/$MATCHER_END/a \  \ {{- end }}" "$TARGET"
+  sed -i "/$MATCHER_IF/i \  \ $IF_CLAUSE" "$TARGET"
+  sed -i "/$MATCHER_END/a \  \ {{- end }}" "$TARGET"
 }
 
 
 # Manifest purge and generation
 echo "Generating helm chart manifest"
-sed -i "s@\($PUBLIC_REGISTRY\)\(.*\)@\1azureserviceoperator:$VERSION@g" "$DIR"charts/azure-service-operator/values.yaml
+sed -i "s@\($PUBLIC_REGISTRY\)\(.*\)@\1azureserviceoperator:$VERSION@g" "$ASO_CHART"/values.yaml
 rm -rf "$GEN_FILES_DIR" # remove generated files
-rm -rf "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/templates
-mkdir -p "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/templates/crds # create dirs for generated files
+rm -rf "$ASO_CHART"/charts/azure-service-operator-crds/templates
+mkdir -p "$ASO_CHART"/charts/azure-service-operator-crds/templates/crds # create dirs for generated files
 mkdir "$GEN_FILES_DIR"
 kustomize build "$DIR"config/default -o "$GEN_FILES_DIR"
 rm "$GEN_FILES_DIR"/*_namespace_* # remove namespace as we will let Helm manage it
@@ -41,7 +42,7 @@ grep -E $KUBE_RBAC_PROXY "$GEN_FILES_DIR"/*_deployment_* > /dev/null # Ensure th
 sed -i "s@$KUBE_RBAC_PROXY.*@{{.Values.image.kubeRBACProxy}}@g" "$GEN_FILES_DIR"/*_deployment_*
 sed -i "s@$LOCAL_REGISTRY_CONTROLLER_DOCKER_IMAGE@{{.Values.image.repository}}@g" "$GEN_FILES_DIR"/*_deployment_* # Replace hardcoded ASO image
 sed -i '/default-logs-container: manager/a \  \ {{- if .Values.podAnnotations }}\n \  \ {{ toYaml .Values.podAnnotations | indent 6 }}\n \  \ {{- end }}' "$GEN_FILES_DIR"/*_deployment_* # Add pod annotations
-sed -i "s/\(version: \)\(.*\)/\1$VERSION/g" "$DIR"charts/azure-service-operator/Chart.yaml  # find version key and update the value with the current version for both main and subchart
+sed -i "s/\(version: \)\(.*\)/\1$VERSION/g" "$ASO_CHART"/Chart.yaml  # find version key and update the value with the current version for both main and subchart
 
 # Metrics Configuration
 flow_control "metrics-addr" "metrics-addr" "{{- if .Values.metrics.enable}}" "$GEN_FILES_DIR"/*_deployment_*
@@ -50,13 +51,13 @@ sed -i 's/containerPort: 8080/containerPort: {{ .Values.metrics.port | default 8
 sed -i '1 i {{- if .Values.metrics.enable -}}' "$GEN_FILES_DIR"/*controller-manager-metrics-service*
 sed -i 's/port: 8080/port: {{ .Values.metrics.port | default 8080 }}/g' "$GEN_FILES_DIR"/*controller-manager-metrics-service*
 sed -i -e '$a{{- end }}' "$GEN_FILES_DIR"/*controller-manager-metrics-service*
-find "$DIR"charts/azure-service-operator/templates/generated/ -type f -exec sed -i 's/azureserviceoperator-system/{{.Release.Namespace}}/g' {} \;
+find "$GEN_FILES_DIR" -type f -exec sed -i 's/azureserviceoperator-system/{{ .Release.Namespace }}/g' {} \;
 
 # Azure-Service-Operator-crds actions
 # We had to split charts here here as with a single chart, we were running into the max size issue with helm
 # See https://github.com/helm/helm/issues/9788
-find "$GEN_FILES_DIR"/*_customresourcedefinition_* -exec mv '{}' "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/templates/crds \; # move CRD definitions to crds chart folder
-sed -i "1,/version:.*/s/\(version: \)\(.*\)/\1$VERSION/g" "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds/Chart.yaml  # find version key and update the value with the current version for crds chart
+find "$GEN_FILES_DIR"/*_customresourcedefinition_* -exec mv '{}' "$ASO_CHART"/charts/azure-service-operator-crds/templates/crds \; # move CRD definitions to crds chart folder
+sed -i "1,/version:.*/s/\(version: \)\(.*\)/\1$VERSION/g" "$ASO_CHART"/charts/azure-service-operator-crds/Chart.yaml  # find version key and update the value with the current version for crds chart
 
 # Perform file level changes for cluster and tenant
 for file in $(find "$GEN_FILES_DIR" -type f)
@@ -64,9 +65,8 @@ do
     if [[ $file == *"clusterrolebinding_azureserviceoperator-manager"* ]]; then
       sed -i "1 s/^/$IF_TENANT\n/;$ a {{- end }}" "$file"
       flow_control "name: azureserviceoperator-manager-rolebinding" "name: azureserviceoperator-manager-rolebinding" "{{- if not .Values.multiTenant.enable }}" "$file" # TODO: flow control on L60, 61 like 63 and 64
-      sed -i "/name: azureserviceoperator-manager-rolebinding/a \  \ {{ else }}\n \ name: azureserviceoperator-manager-rolebinding-{{ .Values.multiTenant.tenantName }}" "$file"
-    elif [[ $file != *"leader-election"* ]] && [[ $file != *"deployment"* ]]; then
-      echo $file
+      sed -i "/name: azureserviceoperator-manager-rolebinding/a \  \ {{ else }}\n \ name: azureserviceoperator-manager-rolebinding-{{ .Release.Namespace }}" "$file"
+    elif [[ $file != *"leader-election"* ]] && [[ $file != *"_deployment_"* ]]; then
       sed -i "1 s/^/$IF_CLUSTER\n/;$ a {{- end }}" "$file"
     fi
 done
@@ -80,7 +80,7 @@ flow_control "volumeMounts:" "secretName:" "$IF_CLUSTER" "$GEN_FILES_DIR"/*_depl
 
 # Helm chart packaging, indexing and updating dependencies
 echo "Packaging helm charts"
-helm package "$DIR"charts/azure-service-operator/charts/azure-service-operator-crds -d "$DIR"charts # package the CRD helm files into a tar file
-helm template "$DIR"charts/azure-service-operator --dependency-update > /dev/null # Update the crds dependency
-helm package "$DIR"charts/azure-service-operator -d "$DIR"charts # package the ASOv2 helm files into a tar file
+helm package "$ASO_CHART"/charts/azure-service-operator-crds -d "$DIR"charts # package the CRD helm files into a tar file
+helm template "$ASO_CHART" --dependency-update > /dev/null # Update the crds dependency
+helm package "$ASO_CHART" -d "$DIR"charts # package the ASOv2 helm files into a tar file
 helm repo index "$DIR"charts # update index.yaml for Helm Repository
