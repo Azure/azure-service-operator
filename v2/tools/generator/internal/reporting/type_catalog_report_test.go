278e0f9b7467dfc931a11986708375f57bccd918
Bevan Arps <bevan.arps@microsoft.com>
2022-10-03 15:04:54 +1300

    Add test to expose recursion bug

--- v2/tools/generator/internal/reporting/type_catalog_report_test.go
+++ v2/tools/generator/internal/reporting/type_catalog_report_test.go
@@ -48,6 +48,53 @@ func Test_TypeCatalogReport_GivenTypes_WhenInlined_ShowsExpectedDetails(t *testi
 	golden.Assert(t, t.Name(), content.Bytes())
 }
 
+func Test_TypeCateaLogReport_GivenDirectlyRecursiveType_WhenInlined_ShowsExpectedDetails(t *testing.T) {
+	t.Parallel()
+
+	golden := goldie.New(t)
+	g := gomega.NewWithT(t)
+
+	personName := astmodel.MakeTypeName(test.Pkg2020, "Person")
+
+	parentProperty := astmodel.NewPropertyDefinition(
+		"Parent",
+		"parentName",
+		astmodel.NewOptionalType(personName)).
+		WithDescription("Optional reference to our parent")
+
+	personObj := astmodel.NewObjectType().
+		WithProperties(
+			test.FullNameProperty,
+			test.FamilyNameProperty,
+			test.KnownAsProperty,
+			parentProperty)
+
+	person := astmodel.MakeTypeDefinition(personName, personObj)
+
+	relationship := test.CreateObjectDefinition(
+		test.Pkg2020,
+		"Relationship",
+		astmodel.NewPropertyDefinition(
+			"FirstParty",
+			"firstparty",
+			astmodel.NewOptionalType(personName)),
+		astmodel.NewPropertyDefinition(
+			"SecondParty",
+			"secondparty",
+			astmodel.NewOptionalType(personName)))
+
+	defs := make(astmodel.TypeDefinitionSet)
+	defs.Add(person)
+	defs.Add(relationship)
+
+	var content bytes.Buffer
+	rpt := NewTypeCatalogReport(defs)
+	rpt.InlineTypes()
+
+	g.Expect(rpt.WriteTo(&content)).To(gomega.Succeed())
+	golden.Assert(t, t.Name(), content.Bytes())
+}
+
 func createDefinitionSet() astmodel.TypeDefinitionSet {
 	testSpec := test.CreateSpec(
 		test.Pkg2020,
