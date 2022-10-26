3c60154a5f14edfa5e6f8562a6e3fa39ebc689aa
Harshdeep Singh <harshdsingh@microsoft.com>
2022-10-26 14:36:13 +1300

    Fix error line number

--- v2/internal/resolver/errors_test.go
+++ v2/internal/resolver/errors_test.go
@@ -128,8 +128,8 @@ func TestOwnerNotFound_RemembersCause(t *testing.T) {
 
 	// Note that both of the below lines are fragile with respect to line number and will
 	// need to be changed if the lines causing the error above are changed.
-	g.Expect(StackTraceOf(err)).To(ContainSubstring("errors_test.go:119"))
-	g.Expect(StackTraceOf(errors.Cause(err))).To(ContainSubstring("errors_test.go:118"))
+	g.Expect(StackTraceOf(err)).To(ContainSubstring("errors_test.go:121"))
+	g.Expect(StackTraceOf(errors.Cause(err))).To(ContainSubstring("errors_test.go:120"))
 }
 
 func StackTraceOf(e error) string {
