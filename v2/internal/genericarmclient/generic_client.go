a880363d06aa45f37e620ad9858046771100b8c5
Harshdeep Singh <harshdsingh@microsoft.com>
2023-01-23 16:26:32 +0530

    Make default http client generic

--- v2/internal/genericarmclient/generic_client.go
+++ v2/internal/genericarmclient/generic_client.go
@@ -7,8 +7,6 @@ package genericarmclient
 
 import (
 	"context"
-	"crypto/tls"
-	"net"
 	"net/http"
 	"strings"
 	"time"
@@ -87,29 +85,7 @@ func NewGenericClientFromHTTPClient(cloudCfg cloud.Configuration, creds azcore.T
 	if httpClient != nil {
 		opts.Transport = httpClient
 	} else {
-		// If httpClient is not provided, we use a HTTPClient with default Transport + settings
-		// to establish multiple TCP ARMClient connections to avoid throttling.
-		// TODO: Use https://github.com/Azure/go-armbalancer here once its prod ready.
-		httpTransport := &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   30 * time.Second, // default transport value
-				KeepAlive: 30 * time.Second, // default transport value
-			}).DialContext,
-			ForceAttemptHTTP2:     false,            // default is true; since HTTP/2 multiplexes a single TCP connection. we'd want to use HTTP/1, which would use multiple TCP connections.
-			MaxIdleConns:          100,              // default transport value
-			MaxIdleConnsPerHost:   10,               // default is 2, so we want to increase the number to use establish more connections.
-			IdleConnTimeout:       90 * time.Second, // default transport value
-			TLSHandshakeTimeout:   10 * time.Second, // default transport value
-			ExpectContinueTimeout: 1 * time.Second,  // default transport value
-			TLSClientConfig: &tls.Config{
-				MinVersion: tls.VersionTLS12, // default tls version
-			},
-		}
-
-		opts.Transport = &http.Client{
-			Transport: httpTransport,
-		}
+		opts.Transport = defaultHttpClient
 	}
 
 	opts.PerCallPolicies = append([]policy.Policy{rpRegistrationPolicy}, opts.PerCallPolicies...)
