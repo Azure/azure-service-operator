eee323297f5ab85e59101103444c8bc92ce2cbcc
Bevan Arps <bevan.arps@microsoft.com>
2023-02-04 05:18:17 +1300

    Add Prereconcile check to PostgreSQL Flexible Server (#2688)

--- v2/api/dbforpostgresql/customizations/flexible_server_extensions.go
+++ v2/api/dbforpostgresql/customizations/flexible_server_extensions.go
@@ -7,11 +7,17 @@ package customizations
 
 import (
 	"context"
+	"fmt"
+	"strings"
+
+	"github.com/Azure/azure-service-operator/v2/internal/set"
+	"github.com/Azure/azure-service-operator/v2/internal/util/kubeclient"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/extensions"
 
 	"github.com/Azure/go-autorest/autorest/to"
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
-	"k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 
@@ -25,9 +31,9 @@ import (
 var _ genruntime.KubernetesExporter = &FlexibleServerExtension{}
 
 func (ext *FlexibleServerExtension) ExportKubernetesResources(
-	ctx context.Context,
+	_ context.Context,
 	obj genruntime.MetaObject,
-	armClient *genericarmclient.GenericClient,
+	_ *genericarmclient.GenericClient,
 	log logr.Logger) ([]client.Object, error) {
 
 	// This has to be the current hub storage version. It will need to be updated
@@ -60,8 +66,8 @@ func secretsSpecified(obj *postgresql.FlexibleServer) bool {
 		return false
 	}
 
-	secrets := obj.Spec.OperatorSpec.Secrets
-	if secrets.FullyQualifiedDomainName != nil {
+	operatorSecrets := obj.Spec.OperatorSpec.Secrets
+	if operatorSecrets.FullyQualifiedDomainName != nil {
 		return true
 	}
 
@@ -78,4 +84,53 @@ func secretsToWrite(obj *postgresql.FlexibleServer) ([]*v1.Secret, error) {
 	collector.AddValue(operatorSpecSecrets.FullyQualifiedDomainName, to.String(obj.Status.FullyQualifiedDomainName))
 
 	return collector.Values()
+}
+
+var _ extensions.PreReconciliationChecker = &FlexibleServerExtension{}
+
+// If the provisioningState of a flexible server is not in this set, it will reject any attempt to PUT the resource
+// out of hand; so there's no point in even trying. This is true even if the PUT we're doing will have no effect on
+// the state of the server.
+// These are all listed lowercase, so we can do a case-insensitive match.
+var nonBlockingFlexibleServerStates = set.Make(
+	"succeeded",
+	"failed",
+	"canceled",
+	"ready",
+)
+
+func (ext *FlexibleServerExtension) PreReconcileCheck(
+	_ context.Context,
+	obj genruntime.MetaObject,
+	owner genruntime.MetaObject,
+	_ kubeclient.Client,
+	_ *genericarmclient.GenericClient,
+	_ logr.Logger,
+	_ extensions.PreReconcileCheckFunc,
+) (extensions.PreReconcileCheckResult, error) {
+	// This has to be the current hub storage version. It will need to be updated
+	// if the hub storage version changes.
+	server, ok := obj.(*postgresql.FlexibleServer)
+	if !ok {
+		return extensions.PreReconcileCheckResult{},
+			errors.Errorf("cannot run on unknown resource type %T, expected *postgresql.FlexibleServer", obj)
+	}
+
+	// Type assert that we are the hub type. This will fail to compile if
+	// the hub type has been changed but this extension has not been updated
+	var _ conversion.Hub = server
+
+	state := server.Status.State
+	if state != nil && flexibleServerStateBlocksReconciliation(*state) {
+		return extensions.BlockReconcile(
+			fmt.Sprintf(
+				"Flexible Server is in provisioning state %q",
+				*state)), nil
+	}
+
+	return extensions.ProceedWithReconcile(), nil
+}
+
+func flexibleServerStateBlocksReconciliation(state string) bool {
+	return !nonBlockingFlexibleServerStates.Contains(strings.ToLower(state))
 }
