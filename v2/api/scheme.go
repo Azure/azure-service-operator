7211a8dc7aca50ea932e72273f45ebc1912924b8
Harshdeep Singh <harshdsingh@microsoft.com>
2023-02-07 13:10:21 +0530

    Add helper method to export controllers.CreateScheme

---
+++ v2/api/scheme.go
@@ -0,0 +1,17 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package api
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/Azure/azure-service-operator/v2/internal/controllers"
+)
+
+// CreateScheme is a helper method to export 'internal/controllers/controller_resources.go/CreateScheme()' method outside internal package.
+func CreateScheme() *runtime.Scheme {
+	return controllers.CreateScheme()
+}
