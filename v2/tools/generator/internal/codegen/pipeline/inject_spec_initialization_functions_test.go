40704451a326d4843893abc52cb341d6e02d330b
Bevan Arps <bevan.arps@microsoft.com>
2023-02-28 10:58:01 +1300

    Add unit test

--- v2/tools/generator/internal/codegen/pipeline/inject_spec_initialization_functions_test.go
+++ v2/tools/generator/internal/codegen/pipeline/inject_spec_initialization_functions_test.go
@@ -21,24 +21,47 @@ func TestGolden_InjectSpecInitializationFunctions(t *testing.T) {
 
 	idFactory := astmodel.NewIdentifierFactory()
 
+	//TODO: Add new properties to the spec and status with identical names but typenames with different names
+	// Need to extend proeprty conversions to handle these cases
+
+	addressSpec := test.CreateObjectDefinition(
+		test.Pkg2020,
+		"Address"+astmodel.SpecSuffix,
+		test.FullAddressProperty,
+		test.CityProperty)
+
+	addressSpecProperty := astmodel.NewPropertyDefinition("Address", "address", addressSpec.Name())
+
+	// We want a status type that has a wholly different name from the spec type
+	locationStatus := test.CreateObjectDefinition(
+		test.Pkg2020,
+		"Location"+astmodel.StatusSuffix,
+		test.FullAddressProperty,
+		test.CityProperty,
+		test.StatusProperty)
+
+	addressStatusProperty := astmodel.NewPropertyDefinition("Address", "address", locationStatus.Name())
+
 	spec := test.CreateSpec(
 		test.Pkg2020,
 		"Person",
 		test.FullNameProperty,
 		test.FamilyNameProperty,
-		test.KnownAsProperty)
+		test.KnownAsProperty,
+		addressSpecProperty)
 
 	status := test.CreateStatus(
 		test.Pkg2020,
 		"Person",
 		test.FullNameProperty,
 		test.FamilyNameProperty,
-		test.KnownAsProperty)
+		test.KnownAsProperty,
+		addressStatusProperty)
 
 	resource := test.CreateResource(test.Pkg2020, "Person", spec, status)
 
 	defs := make(astmodel.TypeDefinitionSet)
-	defs.AddAll(resource, spec, status)
+	defs.AddAll(resource, spec, status, addressSpec, locationStatus)
 
 	state := NewState().WithDefinitions(defs)
 
