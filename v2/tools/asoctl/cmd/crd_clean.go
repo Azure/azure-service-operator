54823866083991198dc01e462787561ff8fb8b5c
Bevan Arps <bevan.arps@microsoft.com>
2023-03-15 12:15:31 +1300

    Move main.go to top level

--- v2/tools/asoctl/cmd/crd_clean.go
+++ v2/tools/asoctl/cmd/crd_clean.go
@@ -3,7 +3,7 @@
  * Licensed under the MIT license.
  */
 
-package main
+package cmd
 
 import (
 	"context"
--- v2/tools/asoctl/cmd/root.go
+++ v2/tools/asoctl/cmd/root.go
@@ -3,7 +3,7 @@
  * Licensed under the MIT license.
  */
 
-package main
+package cmd
 
 import (
 	"github.com/spf13/cobra"
