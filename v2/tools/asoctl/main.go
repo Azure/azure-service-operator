4c5be1ba196856e1a1a6cdb3c23f00d7577d2a93
Bevan Arps <bevan.arps@microsoft.com>
2023-03-20 14:03:24 +1300

    Improve error reporting

--- v2/tools/asoctl/main.go
+++ v2/tools/asoctl/main.go
@@ -8,9 +8,11 @@ package main
 import (
 	goflag "flag"
 	"os"
+	"runtime/debug"
 
 	flag "github.com/spf13/pflag"
 	"k8s.io/klog/v2"
+
 	"github.com/Azure/azure-service-operator/v2/tools/asoctl/cmd"
 )
 
@@ -19,5 +21,15 @@ func main() {
 	klog.InitFlags(flagSet)
 	flagSet.Parse(os.Args[1:]) //nolint:errcheck // error will never be returned due to ExitOnError
 	flag.CommandLine.AddGoFlagSet(flagSet)
+
+	defer func() {
+		// If a panic occurs, log details as debug
+		if r := recover(); r != nil {
+			klog.ErrorDepth(1, r)
+			klog.V(1).InfoDepth(1, string(debug.Stack()))
+			os.Exit(1)
+		}
+	}()
+
 	cmd.Execute()
 }
