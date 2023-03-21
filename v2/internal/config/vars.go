1eaf63a67860bf0eb1824cea7061253c4844b6d7
Harshdeep Singh <harshdsingh@microsoft.com>
2023-03-21 14:03:11 +1300

    Add cert auth test

--- v2/internal/config/vars.go
+++ v2/internal/config/vars.go
@@ -31,11 +31,6 @@ const (
 	azureAuthorityHostVar        = "AZURE_AUTHORITY_HOST"
 	podNamespaceVar              = "POD_NAMESPACE"
 	useWorkloadIdentityAuth      = "USE_WORKLOAD_IDENTITY_AUTH"
-
-	// TODO: These values are used for single operator multitenancy tests. We can get rid of them once we have Managed Identity support.
-	AzureClientIDMultitenantVar = "AZURE_CLIENT_ID_MULTITENANT"
-	// #nosec
-	AzureClientSecretMultitenantVar = "AZURE_CLIENT_SECRET_MULTITENANT"
 )
 
 // These are hardcoded because the init function that initializes them in azcore isn't in /cloud it's in /arm which
