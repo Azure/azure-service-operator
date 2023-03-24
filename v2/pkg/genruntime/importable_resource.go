43e66caae10d75d062aac00118bbb87eaddb181b
Bevan Arps <bevan.arps@microsoft.com>
2023-03-24 14:00:34 +1300

    Update comments

--- v2/pkg/genruntime/importable_resource.go
+++ v2/pkg/genruntime/importable_resource.go
@@ -5,13 +5,13 @@
 
 package genruntime
 
-// ImportableResource is implemented by any resource that can be imported into the operator via asoctl
+// ImportableResource is implemented by any resource that can be imported into the operator
 type ImportableResource interface {
 	// InitializeSpec initializes the Spec of the resource from the provided Status.
 	InitializeSpec(status ConvertibleStatus) error
 }
 
-// ImportableARMResource represents an ARM based resource that can be imported into the operator via asoctl
+// ImportableARMResource represents an ARM based resource that can be imported into the operator
 type ImportableARMResource interface {
 	ImportableResource
 	ARMMetaObject
