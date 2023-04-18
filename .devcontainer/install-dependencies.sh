fef81150cb2cb2362f715ab671487ad6b768f60d
Matthew Christopher <matthchr@users.noreply.github.com>
2023-04-18 10:37:13 -0700

    Update kind and other dependencies (#2897)

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -143,9 +143,9 @@ go-install() {
     fi
 }
 
-go-install conversion-gen k8s.io/code-generator/cmd/conversion-gen@v0.24.3
-go-install controller-gen sigs.k8s.io/controller-tools/cmd/controller-gen@v0.9.2
-go-install kind sigs.k8s.io/kind@v0.14.0
+go-install conversion-gen k8s.io/code-generator/cmd/conversion-gen@v0.26.4
+go-install controller-gen sigs.k8s.io/controller-tools/cmd/controller-gen@v0.11.3
+go-install kind sigs.k8s.io/kind@v0.18.0
 go-install kustomize sigs.k8s.io/kustomize/kustomize/v4@v4.5.7
 
 # for docs site
