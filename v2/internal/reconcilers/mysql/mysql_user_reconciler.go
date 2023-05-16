661eea12a7798635f45dbdcc2034326b00bfd32d
Matthew Christopher <matthchr@users.noreply.github.com>
2023-05-15 22:26:15 -0700

    Add support for MySQL FlexibleServer AAD Users (#2972)
    Includes support for creating AAD users.
    
    In order to manage AAD users, the identity the operator is running as
    (either the global identity or the identity specified in the users
    credential-from annotation) must be the AAD Administrator of the MySQL
    server. This requires that ASO is using a managed identity of some kind.
    We recommend workload identity.

--- v2/internal/reconcilers/mysql/mysql_user_reconciler.go
+++ v2/internal/reconcilers/mysql/mysql_user_reconciler.go
@@ -7,23 +7,20 @@ package mysql
 
 import (
 	"context"
-	"database/sql"
 
 	"github.com/go-logr/logr"
 	_ "github.com/go-sql-driver/mysql" //mysql driver
 	"github.com/pkg/errors"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
-	ctrlconversion "sigs.k8s.io/controller-runtime/pkg/conversion"
 
 	asomysql "github.com/Azure/azure-service-operator/v2/api/dbformysql/v1"
-	dbformysql "github.com/Azure/azure-service-operator/v2/api/dbformysql/v1api20210501storage"
 	"github.com/Azure/azure-service-operator/v2/internal/config"
+	"github.com/Azure/azure-service-operator/v2/internal/identity"
 	. "github.com/Azure/azure-service-operator/v2/internal/logging"
 	"github.com/Azure/azure-service-operator/v2/internal/reconcilers"
 	"github.com/Azure/azure-service-operator/v2/internal/resolver"
 	"github.com/Azure/azure-service-operator/v2/internal/util/kubeclient"
-	mysqlutil "github.com/Azure/azure-service-operator/v2/internal/util/mysql"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/conditions"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/core"
@@ -33,19 +30,22 @@ var _ genruntime.Reconciler = &MySQLUserReconciler{}
 
 type MySQLUserReconciler struct {
 	reconcilers.ARMOwnedResourceReconcilerCommon
-	ResourceResolver *resolver.Resolver
-	Config           config.Values
+	ResourceResolver   *resolver.Resolver
+	CredentialProvider identity.CredentialProvider
+	Config             config.Values
 }
 
 func NewMySQLUserReconciler(
 	kubeClient kubeclient.Client,
 	resourceResolver *resolver.Resolver,
 	positiveConditions *conditions.PositiveConditionBuilder,
+	credentialProvider identity.CredentialProvider,
 	cfg config.Values) *MySQLUserReconciler {
 
 	return &MySQLUserReconciler{
-		ResourceResolver: resourceResolver,
-		Config:           cfg,
+		ResourceResolver:   resourceResolver,
+		CredentialProvider: credentialProvider,
+		Config:             cfg,
 		ARMOwnedResourceReconcilerCommon: reconcilers.ARMOwnedResourceReconcilerCommon{
 			ResourceResolver: resourceResolver,
 			ReconcilerCommon: reconcilers.ReconcilerCommon{
@@ -73,45 +73,15 @@ func (r *MySQLUserReconciler) CreateOrUpdate(ctx context.Context, log logr.Logge
 
 	// Augment Log
 	log = log.WithValues("azureName", user.AzureName())
-
-	// Resolve the secrets
-	secrets, err := r.ResourceResolver.ResolveResourceSecretReferences(ctx, user)
-	if err != nil {
-		return ctrl.Result{}, reconcilers.ClassifyResolverError(err)
-	}
-
-	db, err := r.connectToDB(ctx, log, user, secrets)
+	connector, err := r.newDBConnector(log, user)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
-	defer db.Close()
 
-	log.V(Status).Info("Creating MySQL user")
-
-	password, err := secrets.LookupFromPtr(user.Spec.LocalUser.Password)
+	err = connector.CreateOrUpdate(ctx)
 	if err != nil {
-		return ctrl.Result{}, errors.Wrap(err, "failed to look up .spec.localUser.Password")
+		return ctrl.Result{}, err
 	}
-
-	// Create or update the user. Note that this updates password if it has changed
-	username := user.Spec.AzureName
-	err = mysqlutil.CreateOrUpdateUser(ctx, db, user.Spec.AzureName, user.Spec.Hostname, password)
-	if err != nil {
-		return ctrl.Result{}, errors.Wrap(err, "failed to create user")
-	}
-
-	// Ensure that the privileges are set
-	err = mysqlutil.ReconcileUserServerPrivileges(ctx, db, username, user.Spec.Hostname, user.Spec.Privileges)
-	if err != nil {
-		return ctrl.Result{}, errors.Wrap(err, "ensuring server roles")
-	}
-
-	err = mysqlutil.ReconcileUserDatabasePrivileges(ctx, db, username, user.Spec.Hostname, user.Spec.DatabasePrivileges)
-	if err != nil {
-		return ctrl.Result{}, errors.Wrap(err, "ensuring database roles")
-	}
-
-	log.V(Status).Info("Successfully reconciled MySQLUser")
 
 	return ctrl.Result{}, nil
 }
@@ -138,22 +108,12 @@ func (r *MySQLUserReconciler) Delete(ctx context.Context, log logr.Logger, event
 		return ctrl.Result{}, err
 	}
 
-	secrets, err := r.ResourceResolver.ResolveResourceSecretReferences(ctx, user)
+	connector, err := r.newDBConnector(log, user)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
 
-	db, err := r.connectToDB(ctx, log, user, secrets)
-	if err != nil {
-		return ctrl.Result{}, err
-	}
-	defer db.Close()
-
-	// TODO: There's still probably some ways that this user can be deleted but that we don't detect (and
-	// TODO: so might cause an error triggering the resource to get stuck).
-	// TODO: We check for owner not existing above, but cases where the server is in the process of being
-	// TODO: deleted (or all system tables have been wiped?) might also exist...
-	err = mysqlutil.DropUser(ctx, db, user.Spec.AzureName)
+	err = connector.Delete(ctx)
 	if err != nil {
 		return ctrl.Result{}, err
 	}
@@ -181,18 +141,12 @@ func (r *MySQLUserReconciler) UpdateStatus(ctx context.Context, log logr.Logger,
 		return err
 	}
 
-	secrets, err := r.ResourceResolver.ResolveResourceSecretReferences(ctx, user)
+	connector, err := r.newDBConnector(log, user)
 	if err != nil {
 		return err
 	}
 
-	db, err := r.connectToDB(ctx, log, user, secrets)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	exists, err := mysqlutil.DoesUserExist(ctx, db, user.Spec.AzureName)
+	exists, err := connector.Exists(ctx)
 	if err != nil {
 		return err
 	}
@@ -206,48 +160,26 @@ func (r *MySQLUserReconciler) UpdateStatus(ctx context.Context, log logr.Logger,
 	return nil
 }
 
-func (r *MySQLUserReconciler) connectToDB(ctx context.Context, _ logr.Logger, user *asomysql.User, secrets genruntime.Resolved[genruntime.SecretReference]) (*sql.DB, error) {
-	// Get the owner - at this point it must exist
-	owner, err := r.ResourceResolver.ResolveOwner(ctx, user)
-	if err != nil {
-		return nil, err
+func (r *MySQLUserReconciler) newDBConnector(log logr.Logger, user *asomysql.User) (Connector, error) {
+	if user.Spec.LocalUser != nil {
+		return &localUser{
+			user:               user,
+			resourceResolver:   r.ResourceResolver,
+			credentialProvider: r.CredentialProvider,
+			log:                log,
+		}, nil
 	}
 
-	flexibleServer, ok := owner.(*dbformysql.FlexibleServer)
-	if !ok {
-		return nil, errors.Errorf("owner was not type FlexibleServer, instead: %T", owner)
-	}
-	// Magical assertion to ensure that this is still the storage type
-	var _ ctrlconversion.Hub = &dbformysql.FlexibleServer{}
-
-	if flexibleServer.Status.FullyQualifiedDomainName == nil {
-		// This possibly means that the server hasn't finished deploying yet
-		err = errors.Errorf("owning Flexibleserver %q '.status.fullyQualifiedDomainName' not set. Has the server been provisioned successfully?", flexibleServer.Name)
-		return nil, conditions.NewReadyConditionImpactingError(err, conditions.ConditionSeverityWarning, conditions.ReasonWaitingForOwner)
-	}
-	serverFQDN := *flexibleServer.Status.FullyQualifiedDomainName
-
-	adminPassword, err := secrets.LookupFromPtr(user.Spec.LocalUser.ServerAdminPassword)
-	if err != nil {
-		err = errors.Wrap(err, "failed to look up .spec.localUser.ServerAdminPassword")
-		err = conditions.NewReadyConditionImpactingError(err, conditions.ConditionSeverityWarning, conditions.ReasonSecretNotFound)
-		return nil, err
+	if user.Spec.AADUser != nil {
+		return &aadUser{
+			user:               user,
+			resourceResolver:   r.ResourceResolver,
+			credentialProvider: r.CredentialProvider,
+			log:                log,
+		}, nil
 	}
 
-	// Admin User
-	adminUser := user.Spec.LocalUser.ServerAdminUsername
-
-	// Connect to the DB
-	db, err := mysqlutil.ConnectToDB(ctx, serverFQDN, mysqlutil.SystemDatabase, mysqlutil.ServerPort, adminUser, adminPassword)
-	if err != nil {
-		return nil, errors.Wrapf(
-			err,
-			"failed to connect database. Server: %s, Database: %s, Port: %d, AdminUser: %s",
-			serverFQDN,
-			mysqlutil.SystemDatabase,
-			mysqlutil.ServerPort,
-			adminUser)
-	}
-
-	return db, nil
+	// This is also enforced with a webhook
+	err := errors.Errorf("unknown user type, user must be LocalUser or AADUser")
+	return nil, conditions.NewReadyConditionImpactingError(err, conditions.ConditionSeverityError, conditions.ReasonFailed)
 }
--- v2/internal/util/mysql/mysql.go
+++ v2/internal/util/mysql/mysql.go
@@ -28,33 +28,20 @@ const DriverName = "mysql"
 const SystemDatabase = "mysql"
 
 func ConnectToDB(ctx context.Context, serverAddress string, database string, port int, user string, password string) (*sql.DB, error) {
-	c := mysql.NewConfig()
-	c.Addr = fmt.Sprintf("%s:%d", serverAddress, port)
-	c.DBName = database
-	c.User = user
-	c.Passwd = password
-	c.TLSConfig = "true"
+	c := newConfig(serverAddress, database, port, user, password)
+	return connectToDB(ctx, c)
+}
 
-	// Set other options
-	c.InterpolateParams = true
-	c.Net = "tcp"
+// ConnectToDBAAD connects to the MySQL database using the specified user.
+// user must be an AAD user of the form:
+//   - "user@tenant.onmicrosoft.com" (for AAD users)
+//   - "my-mi" (for Managed Identities)
+//   - "mygroup" (for AAD groups)
+func ConnectToDBAAD(ctx context.Context, serverAddress string, database string, port int, user string, token string) (*sql.DB, error) {
+	c := newConfig(serverAddress, database, port, user, token)
+	c.AllowCleartextPasswords = true // Must be set for AAD auth
 
-	db, err := sql.Open(DriverName, c.FormatDSN())
-	if err != nil {
-		return db, err
-	}
-	db.SetConnMaxLifetime(1 * time.Minute)
-	db.SetMaxOpenConns(1)
-	db.SetMaxIdleConns(1)
-
-	// We ping here to ensure that the connection is actually viable, as per
-	// https://github.com/go-sql-driver/mysql/wiki/Examples#a-word-on-sqlopen
-	err = db.PingContext(ctx)
-	if err != nil {
-		return db, errors.Wrapf(err, "error pinging the mysql db (%s:%d/%s)", serverAddress, port, database)
-	}
-
-	return db, err
+	return connectToDB(ctx, c)
 }
 
 func HostnameOrDefault(hostname string) string {
@@ -71,14 +58,12 @@ func CreateOrUpdateUser(ctx context.Context, db *sql.DB, username string, hostna
 	// we call both CREATE and ALTER here so achieve an idempotent operation that also updates the password seamlessly
 	// if it has changed
 
-	// TODO: Support aliasing: https://github.com/Azure/azure-service-operator/issues/1402
 	statement := "CREATE USER IF NOT EXISTS ?@? IDENTIFIED BY ?"
 	_, err := db.ExecContext(ctx, statement, username, hostname, password)
 	if err != nil {
 		return errors.Wrapf(err, "failed to create user %s", username)
 	}
 
-	// TODO: Support aliasing: https://github.com/Azure/azure-service-operator/issues/1402
 	statement = "ALTER USER IF EXISTS ?@? IDENTIFIED BY ?"
 	_, err = db.ExecContext(ctx, statement, username, hostname, password)
 	if err != nil {
@@ -88,9 +73,32 @@ func CreateOrUpdateUser(ctx context.Context, db *sql.DB, username string, hostna
 	return nil
 }
 
+// CreateOrUpdateAADUser creates or updates an AAD user.
+// See https://learn.microsoft.com/en-us/azure/mysql/flexible-server/how-to-azure-ad for details on how to create AAD
+// users.
+// username can be either the actual AAD username (for real AAD users), the group name for groups, or
+// the managed identity name for managed identities.
+func CreateOrUpdateAADUser(ctx context.Context, db *sql.DB, username string, alias string) error {
+	var err error
+	if alias != "" {
+		statement := "CREATE AADUSER IF NOT EXISTS ? AS ?"
+		// Result rowset never returns any rows for CREATE AADUSER, so we just drop it
+		_, err = db.ExecContext(ctx, statement, username, alias)
+	} else {
+		statement := "CREATE AADUSER IF NOT EXISTS ?"
+		// Result rowset never returns any rows for CREATE AADUSER, so we just drop it
+		_, err = db.ExecContext(ctx, statement, username)
+	}
+	if err != nil {
+		return errors.Wrapf(err, "failed to create user %s", username)
+	}
+
+	return nil
+}
+
 // DoesUserExist checks if db contains user
 func DoesUserExist(ctx context.Context, db *sql.DB, username string) (bool, error) {
-	row := db.QueryRowContext(ctx, "SELECT User FROM mysql.user WHERE User = ?", username)
+	row := db.QueryRowContext(ctx, "SELECT User FROM mysql.user WHERE User = ?", username) // TODO: Username here has to be Alias in AAD case, if there is one
 	var name string
 	err := row.Scan(&name)
 	if err != nil {
@@ -110,4 +118,38 @@ func DoesUserExist(ctx context.Context, db *sql.DB, username string) (bool, erro
 func DropUser(ctx context.Context, db *sql.DB, username string) error {
 	_, err := db.ExecContext(ctx, "DROP USER IF EXISTS ?", username)
 	return err
+}
+
+func newConfig(serverAddress string, database string, port int, user string, password string) *mysql.Config {
+	c := mysql.NewConfig()
+	c.Addr = fmt.Sprintf("%s:%d", serverAddress, port)
+	c.DBName = database
+	c.User = user
+	c.Passwd = password
+	c.TLSConfig = "true"
+
+	// Set other options
+	c.InterpolateParams = true
+	c.Net = "tcp"
+
+	return c
+}
+
+func connectToDB(ctx context.Context, c *mysql.Config) (*sql.DB, error) {
+	db, err := sql.Open(DriverName, c.FormatDSN())
+	if err != nil {
+		return db, err
+	}
+	db.SetConnMaxLifetime(1 * time.Minute)
+	db.SetMaxOpenConns(1)
+	db.SetMaxIdleConns(1)
+
+	// We ping here to ensure that the connection is actually viable, as per
+	// https://github.com/go-sql-driver/mysql/wiki/Examples#a-word-on-sqlopen
+	err = db.PingContext(ctx)
+	if err != nil {
+		return db, errors.Wrapf(err, "error pinging the mysql db (%s/%s)", c.Addr, c.DBName)
+	}
+
+	return db, err
 }
