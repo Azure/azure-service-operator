c0ad9e5f144980eec9339a4a67d535d50810cdbf
Bevan Arps <bevan.arps@microsoft.com>
2023-06-27 11:51:41 +1200

    Modify PostReconciliationChecker to use a resolver

--- v2/api/network/customizations/private_endpoints_extensions.go
+++ v2/api/network/customizations/private_endpoints_extensions.go
@@ -6,6 +6,7 @@ package customizations
 import (
 	"context"
 	"fmt"
+	"github.com/Azure/azure-service-operator/v2/internal/resolver"
 
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
@@ -13,7 +14,6 @@ import (
 
 	network "github.com/Azure/azure-service-operator/v2/api/network/v1api20220701storage"
 	"github.com/Azure/azure-service-operator/v2/internal/genericarmclient"
-	"github.com/Azure/azure-service-operator/v2/internal/util/kubeclient"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/extensions"
 )
@@ -24,7 +24,7 @@ func (extension *PrivateEndpointExtension) PostReconcileCheck(
 	_ context.Context,
 	obj genruntime.MetaObject,
 	_ genruntime.MetaObject,
-	_ kubeclient.Client,
+	_ *resolver.Resolver,
 	_ *genericarmclient.GenericClient,
 	_ logr.Logger,
 	_ extensions.PostReconcileCheckFunc) (extensions.PostReconcileCheckResult, error) {
--- v2/internal/reconcilers/arm/azure_generic_arm_reconciler_instance.go
+++ v2/internal/reconcilers/arm/azure_generic_arm_reconciler_instance.go
@@ -47,8 +47,8 @@ func newAzureDeploymentReconcilerInstance(
 	log logr.Logger,
 	recorder record.EventRecorder,
 	connection Connection,
-	reconciler AzureDeploymentReconciler) *azureDeploymentReconcilerInstance {
-
+	reconciler AzureDeploymentReconciler,
+) *azureDeploymentReconcilerInstance {
 	return &azureDeploymentReconcilerInstance{
 		Obj:                              metaObj,
 		Log:                              log,
@@ -484,7 +484,7 @@ func (r *azureDeploymentReconcilerInstance) postReconciliationCheck(ctx context.
 	}
 
 	// Run our post-reconciliation checker
-	check, checkErr := checker(ctx, r.Obj, owner, r.KubeClient, r.ARMConnection.Client(), r.Log)
+	check, checkErr := checker(ctx, r.Obj, owner, r.ResourceResolver, r.ARMConnection.Client(), r.Log)
 	if checkErr != nil {
 		// Something went wrong running the check.
 		return extensions.PostReconcileCheckResult{}, checkErr
