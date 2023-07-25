12c30a4f34045ec636c4d59460fafc1fbe084f12
Bevan Arps <bevan.arps@microsoft.com>
2023-07-26 10:24:16 +1200

    Add comments to PackageImportStyle

--- v2/tools/generator/internal/astmodel/package_import_style.go
+++ v2/tools/generator/internal/astmodel/package_import_style.go
@@ -5,10 +5,11 @@
 
 package astmodel
 
+// PackageImportStyle is used to determine how to define a unique package import alias in a given context
 type PackageImportStyle string
 
 const (
-	VersionOnly     PackageImportStyle = "VersionOnly"
-	GroupOnly       PackageImportStyle = "GroupOnly"
-	GroupAndVersion PackageImportStyle = "GroupAndVersion"
+	VersionOnly     PackageImportStyle = "VersionOnly"     // Only need the version of the package to be unique
+	GroupOnly       PackageImportStyle = "GroupOnly"       // Only need the group of the package to be unique
+	GroupAndVersion PackageImportStyle = "GroupAndVersion" // Need both group and version of the package to be unique
 )
