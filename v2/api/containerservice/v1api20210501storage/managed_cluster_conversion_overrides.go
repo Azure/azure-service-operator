b3f695a57440401d559a45918f9e90b98b60ccfa
Bevan Arps <bevan.arps@microsoft.com>
2023-10-03 20:13:12 +0000

    Fix imports

--- v2/api/containerservice/v1api20210501storage/managed_cluster_conversion_overrides.go
+++ v2/api/containerservice/v1api20210501storage/managed_cluster_conversion_overrides.go
@@ -1,10 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-package storage
+package v1api20210501storage
 
 import (
-	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201/storage"
+	v20230201s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230201storage"
 	"github.com/Azure/azure-service-operator/v2/internal/util/to"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
