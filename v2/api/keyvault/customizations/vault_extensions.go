7ccb70a10e46a1a46defb46a0e2820a1ae1bb57a
Bevan Arps <bevan.arps@microsoft.com>
2023-10-09 00:33:04 +0000

    Switch to using the SDK to detect and purge

--- v2/api/keyvault/customizations/vault_extensions.go
+++ v2/api/keyvault/customizations/vault_extensions.go
@@ -7,14 +7,16 @@ package customizations
 
 import (
 	"context"
+	"net/http"
 	"strings"
+	"time"
 
 	keyvault "github.com/Azure/azure-service-operator/v2/api/keyvault/v1api20210401previewstorage"
 	resources "github.com/Azure/azure-service-operator/v2/api/resources/v1api20200601storage"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
-	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/runtime"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/keyvault/armkeyvault"
 	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
@@ -64,15 +66,37 @@ func (ex *VaultExtension) ModifyARMResource(
 		return armObj, nil
 	}
 
+	// Get the owner of the KeyVault, we need this resource group to determine the subscription
+	owner, ownerErr := ex.getOwner(ctx, kv, resolver, log)
+	if ownerErr != nil {
+		return nil, errors.Wrapf(ownerErr, "unable to find owner of KeyVault %s", kv.Name)
+	}
+
+	// Parse the ID of the owner
+	// (Can't use the KeyVault as we do this before the KV exists)
+	id, err := genruntime.GetAndParseResourceID(owner)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get and parse resource ID from KeyVault owner")
+	}
+
+	vc, err := armkeyvault.NewVaultsClient(id.SubscriptionID, armClient.Creds(), armClient.ClientOptions())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new VaultsClient")
+	}
+
 	var createMode *string
 
 	if strings.EqualFold(*kv.Spec.Properties.CreateMode, CreateMode_CreateOrRecover) {
-		mode := ex.handleCreateOrRecover(ctx, kv, armClient, kubeClient, resolver, log)
+		mode, err := ex.handleCreateOrRecover(ctx, kv, vc, resolver, log)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error checking for existence of soft-deleted KeyVault")
+		}
+
 		createMode = &mode
 	}
 
 	if strings.EqualFold(*kv.Spec.Properties.CreateMode, CreateMode_PurgeThenCreate) {
-		err := ex.handlePurgeThenCreate(ctx, kv, armClient, kubeClient, resolver, log)
+		err := ex.handlePurgeThenCreate(ctx, kv, vc, resolver, log)
 		if err != nil {
 			return nil, errors.Wrapf(err, "error purging soft-deleted KeyVault")
 		}
@@ -96,16 +120,13 @@ func (ex *VaultExtension) ModifyARMResource(
 func (ex *VaultExtension) handleCreateOrRecover(
 	ctx context.Context,
 	kv *keyvault.Vault,
-	armClient *genericarmclient.GenericClient,
-	kubeClient kubeclient.Client,
+	vc *armkeyvault.VaultsClient,
 	resolver *resolver.Resolver,
 	log logr.Logger,
-) string {
-	exists, err := ex.checkForExistenceOfDeletedKeyVault(ctx, kv, armClient, kubeClient, resolver, log)
+) (string, error) {
+	exists, err := ex.checkForExistenceOfDeletedKeyVault(ctx, kv, resolver, vc, log)
 	if err != nil {
-		// If we can't detect, assume best case and proceed
-		log.Error(err, "error checking for existence of soft-deleted KeyVault")
-		return CreateMode_Default
+		return "", errors.Wrapf(err, "error checking for existence of soft-deleted KeyVault %s", kv.Name)
 	}
 
 	result := CreateMode_Default
@@ -119,21 +140,20 @@ func (ex *VaultExtension) handleCreateOrRecover(
 		"Soft-deleted-KeyVault-exists", exists,
 		"createMode", result)
 
-	return result
+	return result, err
 }
 
 func (ex *VaultExtension) handlePurgeThenCreate(
 	ctx context.Context,
 	kv *keyvault.Vault,
-	armClient *genericarmclient.GenericClient,
-	kubeClient kubeclient.Client,
+	vc *armkeyvault.VaultsClient,
 	resolver *resolver.Resolver,
 	log logr.Logger,
 ) error {
 	// Find out whether a soft-deleted KeyVault with the same name exists
-	exists, err := ex.checkForExistenceOfDeletedKeyVault(ctx, kv, armClient, kubeClient, resolver, log)
+	exists, err := ex.checkForExistenceOfDeletedKeyVault(ctx, kv, resolver, vc, log)
 	if err != nil {
-		// Couldn't determine whether a soft-deleted keyvault exists, assume it doesn't
+		// Could not determine whether a soft-deleted keyvault exists, assume it doesn't
 
 		log.Error(err, "error checking for existence of soft-deleted KeyVault")
 		return nil
@@ -145,15 +165,27 @@ func (ex *VaultExtension) handlePurgeThenCreate(
 		"Deleted KeyVault exists", exists)
 
 	if exists {
-		// if a soft-deleted KeyVault exists, we need to purge it before we can create a new one
-		req, err := ex.createPurgeRequest(ctx, kv, armClient, kubeClient, resolver, log)
-		if err != nil {
-			return errors.Wrapf(err, "error creating purge request for KeyVault %s", kv.Name)
+		// Get the owner of the KeyVault, we need this resource group to determine the location
+		owner, ownerErr := ex.getOwner(ctx, kv, resolver, log)
+		if ownerErr != nil {
+			return errors.Wrapf(ownerErr, "unable to find owner of KeyVault %s", kv.Name)
 		}
 
-		_, err = armClient.Post(ctx, req)
+		// if a soft-deleted KeyVault exists, we need to purge it before we can create a new one
+		// Get the location of the KeyVault
+		location, locationOk := ex.getLocation(kv, owner)
+		if !locationOk {
+			return errors.Errorf("unable to determine location of KeyVault %s", kv.Name)
+		}
+
+		poller, err := vc.BeginPurgeDeleted(ctx, kv.Name, location, &armkeyvault.VaultsClientBeginPurgeDeletedOptions{})
 		if err != nil {
-			return errors.Wrapf(err, "error purging KeyVault %s", kv.Name)
+			return errors.Wrapf(err, "failed to begin purging deleted KeyVault %s", kv.Name)
+		}
+
+		_, err = poller.PollUntilDone(ctx, &runtime.PollUntilDoneOptions{Frequency: 10 * time.Second})
+		if err != nil {
+			return errors.Wrapf(err, "failed to purge deleted KeyVault %s", kv.Name)
 		}
 	}
 
@@ -165,9 +197,8 @@ func (ex *VaultExtension) handlePurgeThenCreate(
 func (ex *VaultExtension) checkForExistenceOfDeletedKeyVault(
 	ctx context.Context,
 	kv *keyvault.Vault,
-	armClient *genericarmclient.GenericClient,
-	kubeClient kubeclient.Client,
 	resolver *resolver.Resolver,
+	vaultsClient *armkeyvault.VaultsClient,
 	log logr.Logger,
 ) (bool, error) {
 	// Get the owner of the KeyVault, we need this resource group to determine the subscription
@@ -176,100 +207,43 @@ func (ex *VaultExtension) checkForExistenceOfDeletedKeyVault(
 		return false, errors.Wrapf(ownerErr, "unable to find owner of KeyVault %s", kv.Name)
 	}
 
-	// Get the subscription of the KeyVault
-	subscription, subscriptionErr := ex.getSubscription(owner)
-	if subscriptionErr != nil {
-		return false, errors.Wrapf(subscriptionErr, "unable to determine subscription for KeyVault %s", kv.Name)
-	}
-
 	// Get the location of the KeyVault
 	location, locationOk := ex.getLocation(kv, owner)
 	if !locationOk {
 		return false, errors.Errorf("unable to determine location of KeyVault %s", kv.Name)
 	}
 
-	// Create the ARM ID of the soft-deleted KeyVault, if any
-	armIdOfDeletedKeyVault, err := ex.createDeletedKeyVaultARMID(
-		subscription,
-		location,
-		kv.Name)
-	if err != nil {
-		return false, errors.Wrapf(err, "failed to create ARM ID for potential soft-deleted KeyVault %s", kv.Name)
+	// Get the name of the KeyVault
+	vaultName := kv.Spec.AzureName
+	if vaultName == "" {
+		vaultName = kv.Name
 	}
 
-	// Get the API version of the KeyVault API to use
-	apiVersion, err := genruntime.GetAPIVersion(kv, kubeClient.Scheme())
-	if err != nil {
-		return false, errors.Wrapf(err, "unable to get api version for KeyVault")
-	}
+	// Default to assuming a soft-deleted keyvault exists
+	exists := true
 
-	// Check to see if the deleted keyvault exists
-	exists, _, err := armClient.HeadByID(ctx, armIdOfDeletedKeyVault, apiVersion)
+	// Check to see if this is true
+	_, err := vaultsClient.GetDeleted(ctx, vaultName, location, &armkeyvault.VaultsClientGetDeletedOptions{})
 	if err != nil {
-		return false, errors.Wrapf(err, "error checking for existence of deleted KeyVault %s", kv.Name)
+		var responseError *azcore.ResponseError
+		if errors.As(err, &responseError) {
+			if responseError.StatusCode != http.StatusNotFound {
+				return false, errors.Wrapf(err, "failed to get deleted KeyVault %s, error %d", kv.Name, responseError.StatusCode)
+			}
+
+			// KeyVault doesn't exist,
+			exists = false
+		}
 	}
 
 	log.Info(
 		"Checking for existence of soft-deleted KeyVault",
 		"KeyVault", kv.Name,
-		"Subscription", subscription,
 		"Location", location,
 		"Exists", exists,
 	)
 
 	return exists, nil
-}
-
-func (ex *VaultExtension) createPurgeRequest(
-	ctx context.Context,
-	kv *keyvault.Vault,
-	armClient *genericarmclient.GenericClient,
-	kubeClient kubeclient.Client,
-	resolver *resolver.Resolver,
-	log logr.Logger,
-) (*policy.Request, error) {
-	// Get the owner of the KeyVault, we need this resource group to determine the subscription
-	owner, ownerErr := ex.getOwner(ctx, kv, resolver, log)
-	if ownerErr != nil {
-		return nil, errors.Wrapf(ownerErr, "unable to find owner of KeyVault %s", kv.Name)
-	}
-
-	// Get the subscription of the KeyVault
-	subscription, subscriptionErr := ex.getSubscription(owner)
-	if subscriptionErr != nil {
-		return nil, errors.Wrapf(subscriptionErr, "unable to determine subscription for KeyVault %s", kv.Name)
-	}
-
-	// Get the location of the KeyVault
-	location, locationOk := ex.getLocation(kv, owner)
-	if !locationOk {
-		return nil, errors.Errorf("unable to determine location of KeyVault %s", kv.Name)
-	}
-
-	// Create the ARM ID of the soft-deleted KeyVault, if any
-	armIdOfDeletedKeyVault, err := ex.createDeletedKeyVaultARMID(
-		subscription,
-		location,
-		kv.Name)
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to create ARM ID for potential soft-deleted KeyVault %s", kv.Name)
-	}
-
-	// Get the API version of the KeyVault API to use
-	apiVersion, err := genruntime.GetAPIVersion(kv, kubeClient.Scheme())
-	if err != nil {
-		return nil, errors.Wrapf(err, "unable to get api version for KeyVault")
-	}
-
-	req, err := armClient.CreatePostRequest(
-		ctx,
-		runtime.JoinPaths(armIdOfDeletedKeyVault, "purge"),
-		apiVersion)
-	if err != nil {
-		return nil, errors.Wrapf(err, "error creating purge request for KeyVault %s", kv.Name)
-	}
-
-	return req, nil
 }
 
 func (*VaultExtension) getOwner(
@@ -303,27 +277,6 @@ func (*VaultExtension) getOwner(
 	return rg, nil
 }
 
-func (*VaultExtension) getSubscription(
-	rg *resources.ResourceGroup,
-) (string, error) {
-	if rg.Status.Id == nil {
-		return "", errors.Errorf(
-			"unable to determine subscription as resource group %s has no id",
-			rg.Name)
-	}
-
-	// Parse id into a more useful form
-	armID, err := arm.ParseResourceID(*rg.Status.Id)
-	if err != nil {
-		return "", errors.Wrapf(
-			err,
-			"unable to parse ARMID of ResourceGroup %s",
-			rg.AzureName())
-	}
-
-	return armID.SubscriptionID, nil
-}
-
 // findKeyVaultLocation determines which location we're trying to create KeyVault within
 func (*VaultExtension) getLocation(
 	kv *keyvault.Vault,
@@ -341,36 +294,3 @@ func (*VaultExtension) getLocation(
 
 	return "", false
 }
-
-func (*VaultExtension) getAPIVersion(
-	kv *keyvault.Vault,
-	kubeClient kubeclient.Client,
-) (string, error) {
-	api, err := genruntime.GetAPIVersion(kv, kubeClient.Scheme())
-	if err != nil {
-		return "", errors.Wrapf(err, "error getting api version for resource %s", kv.GetName())
-	}
-
-	return api, nil
-}
-
-func (*VaultExtension) createDeletedKeyVaultARMID(
-	subscription string,
-	location string,
-	name string,
-) (string, error) {
-	// For a deleted KeyVault, the ARM ID has this format:
-	// /subscriptions/{subscriptionId}/providers/Microsoft.KeyVault/locations/{location}/deletedVaults/{vaultName}?api-version=2022-07-01
-
-	return genericarmclient.MakeSubscriptionScopeARMID(
-		subscription,
-		"Microsoft.KeyVault",
-		"locations", location,
-		"deletedVaults", name)
-}
-
-/*
-
-  logr.go:278: I2023-09-27T19:52:21Z] VaultController "msg"="updated resource in etcd" name="aso-kv-gs" namespace="aso-test-keyvault-whenrecoverspecified-recoverssuccessfully" kind="&TypeMeta{Kind:Vault,APIVersion:keyvault.azure.com/v1api20210401previewstorage,}" resourceVersion="983" generation=1 uid="3d532c3b-e7b7-484c-9432-485c9bf4539e" ownerReferences=[]v1.OwnerReference{v1.OwnerReference{APIVersion:"resources.azure.com/v1api20200601storage", Kind:"ResourceGroup", Name:"asotest-rg-bmhvxo", UID:"77167b62-35a0-408e-b62f-a067ec8f0851", Controller:(*bool)(nil), BlockOwnerDeletion:(*bool)(nil)}} creationTimestamp="2023-09-27 19:43:35 +0000 UTC" deletionTimestamp=<nil> finalizers=[]string{"serviceoperator.azure.com/finalizer"} annotations=map[string]string{"serviceoperator.azure.com/resource-id":"/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/asotest-rg-bmhvxo/providers/Microsoft.KeyVault/vaults/aso-kv-gs"} conditions="[Condition [Ready], Status = \"False\", ObservedGeneration = 1, Severity = \"Warning\", Reason = \"ConflictError\", Message = \"A vault with the same name already exists in deleted state. You need to either recover or purge existing key vault. Follow this link https://go.microsoft.com/fwlink/?linkid=2149745 for more information on soft delete.: PUT https://management.azure.com/subscriptions/00000000-0000-0000-0000-000000000000/resourceGroups/asotest-rg-bmhvxo/providers/Microsoft.KeyVault/vaults/aso-kv-gs\\n--------------------------------------------------------------------------------\\nRESPONSE 409: 409 Conflict\\nERROR CODE: ConflictError\\n--------------------------------------------------------------------------------\\n{\\n  \\\"error\\\": {\\n    \\\"code\\\": \\\"ConflictError\\\",\\n    \\\"message\\\": \\\"A vault with the same name already exists in deleted state. You need to either recover or purge existing key vault. Follow this link https://go.microsoft.com/fwlink/?linkid=2149745 for more information on soft delete.\\\"\\n  }\\n}\\n--------------------------------------------------------------------------------\\n\", LastTransitionTime = \"2023-09-27 19:52:10 +0000 UTC\"]" owner="asotest-rg-bmhvxo, Group/Kind: resources.azure.com/ResourceGroup"
-
-*/
