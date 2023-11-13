da66f376e0d69859629b097e3a9b819ee1e00e05
Harshdeep Singh <38904804+super-harsh@users.noreply.github.com>
2023-11-14 12:47:32 +1300

    Add todo for random name exclusion

--- v2/internal/controllers/samples_test.go
+++ v2/internal/controllers/samples_test.go
@@ -39,6 +39,7 @@ var randomNameExclusions = []string{
 	"compute",
 	"documentdb",
 	"insights",
+	// TODO: We can use random names for 'kubernetesconfiguration' once https://github.com/Azure/azure-service-operator/issues/3544 is merged.
 	"kubernetesconfiguration",
 	"network",
 	"web",
