c444a7559d0b8d8a22827ca38b887d83aef59d42
Bevan Arps <bevan.arps@microsoft.com>
2023-12-07 10:54:38 +1300

    Augment conversion for UpgradeOverrideSettings

---
+++ v2/api/containerservice/v1api20230201/storage/compat/cluster_upgrade_settings_types.go
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ */
+
+package compat
+
+import (
+	v20231001s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20231001/storage"
+	"github.com/Azure/azure-service-operator/v2/internal/util/to"
+)
+
+var _ augmentConversionForUpgradeOverrideSettings = &UpgradeOverrideSettings{}
+
+const ignoreKubernetesDeprecations = "IgnoreKubernetesDeprecations"
+
+func (settings *UpgradeOverrideSettings) AssignPropertiesFrom(src *v20231001s.UpgradeOverrideSettings) error {
+	// If the GA version has ForceUpgrade true, the preview version needs to add "IgnoreKubernetesDeprecations"
+	if src.ForceUpgrade != nil && *src.ForceUpgrade {
+		settings.ControlPlaneOverrides = append(settings.ControlPlaneOverrides, ignoreKubernetesDeprecations)
+	}
+
+	return nil
+}
+
+func (settings *UpgradeOverrideSettings) AssignPropertiesTo(dest *v20231001s.UpgradeOverrideSettings) error {
+	// If the preview version has "IgnoreKubernetesDeprecations" in ControlPlaneOverrides, the GA version needs to have
+	// ForceUpgrade true, otherwise false.
+	dest.ForceUpgrade = to.Ptr(false)
+	for _, override := range settings.ControlPlaneOverrides {
+		if override == ignoreKubernetesDeprecations {
+			dest.ForceUpgrade = to.Ptr(true)
+			break
+		}
+	}
+
+	return nil
+}
