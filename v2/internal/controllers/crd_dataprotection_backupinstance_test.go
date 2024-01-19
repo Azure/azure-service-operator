4e6a9cb5a59e4db5764d13ee280b81079d7a6418
Mayank Aggarwal <mayaggar@microsoft.com>
2024-01-19 06:40:37 +0000

    changes

--- v2/internal/controllers/crd_dataprotection_backupinstance_test.go
+++ v2/internal/controllers/crd_dataprotection_backupinstance_test.go
@@ -7,6 +7,7 @@ package controllers_test
 
 import (
 	// The testing package is imported for testing-related functionality.
+	"fmt"
 	"testing"
 
 	// The gomega package is used for assertions and expectations in tests.
@@ -14,7 +15,7 @@ import (
 
 	// The dataprotection package contains types and functions related to dataprotection resources.
 	authorization "github.com/Azure/azure-service-operator/v2/api/authorization/v1api20220401"
-	aks "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20231001"
+	aks "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230202preview"
 	dataprotection "github.com/Azure/azure-service-operator/v2/api/dataprotection/v1api20230101"
 	kubernetesconfiguration "github.com/Azure/azure-service-operator/v2/api/kubernetesconfiguration/v1api20230501"
 	storage "github.com/Azure/azure-service-operator/v2/api/storage/v1api20230101"
@@ -22,6 +23,7 @@ import (
 	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
 	// The to package includes utilities for converting values to pointers.
 	"github.com/Azure/azure-service-operator/v2/internal/util/to"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
 func Test_Dataprotection_Backupinstace_CRUD(t *testing.T) {
@@ -111,49 +113,70 @@ func Test_Dataprotection_Backupinstace_CRUD(t *testing.T) {
 	tc.CreateResourceAndWait(extension)
 
 	// give permission to extension msi over SA
-	roleAssignmentGUID, err := tc.Namer.GenerateUUID()
-	tc.Expect(err).ToNot(HaveOccurred())
-
-	roleAssignment := &authorization.RoleAssignment{
-		ObjectMeta: tc.MakeObjectMetaWithName(roleAssignmentGUID.String()),
+	extenstionRoleAssignment := &authorization.RoleAssignment{
+		ObjectMeta: tc.MakeObjectMetaWithName(tc.Namer.GenerateUUID().String()),
 		Spec: authorization.RoleAssignment_Spec{
-			Owner: tc.AsExtensionOwner(acct),
-			PrincipalIdFromConfig: &genruntime.ConfigMapReference{
-				Name: configMapName,
-				Key:  principalIdKey,
-			},
+			Owner:       tc.AsExtensionOwner(acct),
+			PrincipalId: extension.Status.Identity.PrincipalId,
 			RoleDefinitionReference: &genruntime.ResourceReference{
-				ARMID: fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c", tc.AzureSubscription), // This is contributor
+				ARMID: fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/17d1049b-9a84-46fb-8f53-869881c3d3ab", tc.AzureSubscription), // This is Storage Account Contributor Role
 			},
 		},
 	}
 
-	tc.CreateResourceAndWait(roleAssignment)
+	tc.CreateResourceAndWait(extenstionRoleAssignment)
 
-	// create vault
-
-	// Create a new backupvault resource
+	// create vault and policy
 	backupVault := newBackupVault(tc, rg, "asotestbackupvault")
+	backupPolicy := newBackupPolicy(tc, backupVault, "asotestbackuppolicy")
+	tc.CreateResourcesAndWait(backupVault, backupPolicy)
 
 	// give read permission to vault msi over SRG
-	// give read permission to vault msi over cluster
-	// create TA role binding
+	snapshotRGRoleAssignment := &authorization.RoleAssignment{
+		ObjectMeta: tc.MakeObjectMetaWithName(tc.Namer.GenerateUUID().String()),
+		Spec: authorization.RoleAssignment_Spec{
+			Owner:       tc.AsExtensionOwner(rg),
+			PrincipalId: backupVault.Status.Identity.PrincipalId,
+			RoleDefinitionReference: &genruntime.ResourceReference{
+				ARMID: fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/acdd72a7-3385-48ef-bd42-f606fba81ae7", tc.AzureSubscription), // This is Reader Role
+			},
+		},
+	}
 
-	// create policy
+	tc.CreateResourceAndWait(snapshotRGRoleAssignment)
+
+	// give read permission to vault msi over cluster
+	clusterRoleAssignment := &authorization.RoleAssignment{
+		ObjectMeta: tc.MakeObjectMetaWithName(tc.Namer.GenerateUUID().String()),
+		Spec: authorization.RoleAssignment_Spec{
+			Owner:       tc.AsExtensionOwner(cluster),
+			PrincipalId: backupVault.Status.Identity.PrincipalId,
+			RoleDefinitionReference: &genruntime.ResourceReference{
+				ARMID: fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Authorization/roleDefinitions/acdd72a7-3385-48ef-bd42-f606fba81ae7", tc.AzureSubscription), // This is Reader Role
+			},
+		},
+	}
+
+	tc.CreateResourceAndWait(clusterRoleAssignment)
+
+	// create TA role binding
+	trustedAccessRoleBinding := &aks.TrustedAccessRoleBinding{
+		ObjectMeta: tc.MakeObjectMetaWithName("tarb"),
+		Spec: aks.ManagedClusters_TrustedAccessRoleBinding_Spec{
+			Owner: testcommon.AsOwner(cluster),
+			Roles: []string{
+				// Microsoft.DataProtection/backupVaults/backup-operator
+				backupVault.GetType() + "/backup-operator",
+			},
+			SourceResourceReference: tc.MakeReferenceFromResource(backupVault),
+		},
+	}
+
+	tc.CreateResourceAndWait(trustedAccessRoleBinding)
 
 	//create backup instance
 
-	// Note:
-	// It is mandatory to create a backupvault before creating a backuppolicy
-
-	// Create a BackupPolicy
-	backupPolicy := newBackupPolicy(tc, backupVault, "asotestbackuppolicy")
-
-	// Sequence of creating BackupVault and BackupPolicy is handled by ASO internally
-	tc.CreateResourcesAndWait(backupVault, backupPolicy)
-
 	// Assertions and Expectations
-	armId := *backupPolicy.Status.Id
 
 	// Note:
 	// Patch Operations are currently not allowed on BackupInstance
