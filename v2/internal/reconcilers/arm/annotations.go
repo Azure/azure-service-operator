e2c68a3c2ee9e024313f9128b87381227179914d
Bevan Arps <bevan.arps@microsoft.com>
2024-02-14 17:54:14 +1300

    Redact large annotations from the log (#3782)
    * Redact token annotations in log
    
    * Use constant to identify token annocation

--- v2/internal/reconcilers/arm/annotations.go
+++ v2/internal/reconcilers/arm/annotations.go
@@ -8,40 +8,35 @@ package arm
 import (
 	"strconv"
 
+	"github.com/Azure/azure-service-operator/v2/internal/reconcilers"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
-)
-
-const (
-	PollerResumeTokenAnnotation = "serviceoperator.azure.com/poller-resume-token"
-	PollerResumeIDAnnotation    = "serviceoperator.azure.com/poller-resume-id"
-	LatestReconciledGeneration  = "serviceoperator.azure.com/latest-reconciled-generation"
 )
 
 // GetPollerResumeToken returns a poller ID and the poller token
 func GetPollerResumeToken(obj genruntime.MetaObject) (string, string, bool) {
-	token, hasResumeToken := obj.GetAnnotations()[PollerResumeTokenAnnotation]
-	id, hasResumeID := obj.GetAnnotations()[PollerResumeIDAnnotation]
+	token, hasResumeToken := obj.GetAnnotations()[reconcilers.PollerResumeTokenAnnotation]
+	id, hasResumeID := obj.GetAnnotations()[reconcilers.PollerResumeIDAnnotation]
 
 	return id, token, hasResumeToken && hasResumeID
 }
 
 func SetPollerResumeToken(obj genruntime.MetaObject, id string, token string) {
-	genruntime.AddAnnotation(obj, PollerResumeTokenAnnotation, token)
-	genruntime.AddAnnotation(obj, PollerResumeIDAnnotation, id)
+	genruntime.AddAnnotation(obj, reconcilers.PollerResumeTokenAnnotation, token)
+	genruntime.AddAnnotation(obj, reconcilers.PollerResumeIDAnnotation, id)
 }
 
 // ClearPollerResumeToken clears the poller resume token and ID annotations
 func ClearPollerResumeToken(obj genruntime.MetaObject) {
-	genruntime.RemoveAnnotation(obj, PollerResumeTokenAnnotation)
-	genruntime.RemoveAnnotation(obj, PollerResumeIDAnnotation)
+	genruntime.RemoveAnnotation(obj, reconcilers.PollerResumeTokenAnnotation)
+	genruntime.RemoveAnnotation(obj, reconcilers.PollerResumeIDAnnotation)
 }
 
 func SetLatestReconciledGeneration(obj genruntime.MetaObject) {
-	genruntime.AddAnnotation(obj, LatestReconciledGeneration, strconv.FormatInt(obj.GetGeneration(), 10))
+	genruntime.AddAnnotation(obj, reconcilers.LatestReconciledGeneration, strconv.FormatInt(obj.GetGeneration(), 10))
 }
 
 func GetLatestReconciledGeneration(obj genruntime.MetaObject) (int64, bool) {
-	val, hasGeneration := obj.GetAnnotations()[LatestReconciledGeneration]
+	val, hasGeneration := obj.GetAnnotations()[reconcilers.LatestReconciledGeneration]
 	gen, err := strconv.Atoi(val)
 	if err != nil {
 		return 0, false
