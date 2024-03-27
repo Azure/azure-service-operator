802baa0370b44eb2490ac7262d55061026711a2b
Bevan Arps <bevan.arps@microsoft.com>
2024-03-27 16:40:48 +1300

    Modify creation of ARM TypeNames to use a subpackage, subject to deny list

--- v2/tools/generator/internal/astmodel/type_name.go
+++ v2/tools/generator/internal/astmodel/type_name.go
@@ -5,6 +5,8 @@
 
 package astmodel
 
+import "github.com/Azure/azure-service-operator/v2/internal/set"
+
 type TypeName interface {
 	Type
 	Name() string
@@ -18,9 +20,59 @@ const (
 	StatusSuffix = "_STATUS"
 	// ARMSuffix is the suffix used for all ARM types
 	ARMSuffix = "_ARM"
+	// ARMPackageName is the name used for ARM subpackages
+	ARMPackageName = "arm"
 )
+
+var armPackageDenyList = set.Make(
+	"alertsmanagement",
+	"apimanagement",
+	"appconfiguration",
+	"authorization",
+	"batch",
+	"cache",
+	"cdn",
+	"compute",
+	"containerinstance",
+	"containerregistry",
+	"containerservice",
+	"datafactory",
+	"dataprotection",
+	"dbformariadb",
+	"dbformysql",
+	"dbforpostgresql",
+	"devices",
+	"documentdb",
+	"eventgrid",
+	"eventhub",
+	"insights",
+	"keyvault",
+	"kubernetesconfiguration",
+	"machinelearningservices",
+	"managedidentity",
+	"monitor",
+	"network",
+	"network.frontdoor",
+	"operationalinsights",
+	//"person",
+	"redhatopenshift",
+	"resources",
+	"search",
+	"servicebus",
+	"signalrservice",
+	"sql",
+	"storage",
+	"subscription",
+	"synapse",
+	"web")
 
 // CreateARMTypeName creates an ARM object type name
 func CreateARMTypeName(name InternalTypeName) InternalTypeName {
-	return MakeInternalTypeName(name.InternalPackageReference(), name.Name()+ARMSuffix)
+	pkg := name.InternalPackageReference()
+	if armPackageDenyList.Contains(pkg.Group()) {
+		return MakeInternalTypeName(pkg, name.Name()+ARMSuffix)
+	}
+
+	armPackage := MakeSubPackageReference(ARMPackageName, name.InternalPackageReference())
+	return MakeInternalTypeName(armPackage, name.Name())
 }
