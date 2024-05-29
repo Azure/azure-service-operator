b0964b3ce16ead75cec4d4266439f537b69165fe
Harshdeep Singh <38904804+super-harsh@users.noreply.github.com>
2024-05-29 13:52:47 +1200

    Auto-gen mutating-webhook, validation-webhook and manager-role files

--- scripts/v2/package-helm-manifest.sh
+++ scripts/v2/package-helm-manifest.sh
@@ -11,28 +11,34 @@ DIR=$4
 
 ASO_CHART="$DIR"charts/azure-service-operator
 GEN_FILES_DIR="$ASO_CHART"/templates/generated
+TEMP_DIR="$GEN_FILES_DIR"/temp
 IF_CLUSTER="{{- if or (eq .Values.multitenant.enable false) (eq .Values.azureOperatorMode \"webhooks\") }}"
-IF_TENANT="{{- if or (eq .Values.multitenant.enable false) (eq .Values.azureOperatorMode \"watchers\") }}"
-IF_CRDS="{{- if and (eq .Values.installCRDs true) (or (eq .Values.multitenant.enable false) (eq .Values.azureOperatorMode \"webhooks\")) }}"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-# Matches and adds helm flow control to a file
-flow_control(){
-  MATCHER_IF=$1
-  MATCHER_END=$2
-  IF_CLAUSE=$3
-  TARGET=$4
-
-  sed -i "/$MATCHER_IF/i \  \ $IF_CLAUSE" "$TARGET"
-  sed -i "/$MATCHER_END/a \  \ {{- end }}" "$TARGET"
-}
 
 echo "Updating helm chart manifest"
 sed -i "s@\($PUBLIC_REGISTRY\)\(.*\)@\1azureserviceoperator:$VERSION@g" "$ASO_CHART"/values.yaml
 
 # Chart replacements
 sed -i "s/\(version: \)\(.*\)/\1${VERSION//v}/g" "$ASO_CHART"/Chart.yaml  # find version key and update the value with the current version
+
+mkdir "$TEMP_DIR"
+${SCRIPT_DIR}/kustomize-build.sh -v "$VERSION" -k operator -o "$TEMP_DIR"
+
+echo "Making sed replacements and copying generated yamls"
+for file in $(find "$TEMP_DIR" -type f)
+do
+  # Append cluster or tenant guards to each file
+  if [[ $file == *"mutating-webhook-configuration"* ]] ||
+     [[ $file == *"validating-webhook-configuration"* ]] ||
+     [[ $file == *"azureserviceoperator-manager-role.yaml" ]] ; then
+        sed -i "1 s/^/$IF_CLUSTER\n/;$ a {{- end }}" "$file"
+        sed -i 's/azureserviceoperator-system/{{ .Release.Namespace }}/g' "$file"
+        mv -f "$file" "$GEN_FILES_DIR"
+  fi
+done
+
+rm -rf "$TEMP_DIR"
 
 # Helm chart packaging, indexing and updating dependencies
 echo "Packaging helm charts"
