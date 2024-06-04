f2dd4c8e47941292d58e1c7d0f0cf4f5ed035a57
Bevan Arps <bevan.arps@microsoft.com>
2024-06-05 10:33:11 +1200

    update generated files

--- v2/api/containerservice/v1api20230315preview/fleets_update_run_types_gen.go
+++ v2/api/containerservice/v1api20230315preview/fleets_update_run_types_gen.go
@@ -1586,9 +1586,7 @@ type ManagedClusterUpgradeSpec struct {
 	KubernetesVersion *string `json:"kubernetesVersion,omitempty"`
 
 	// +kubebuilder:validation:Required
-	// Type: The upgrade type.
-	// Full requires the KubernetesVersion property to be set.
-	// NodeImageOnly requires the KubernetesVersion property not to be set.
+	// Type: ManagedClusterUpgradeType is the type of upgrade to be applied.
 	Type *ManagedClusterUpgradeType `json:"type,omitempty"`
 }
 
@@ -1712,9 +1710,7 @@ type ManagedClusterUpgradeSpec_STATUS struct {
 	// KubernetesVersion: The Kubernetes version to upgrade the member clusters to.
 	KubernetesVersion *string `json:"kubernetesVersion,omitempty"`
 
-	// Type: The upgrade type.
-	// Full requires the KubernetesVersion property to be set.
-	// NodeImageOnly requires the KubernetesVersion property not to be set.
+	// Type: ManagedClusterUpgradeType is the type of upgrade to be applied.
 	Type *ManagedClusterUpgradeType_STATUS `json:"type,omitempty"`
 }
 
