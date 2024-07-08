359e96538ba5882b7c0ecac4c3982ebffe9b8929
Matthew Christopher <matthchr@users.noreply.github.com>
2024-07-08 08:52:13 -0700

    Fix bug where 'none' crd-management mode blocked pod start (#4151)
    This happened because in 'none' mode goalCRDs was the set of
    installed CRDs, but we built an exclusion list from that set and used it
    to filter the set of known storage types from the scheme. When
    count(existing) = 5 and count(scheme)=200, the most you can possibly
    filter from the scheme with an exclusion list is 5 which leaves 195 CRDs
    being mistakenly installed.
    
    Fixes #4146.

---
+++ v2/internal/crdmanagement/helpers_test.go
@@ -0,0 +1,140 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package crdmanagement_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/go-logr/logr"
+	. "github.com/onsi/gomega"
+	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/Azure/azure-service-operator/v2/api"
+	"github.com/Azure/azure-service-operator/v2/internal/config"
+	"github.com/Azure/azure-service-operator/v2/internal/controllers"
+	"github.com/Azure/azure-service-operator/v2/internal/crdmanagement"
+	"github.com/Azure/azure-service-operator/v2/internal/reconcilers/arm"
+	"github.com/Azure/azure-service-operator/v2/internal/reconcilers/generic"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
+	"github.com/Azure/azure-service-operator/v2/internal/util/kubeclient"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/registration"
+)
+
+type schemer struct {
+	scheme *runtime.Scheme
+}
+
+func (s *schemer) GetScheme() *runtime.Scheme {
+	return s.scheme
+}
+
+// This test requires that the task target `bundle-crds` has been run
+func Test_AllCRDsReady_NoneAreFiltered(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testData := testSetup(t)
+
+	// load crds
+	goalCRDs, err := testData.crdManager.LoadOperatorCRDs(testData.crdPath, testData.namespace)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	readyResources := crdmanagement.MakeCRDMap(goalCRDs)
+
+	knownTypes, err := testData.getKnownStorageTypes()
+	g.Expect(err).ToNot(HaveOccurred())
+
+	// Filter the types to register
+	objs, err := crdmanagement.FilterStorageTypesByReadyCRDs(testData.logger, testData.s.GetScheme(), readyResources, knownTypes)
+	g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(objs).To(Equal(knownTypes))
+}
+
+// This test requires that the task target `bundle-crds` has been run
+func Test_FiveCRDsReady_AllOthersAreFiltered(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	testData := testSetup(t)
+
+	// load crds
+	goalCRDs, err := testData.crdManager.LoadOperatorCRDs(testData.crdPath, testData.namespace)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	// Filter all but the first 5 CRDs
+	filteredGoalCRDs := make([]apiextensions.CustomResourceDefinition, 0, 5)
+	for i, goalCRD := range goalCRDs {
+		if i >= 5 {
+			break
+		}
+		filteredGoalCRDs = append(filteredGoalCRDs, goalCRD)
+	}
+
+	readyResources := crdmanagement.MakeCRDMap(filteredGoalCRDs)
+
+	knownTypes, err := testData.getKnownStorageTypes()
+	g.Expect(err).ToNot(HaveOccurred())
+
+	// Filter the types to register
+	objs, err := crdmanagement.FilterStorageTypesByReadyCRDs(testData.logger, testData.s.GetScheme(), readyResources, knownTypes)
+	g.Expect(err).ToNot(HaveOccurred())
+	g.Expect(objs).To(HaveLen(5))
+}
+
+/*
+ * Helpers
+ */
+
+func NewFakeKubeClient(s *runtime.Scheme) kubeclient.Client {
+	fakeClient := fake.NewClientBuilder().WithScheme(s).Build()
+	return kubeclient.NewClient(fakeClient)
+}
+
+type testData struct {
+	cfg        config.Values
+	s          controllers.Schemer
+	kubeClient kubeclient.Client
+	logger     logr.Logger
+	crdManager *crdmanagement.Manager
+	crdPath    string
+	namespace  string
+}
+
+func testSetup(t *testing.T) *testData {
+	asoScheme := api.CreateScheme()
+	s := &schemer{scheme: asoScheme}
+
+	kubeClient := NewFakeKubeClient(asoScheme)
+	logger := testcommon.NewTestLogger(t)
+	cfg := config.Values{}
+
+	crdManager := crdmanagement.NewManager(logger, kubeClient)
+
+	return &testData{
+		cfg:        cfg,
+		s:          s,
+		kubeClient: kubeClient,
+		logger:     logger,
+		crdManager: crdManager,
+		crdPath:    "../../out/crds",
+		namespace:  "azureserviceoperator-system",
+	}
+}
+
+func (t *testData) getKnownStorageTypes() ([]*registration.StorageType, error) {
+	return controllers.GetKnownStorageTypes(
+		t.s,
+		func(ctx context.Context, object genruntime.ARMMetaObject) (arm.Connection, error) {
+			// For the purposes of this test, we're not really going to go to ARM so we can just do nothing here
+			return nil, nil
+		},
+		nil, // Not used for this test
+		t.kubeClient,
+		nil, // Not used for this test
+		generic.Options{})
+}
