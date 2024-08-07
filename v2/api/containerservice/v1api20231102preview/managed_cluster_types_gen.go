22af13368be82c4aa5a618375a1ce766b545f31e
Bevan Arps <bevan.arps@microsoft.com>
2024-08-07 15:42:49 +1200

    Update generated types

--- v2/api/containerservice/v1api20231102preview/managed_cluster_types_gen.go
+++ v2/api/containerservice/v1api20231102preview/managed_cluster_types_gen.go
@@ -14317,7 +14317,11 @@ func (profile *ManagedClusterSecurityProfile) ConvertToARM(resolved genruntime.C
 	}
 
 	// Set property "CustomCATrustCertificates":
-	result.CustomCATrustCertificates = profile.CustomCATrustCertificates
+	var customCATrustCertificatesTemp []string
+	for _, item := range profile.CustomCATrustCertificates {
+		customCATrustCertificatesTemp = append(customCATrustCertificatesTemp, item)
+	}
+	result.CustomCATrustCertificates = customCATrustCertificatesTemp
 
 	// Set property "Defender":
 	if profile.Defender != nil {
@@ -14395,7 +14399,11 @@ func (profile *ManagedClusterSecurityProfile) PopulateFromARM(owner genruntime.A
 	}
 
 	// Set property "CustomCATrustCertificates":
-	profile.CustomCATrustCertificates = typedInput.CustomCATrustCertificates
+	var customCATrustCertificatesTemp []string
+	for _, item := range typedInput.CustomCATrustCertificates {
+		customCATrustCertificatesTemp = append(customCATrustCertificatesTemp, item)
+	}
+	profile.CustomCATrustCertificates = ManagedClusterSecurityProfileCustomCATrustCertificates(customCATrustCertificatesTemp)
 
 	// Set property "Defender":
 	if typedInput.Defender != nil {
