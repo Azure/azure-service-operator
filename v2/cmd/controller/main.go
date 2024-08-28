8d84cc96d756756330a3ac071e249bc9f174fe39
Bevan Arps <bevan.arps@microsoft.com>
2024-08-28 14:43:23 +1200

    Refactor main

--- v2/cmd/controller/main.go
+++ v2/cmd/controller/main.go
@@ -6,31 +6,45 @@ Licensed under the MIT license.
 package main
 
 import (
+	"flag"
 	"os"
 
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"k8s.io/klog/v2/klogr"
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/Azure/azure-service-operator/v2/cmd/controller/app"
+	"github.com/Azure/azure-service-operator/v2/cmd/controller/logging"
+	"github.com/Azure/azure-service-operator/v2/internal/version"
 )
 
 func main() {
-	setupLog := ctrl.Log.WithName("setup")
-	ctrl.SetLogger(klogr.New()) //nolint: staticcheck
+	// Set up to parse command line flags
+	exeName := os.Args[0] + " " + version.BuildVersion
+	flagSet := flag.NewFlagSet(exeName, flag.ExitOnError)
+
+	// Create a temporary logger for while we get set up
+	log := logging.Create()
+
 	ctx := ctrl.SetupSignalHandler()
 
-	flgs, err := app.ParseFlags(os.Args)
+	// Add application and logging flags
+	flgs := app.InitFlags(flagSet)
+	logging.InitFlags(flagSet)
+	err := flagSet.Parse(os.Args[1:])
 	if err != nil {
-		setupLog.Error(err, "failed to parse cmdline flags")
+		log.Error(err, "failed to parse cmdline flags")
 		os.Exit(1)
 	}
 
-	setupLog.Info("Launching with flags", "flags", flgs.String())
-	mgr := app.SetupControllerManager(ctx, setupLog, flgs)
-	setupLog.Info("starting manager")
+	// Replace the logger with a configured one
+	log = logging.Create()
+	ctrl.SetLogger(log)
+	log.Info("Launching with flags", "flags", flgs.String())
+
+	mgr := app.SetupControllerManager(ctx, log, flgs)
+	log.Info("starting manager")
 	if err = mgr.Start(ctx); err != nil {
-		setupLog.Error(err, "failed to start manager")
+		log.Error(err, "failed to start manager")
 		os.Exit(1)
 	}
 }
