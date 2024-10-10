f209def693ddac4144e63a0d42074c178ea38d0f
Matthew Christopher <matthchr@users.noreply.github.com>
2024-10-09 17:00:10 -0700

    Fix bug where some ConfigMap and Secret references could be missed
    The ASO controller uses reflection to look through the resource and find
    the ConfigMapReference and SecretReference fields. This reflection
    could miss SecretReference and ConfigMapReference fields that had been
    serialized into the "PropertyBag" property on the storage type. This was
    most common when working with preview APIs that had added new
    ConfigMapReference or SecretReference fields that don't exist in the GA
    api version.
    
    The fix is to use the converted version for reflection-based discovery.
    
    Fixes #4298.
    Fixes #4316.

--- v2/internal/testcommon/scheme.go
+++ v2/internal/testcommon/scheme.go
@@ -6,22 +6,37 @@ Licensed under the MIT license.
 package testcommon
 
 import (
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	batch "github.com/Azure/azure-service-operator/v2/api/batch/v1api20210101"
+	batchstorage "github.com/Azure/azure-service-operator/v2/api/batch/v1api20210101/storage"
+	dbforpostgresqlstorage "github.com/Azure/azure-service-operator/v2/api/dbforpostgresql/v1api20221201/storage"
+	dbforpostgresql "github.com/Azure/azure-service-operator/v2/api/dbforpostgresql/v1api20230601preview"
 	resources "github.com/Azure/azure-service-operator/v2/api/resources/v1api20200601"
+	resourcesstorage "github.com/Azure/azure-service-operator/v2/api/resources/v1api20200601/storage"
 )
 
 func CreateScheme() (*runtime.Scheme, error) {
 	scheme := runtime.NewScheme()
-	err := batch.AddToScheme(scheme)
-	if err != nil {
-		return nil, err
+
+	addToScheme := []func(scheme *runtime.Scheme) error{
+		batch.AddToScheme,
+		batchstorage.AddToScheme,
+		resources.AddToScheme,
+		resourcesstorage.AddToScheme,
+		// These dbforpostgresql resources are an example that illustrates
+		// https://github.com/Azure/azure-service-operator/issues/4316
+		dbforpostgresql.AddToScheme,
+		dbforpostgresqlstorage.AddToScheme,
+		v1.AddToScheme,
 	}
 
-	err = resources.AddToScheme(scheme)
-	if err != nil {
-		return nil, err
+	for _, f := range addToScheme {
+		err := f(scheme)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return scheme, nil
