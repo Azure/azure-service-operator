a389d1bec9ec2385cedd57b4e365f1ad3d63afd5
Harshdeep Singh <38904804+super-harsh@users.noreply.github.com>
2024-10-10 15:09:25 +0530

    Add test

--- v2/internal/controllers/machinelearningservices_registry_20240401_test.go
+++ v2/internal/controllers/machinelearningservices_registry_20240401_test.go
@@ -1,1 +1,37 @@
-package controllers
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+package controllers_test
+
+import (
+	"testing"
+
+	machinelearningservices "github.com/Azure/azure-service-operator/v2/api/machinelearningservices/v1api20240401"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
+	"github.com/Azure/azure-service-operator/v2/internal/util/to"
+)
+
+func Test_MachineLearning_Registry_20240401_CRUD(t *testing.T) {
+	t.Parallel()
+
+	tc := globalTestContext.ForTest(t)
+	rg := tc.CreateTestResourceGroupAndWait()
+
+	registry := &machinelearningservices.Registry{
+		ObjectMeta: tc.MakeObjectMetaWithName(tc.NoSpaceNamer.GenerateName("registry")),
+		Spec: machinelearningservices.Registry_Spec{
+			DiscoveryUrl: nil,
+			Identity: &machinelearningservices.ManagedServiceIdentity{
+				Type: to.Ptr(machinelearningservices.ManagedServiceIdentityType_None),
+			},
+			Location: tc.AzureRegion,
+			Owner:    testcommon.AsOwner(rg),
+			Sku: &machinelearningservices.Sku{
+				Name: to.Ptr("P3"),
+				Tier: to.Ptr(machinelearningservices.SkuTier_Free),
+			},
+		},
+	}
+
+	tc.CreateResourceAndWait(registry)
+}
