efa19bc67dac55f9bba3446a0cf162a27ab461f3
Bevan Arps <bevan.arps@microsoft.com>
2024-10-16 10:53:08 +1300

    Update version of Go (#4347)

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -104,7 +104,7 @@ GOVER=$(go version)
 write-info "Go version: ${GOVER[*]}"
 
 GOVERREGEX=".*go1.([0-9]+).([0-9]+).*"
-GOVERREQUIRED="go1.22.*"
+GOVERREQUIRED="go1.23.*"
 GOVERACTUAL=$(go version | { read _ _ ver _; echo "$ver"; })
 
 if ! [[ $GOVERACTUAL =~ $GOVERREGEX ]]; then
