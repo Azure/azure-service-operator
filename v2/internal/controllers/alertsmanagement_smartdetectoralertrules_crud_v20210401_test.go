14ba87d4c9e0876fcbf210f6fa68acbe83276845
Bingi Narasimha Karthik <bkarthik@adobe.com>
2024-11-04 15:27:35 +0530

    Add ActionGroups and Detector mandatory fields

--- v2/internal/controllers/alertsmanagement_smartdetectoralertrules_crud_v20210401_test.go
+++ v2/internal/controllers/alertsmanagement_smartdetectoralertrules_crud_v20210401_test.go
@@ -12,6 +12,7 @@ import (
 	. "github.com/onsi/gomega"
 
 	alertsmanagement "github.com/Azure/azure-service-operator/v2/api/alertsmanagement/v1api20210401"
+	insights "github.com/Azure/azure-service-operator/v2/api/insights/v1api20230101"
 	monitor "github.com/Azure/azure-service-operator/v2/api/monitor/v1api20230403"
 	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
 	"github.com/Azure/azure-service-operator/v2/internal/util/to"
@@ -32,19 +33,40 @@ func Test_AlertsManagement_SmartDetectorAlertRules_CRUD(t *testing.T) {
 			Owner:    testcommon.AsOwner(rg),
 		},
 	}
+	ag := &insights.ActionGroup{
+		ObjectMeta: tc.MakeObjectMeta("actiongroup"),
+		Spec: insights.ActionGroup_Spec{
+			Enabled:        to.Ptr(true),
+			GroupShortName: to.Ptr("actiongroup"),
+			Location:       tc.AzureRegion,
+			Owner:          testcommon.AsOwner(rg),
+		},
+	}
 
 	// Create a Smart Detector alert rule
 	// This was adapted from https://learn.microsoft.com/en-us/rest/api/monitor/smart-detector-alert-rules/create-or-update?view=rest-monitor-2019-06-01&tabs=HTTP#create-or-update-a-smart-detector-alert-rule
 	state := alertsmanagement.AlertRuleProperties_State("Enabled")
 	severity := alertsmanagement.AlertRuleProperties_Severity("Sev3")
+	detectorId := &alertsmanagement.Detector{
+		Reference: &genruntime.ResourceReference{
+			ARMID: "VMMemoryLeak",
+		},
+	}
+	actionGroup := &alertsmanagement.ActionGroupsInformation{
+		GroupReferences: []genruntime.ResourceReference{
+			*tc.MakeReferenceFromResource(ag),
+		},
+	}
 	alertRule := &alertsmanagement.SmartDetectorAlertRule{
 		ObjectMeta: tc.MakeObjectMeta("smartalertrules"),
 		Spec: alertsmanagement.SmartDetectorAlertRule_Spec{
-			Location:  tc.AzureRegion,
-			Owner:     testcommon.AsOwner(rg),
-			State:     &state,
-			Severity:  &severity,
-			Frequency: to.Ptr("PT1M"),
+			Location:     tc.AzureRegion,
+			Owner:        testcommon.AsOwner(rg),
+			State:        &state,
+			Severity:     &severity,
+			Frequency:    to.Ptr("PT1M"),
+			Detector:     detectorId,
+			ActionGroups: actionGroup,
 			ScopeReferences: []genruntime.ResourceReference{
 				*tc.MakeReferenceFromResource(acct),
 			},
