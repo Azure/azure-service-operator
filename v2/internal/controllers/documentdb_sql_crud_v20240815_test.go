eb0f4871823fbd71fec93cab597510b213a096de
Petre Popescu <petre.popescu@uipath.com>
2025-02-03 16:22:13 +0000

    Add azureName to cosmosdb sql test & sample

--- v2/internal/controllers/documentdb_sql_crud_v20240815_test.go
+++ v2/internal/controllers/documentdb_sql_crud_v20240815_test.go
@@ -404,11 +404,15 @@ func CosmosDB_SQL_RoleAssignment_v20240815_CRUD(tc *testcommon.KubePerTestContex
 		rg.AzureName(),
 		acct.AzureName())
 
+	// AzureName is required and it has to be a GUID
+	azureName := "00000000-0000-0000-0000-000000000000"
+
 	// Declare a role assignment
 	roleAssignment := &documentdb.SqlRoleAssignment{
 		ObjectMeta: tc.MakeObjectMeta("roleassignment"),
 		Spec: documentdb.SqlRoleAssignment_Spec{
-			Owner: testcommon.AsOwner(acct),
+			AzureName: azureName,
+			Owner:     testcommon.AsOwner(acct),
 			PrincipalIdFromConfig: &genruntime.ConfigMapReference{
 				Name: configMapName,
 				Key:  principalIdKey,
