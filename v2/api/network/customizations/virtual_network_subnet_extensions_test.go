fa4f7398a0a65a19a0bf5a1a7c3a67b892938c32
Bevan Arps <bevan.arps@microsoft.com>
2025-09-09 21:56:23 +0000

    Format code for copilot

--- v2/api/network/customizations/virtual_network_subnet_extensions_test.go
+++ v2/api/network/customizations/virtual_network_subnet_extensions_test.go
@@ -8,8 +8,9 @@ package customizations
 import (
 	"testing"
 
-	"github.com/go-logr/logr"
 	. "github.com/onsi/gomega"
+
+	"github.com/go-logr/logr"
 
 	"github.com/Azure/azure-service-operator/v2/internal/genericarmclient"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/core"
