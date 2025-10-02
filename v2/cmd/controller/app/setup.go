Azure/azure-service-operator

commit 98a2df76e21c03a9da755c9364d991043ab8fc7d

Set ReaderFailOnMissingInformer (#4857)
This is a safety feature to protect against starting unexpected watches.

v2/cmd/controller/app/setup.go
@@ -81,22 +81,21 @@ func SetupControllerManager(ctx context.Context, setupLog logr.Logger, flgs *Fla
 		os.Exit(1)
 	}
 
-	var cacheFunc cache.NewCacheFunc
+	cacheOpts := cache.Options{
+		// This will make sure that if we try to read an object that is not cached, we will fail rather than start a new informer
+		ReaderFailOnMissingInformer: true,
+	}
 	if cfg.TargetNamespaces != nil && cfg.OperatorMode.IncludesWatchers() {
-		cacheFunc = func(config *rest.Config, opts cache.Options) (cache.Cache, error) {
-			opts.DefaultNamespaces = make(map[string]cache.Config, len(cfg.TargetNamespaces))
-			for _, ns := range cfg.TargetNamespaces {
-				opts.DefaultNamespaces[ns] = cache.Config{}
-			}
-
-			return cache.New(config, opts)
+		cacheOpts.DefaultNamespaces = make(map[string]cache.Config, len(cfg.TargetNamespaces))
+		for _, ns := range cfg.TargetNamespaces {
+			cacheOpts.DefaultNamespaces[ns] = cache.Config{}
 		}
 	}
 
 	k8sConfig := ctrl.GetConfigOrDie()
 	ctrlOptions := ctrl.Options{
 		Scheme:           scheme,
-		NewCache:         cacheFunc,
+		Cache:            cacheOpts,
 		LeaderElection:   flgs.EnableLeaderElection,
 		LeaderElectionID: "controllers-leader-election-azinfra-generated",
 		// Manually set lease duration (to default) so that we can use it for our leader elector too.

v2/internal/testcommon/kube_test_context_envtest.go
@@ -113,15 +113,14 @@ func createSharedEnvTest(cfg testConfig, namespaceResources *namespaceResources)
 		}
 	}
 
-	var cacheFunc cache.NewCacheFunc
+	cacheOpts := cache.Options{
+		// This will make sure that if we try to read an object that is not cached, we will fail rather than start a new informer
+		ReaderFailOnMissingInformer: true,
+	}
 	if cfg.TargetNamespaces != nil && cfg.OperatorMode.IncludesWatchers() {
-		cacheFunc = func(config *rest.Config, opts cache.Options) (cache.Cache, error) {
-			opts.DefaultNamespaces = make(map[string]cache.Config, len(cfg.TargetNamespaces))
-			for _, ns := range cfg.TargetNamespaces {
-				opts.DefaultNamespaces[ns] = cache.Config{}
-			}
-
-			return cache.New(config, opts)
+		cacheOpts.DefaultNamespaces = make(map[string]cache.Config, len(cfg.TargetNamespaces))
+		for _, ns := range cfg.TargetNamespaces {
+			cacheOpts.DefaultNamespaces[ns] = cache.Config{}
 		}
 	}
 
@@ -150,7 +149,7 @@ func createSharedEnvTest(cfg testConfig, namespaceResources *namespaceResources)
 			options.Cache = &client.CacheOptions{}
 			return NewTestClient(config, options)
 		},
-		NewCache: cacheFunc,
+		Cache: cacheOpts,
 		Metrics: server.Options{
 			BindAddress: "0", // disable serving metrics, or else we get conflicts listening on same port 8080
 		},

commit 3e1789f9de76fb51eb6643d490d8b1f79bb22ae7

Fix bug preventing resource reconciliation due to "Secret not cached"
This was caused by PR #4857 setting ReaderFailOnMissingInformer. It
happens only if the CRDs installed via crdPattern do not include a resource
which requires a Secret informer.

This issue was missed in our testing because we generally run with a
full (or close to full) set of CRDs installed, but the problem only
appears with a minimal subset installed.

Fixes #4926.

v2/cmd/controller/app/setup.go
@@ -416,7 +416,6 @@ func initializeClients(cfg config.Values, mgr ctrl.Manager) (*clients, error) {
 
 	armClientCache := armreconciler.NewARMClientCache(
 		credentialProvider,
-		kubeClient,
 		cfg.Cloud(),
 		nil,
 		armMetrics)

v2/internal/reconcilers/arm/arm_client_cache.go
@@ -15,7 +15,6 @@ import (
 	"github.com/Azure/azure-service-operator/v2/internal/genericarmclient"
 	"github.com/Azure/azure-service-operator/v2/internal/identity"
 	"github.com/Azure/azure-service-operator/v2/internal/metrics"
-	"github.com/Azure/azure-service-operator/v2/internal/util/kubeclient"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -26,14 +25,12 @@ type ARMClientCache struct {
 	clients            map[string]*armClient
 	cloudConfig        cloud.Configuration
 	credentialProvider identity.CredentialProvider
-	kubeClient         kubeclient.Client
 	httpClient         *http.Client
 	armMetrics         *metrics.ARMClientMetrics
 }
 
 func NewARMClientCache(
 	credentialProvider identity.CredentialProvider,
-	kubeClient kubeclient.Client,
 	configuration cloud.Configuration,
 	httpClient *http.Client,
 	armMetrics *metrics.ARMClientMetrics,
@@ -42,7 +39,6 @@ func NewARMClientCache(
 		lock:               sync.Mutex{},
 		clients:            make(map[string]*armClient),
 		cloudConfig:        configuration,
-		kubeClient:         kubeClient,
 		credentialProvider: credentialProvider,
 		httpClient:         httpClient,
 		armMetrics:         armMetrics,

v2/internal/reconcilers/generic/register.go
@@ -22,6 +22,7 @@ import (
 	kerrors "k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	ctrlbuilder "sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
@@ -98,6 +99,21 @@ func RegisterAll(
 				return eris.Wrapf(err, "failed to register indexer for %T, Key: %q", obj.Obj, indexer.Key)
 			}
 		}
+	}
+
+	// Start informer for secrets. Not all ASO resources have registered for events from secrets, but we always need the ability
+	// to read secrets as at the very least we need to check for the aso-credential secret at the root of the resource namespace.
+	// This ensures the cache is populated since ReaderFailOnMissingInformer is true.
+	secretGVK := schema.GroupVersionKind{
+		Group:   "",
+		Version: "v1",
+		Kind:    "Secret",
+	}
+	mgr.GetLogger().V(Info).Info("Registering informer for type", "type", secretGVK.String())
+	// We don't need to block until synced, we just want to make sure the informer is going to start
+	_, err := mgr.GetCache().GetInformerForKind(context.Background(), secretGVK, cache.BlockUntilSynced(false))
+	if err != nil {
+		return eris.Wrapf(err, "failed to start informer for secrets")
 	}
 
 	var errs []error

v2/internal/testcommon/kube_test_context_envtest.go
@@ -543,7 +543,6 @@ func createEnvtestContext() (BaseTestContextFactory, context.CancelFunc) {
 			// register resources needed by controller for namespace
 			armClientCache := arm.NewARMClientCache(
 				credentialProvider,
-				envtest.KubeClient,
 				cfg.Cloud(),
 				perTestContext.HTTPClient,
 				metrics.NewARMClientMetrics())

v2/internal/reconcilers/arm/arm_client_cache_test.go
@@ -69,7 +69,7 @@ func NewTestARMClientCache(client kubeclient.Client) (*ARMClientCache, error) {
 		return nil, err
 	}
 
-	return NewARMClientCache(credentialProvider, client, cfg.Cloud(), nil, metrics.NewARMClientMetrics()), nil
+	return NewARMClientCache(credentialProvider, cfg.Cloud(), nil, metrics.NewARMClientMetrics()), nil
 }
 
 type testResources struct {
@@ -113,7 +113,7 @@ func Test_DefaultCredential_NotSet_ReturnsErrorWhenTryToUseGlobalCredential(t *t
 	g.Expect(err).To(BeNil())
 
 	providerWithNoDefaultCred := identity.NewCredentialProvider(nil, kubeClient, nil)
-	clientWithNoDefaultCred := NewARMClientCache(providerWithNoDefaultCred, kubeClient, cfg.Cloud(), nil, metrics.NewARMClientMetrics())
+	clientWithNoDefaultCred := NewARMClientCache(providerWithNoDefaultCred, cfg.Cloud(), nil, metrics.NewARMClientMetrics())
 
 	rg := newResourceGroup("")
 