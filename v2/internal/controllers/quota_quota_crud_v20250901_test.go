e7de2f1e28e1a3ea3a3fed422d9545ac8f536b88
Bingi Narasimha Karthik <bkarthik@adobe.com>
2025-10-17 21:47:12 +0530

    Updates

--- v2/internal/controllers/quota_quota_crud_v20250901_test.go
+++ v2/internal/controllers/quota_quota_crud_v20250901_test.go
@@ -6,50 +6,61 @@ Licensed under the MIT license.
 package controllers_test
 
 import (
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	quota "github.com/Azure/azure-service-operator/v2/api/quota/v1api20250901"
 	"github.com/Azure/azure-service-operator/v2/internal/util/to"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
-func Test_Quota_Quota_CRUD(t *testing.T) {
+func Test_Quota_Quota_v20250901_CRUD(t *testing.T) {
 	t.Parallel()
 	g := NewGomegaWithT(t)
 
 	tc := globalTestContext.ForTest(t)
 
-	// Create a Resource Group to own the Quota (since it's an extension resource)
-	rg := tc.CreateTestResourceGroupAndWait()
+	// Compute quotas must be scoped to provider-location, not ResourceGroup
+	providerLocationScope := fmt.Sprintf("/subscriptions/%s/providers/Microsoft.Compute/locations/%s", 
+		tc.AzureSubscription, *tc.AzureRegion)
 
 	quotaResource := &quota.Quota{
 		ObjectMeta: tc.MakeObjectMeta("asotest-quota"),
 		Spec: quota.Quota_Spec{
-			Owner: tc.AsExtensionOwner(rg),
+			AzureName: "cores", // Must match the quota name in properties
+			Owner: &genruntime.ArbitraryOwnerReference{
+				ARMID: providerLocationScope,
+			},
 			Properties: &quota.QuotaProperties{
 				Name: &quota.ResourceName{
 					Value: to.Ptr("cores"),
 				},
-				ResourceType: to.Ptr("cores"),
 				Limit: &quota.LimitJsonObject{
 					LimitValue: &quota.LimitObject{
 						Value:           to.Ptr(100),
 						LimitObjectType: to.Ptr(quota.LimitType_LimitValue),
+						LimitType:       to.Ptr(quota.QuotaLimitTypes_Independent), // Add missing limitType
 					},
 				},
 			},
 		},
 	}
 
-	tc.CreateResourceAndWait(quotaResource)
+	// Create the quota resource without registering it for cleanup
+	// since Azure Quota API doesn't support deletion
+	tc.CreateResourceAndWaitWithoutCleanup(quotaResource)
 
 	g.Expect(quotaResource.Status.Id).ToNot(BeNil())
 	g.Expect(quotaResource.Status.Name).ToNot(BeNil())
 	g.Expect(quotaResource.Status.Properties).ToNot(BeNil())
 	if quotaResource.Status.Properties != nil {
 		g.Expect(quotaResource.Status.Properties.Name).ToNot(BeNil())
-		g.Expect(quotaResource.Status.Properties.ResourceType).To(Equal(to.Ptr("cores")))
+		if quotaResource.Status.Properties.Name != nil {
+			g.Expect(quotaResource.Status.Properties.Name.Value).To(Equal(to.Ptr("cores")))
+		}
 	}
 
 	// Update the Quota limit
@@ -78,6 +89,13 @@ func Test_Quota_Quota_CRUD(t *testing.T) {
 	tc.Expect(err).ToNot(HaveOccurred())
 	tc.Expect(retryAfter).To(BeZero())
 	tc.Expect(exists).To(BeTrue()) // Quota should still exist
+
+	// Remove owner reference and finalizers to prevent deletion attempts
+	// since Azure Quota API doesn't support deletion
+	old = quotaResource.DeepCopy()
+	quotaResource.SetOwnerReferences([]metav1.OwnerReference{}) // Remove owner reference
+	quotaResource.SetFinalizers([]string{})                    // Remove finalizers
+	tc.Patch(old, quotaResource)
 }
 
 
