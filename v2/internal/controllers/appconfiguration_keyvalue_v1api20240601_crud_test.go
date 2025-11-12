d3fdc3e2a01c2fa374d3e853a5ee478308f31dcd
Bevan Arps <bevan.arps@microsoft.com>
2025-11-12 22:41:02 +0000

    Fix tests

--- v2/internal/controllers/appconfiguration_keyvalue_v1api20240601_crud_test.go
+++ v2/internal/controllers/appconfiguration_keyvalue_v1api20240601_crud_test.go
@@ -13,7 +13,6 @@ import (
 	appconfig "github.com/Azure/azure-service-operator/v2/api/appconfiguration/v1api20240601"
 	"github.com/Azure/azure-service-operator/v2/internal/testcommon"
 	"github.com/Azure/azure-service-operator/v2/internal/util/to"
-	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
 func Test_AppConfiguration_KeyValue_v1api20240601_CRUD(t *testing.T) {
@@ -26,7 +25,7 @@ func Test_AppConfiguration_KeyValue_v1api20240601_CRUD(t *testing.T) {
 	tc := globalTestContext.ForTest(t)
 
 	rg := tc.CreateTestResourceGroupAndWait()
-	
+
 	// Create ConfigurationStore
 	cs := &appconfig.ConfigurationStore{
 		ObjectMeta: tc.MakeObjectMeta("confstore"),
@@ -40,7 +39,7 @@ func Test_AppConfiguration_KeyValue_v1api20240601_CRUD(t *testing.T) {
 			PublicNetworkAccess: to.Ptr(appconfig.ConfigurationStoreProperties_PublicNetworkAccess_Enabled),
 		},
 	}
-	
+
 	tc.CreateResourceAndWait(cs)
 
 	tc.RunParallelSubtests(
@@ -73,9 +72,9 @@ func AppConfiguration_KeyValue_v1api20240601_CRUD(tc *testcommon.KubePerTestCont
 	keyValue := &appconfig.KeyValue{
 		ObjectMeta: tc.MakeObjectMetaWithName(keyValueName),
 		Spec: appconfig.KeyValue_Spec{
-			AzureName: to.Ptr("MyApp:Settings:TestKey"),
-			Owner:     testcommon.AsOwner(cs),
-			Value:     to.Ptr("TestValue"),
+			AzureName:   "MyApp:Settings:TestKey",
+			Owner:       testcommon.AsOwner(cs),
+			Value:       to.Ptr("TestValue"),
 			ContentType: to.Ptr("text/plain"),
 			Tags: map[string]string{
 				"Environment": "Test",
@@ -83,23 +82,23 @@ func AppConfiguration_KeyValue_v1api20240601_CRUD(tc *testcommon.KubePerTestCont
 			},
 		},
 	}
-	
+
 	tc.CreateResourceAndWait(keyValue)
-	
+
 	armId := *keyValue.Status.Id
 	tc.Expect(keyValue.Status.Key).ToNot(BeNil())
 	tc.Expect(*keyValue.Status.Key).To(Equal("MyApp:Settings:TestKey"))
 	tc.Expect(keyValue.Status.Value).ToNot(BeNil())
 	tc.Expect(*keyValue.Status.Value).To(Equal("TestValue"))
-	
+
 	// Update the value
 	old := keyValue.DeepCopy()
 	keyValue.Spec.Value = to.Ptr("UpdatedTestValue")
 	tc.PatchResourceAndWait(old, keyValue)
 	tc.Expect(*keyValue.Status.Value).To(Equal("UpdatedTestValue"))
-	
+
 	tc.DeleteResourceAndWait(keyValue)
-	
+
 	// Ensure that the resource was really deleted in Azure
 	exists, _, err := tc.AzureClient.CheckExistenceWithGetByID(
 		tc.Ctx,
@@ -116,22 +115,22 @@ func AppConfiguration_Replica_v1api20240601_CRUD(tc *testcommon.KubePerTestConte
 	replica := &appconfig.Replica{
 		ObjectMeta: tc.MakeObjectMetaWithName(replicaName),
 		Spec: appconfig.Replica_Spec{
-			AzureName: to.Ptr("eastus2-replica"),
+			AzureName: "eastus2-replica",
 			Owner:     testcommon.AsOwner(cs),
 			Location:  to.Ptr("eastus2"),
 		},
 	}
-	
+
 	tc.CreateResourceAndWait(replica)
-	
+
 	armId := *replica.Status.Id
 	tc.Expect(replica.Status.Location).ToNot(BeNil())
 	tc.Expect(*replica.Status.Location).To(Equal("eastus2"))
 	tc.Expect(replica.Status.Endpoint).ToNot(BeNil())
 	tc.Expect(replica.Status.ProvisioningState).ToNot(BeNil())
-	
+
 	tc.DeleteResourceAndWait(replica)
-	
+
 	// Ensure that the resource was really deleted in Azure
 	exists, _, err := tc.AzureClient.CheckExistenceWithGetByID(
 		tc.Ctx,
@@ -148,31 +147,31 @@ func AppConfiguration_Snapshot_v1api20240601_CRUD(tc *testcommon.KubePerTestCont
 	snapshot := &appconfig.Snapshot{
 		ObjectMeta: tc.MakeObjectMetaWithName(snapshotName),
 		Spec: appconfig.Snapshot_Spec{
-			AzureName: to.Ptr("test-snapshot"),
-			Owner:     testcommon.AsOwner(cs),
+			AzureName:       "test-snapshot",
+			Owner:           testcommon.AsOwner(cs),
 			CompositionType: to.Ptr(appconfig.SnapshotProperties_CompositionType_Key),
 			Filters: []appconfig.KeyValueFilter{
 				{
 					Key: to.Ptr("MyApp:*"),
 				},
 			},
-			RetentionPeriod: to.Ptr("P30D"),
+			RetentionPeriod: to.Ptr(60 * 60 * 24 * 30), // 30 days
 			Tags: map[string]string{
 				"Environment": "Test",
 				"Purpose":     "Testing",
 			},
 		},
 	}
-	
+
 	tc.CreateResourceAndWait(snapshot)
-	
+
 	armId := *snapshot.Status.Id
 	tc.Expect(snapshot.Status.CompositionType).ToNot(BeNil())
 	tc.Expect(snapshot.Status.ProvisioningState).ToNot(BeNil())
 	tc.Expect(snapshot.Status.ItemsCount).ToNot(BeNil())
-	
+
 	tc.DeleteResourceAndWait(snapshot)
-	
+
 	// Ensure that the resource was really deleted in Azure
 	exists, _, err := tc.AzureClient.CheckExistenceWithGetByID(
 		tc.Ctx,
