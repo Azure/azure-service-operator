7453010732e00cffba561ac23354a948e7af074c
Matthew Christopher <matthchr@users.noreply.github.com>
2025-12-02 19:23:11 -0800

    Remove deprecated Fleet 2023-03-15-preview API version (#5064)

--- v2/api/containerservice/customizations/fleets_member_extension_types_gen.go
+++ v2/api/containerservice/customizations/fleets_member_extension_types_gen.go
@@ -4,10 +4,8 @@
 package customizations
 
 import (
-	v20230315p "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230315preview"
-	v20230315ps "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20230315preview/storage"
 	v20250301 "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20250301"
-	v20250301s "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20250301/storage"
+	storage "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20250301/storage"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 )
 
@@ -17,8 +15,6 @@ type FleetsMemberExtension struct {
 // GetExtendedResources Returns the KubernetesResource slice for Resource versions
 func (extension *FleetsMemberExtension) GetExtendedResources() []genruntime.KubernetesResource {
 	return []genruntime.KubernetesResource{
-		&v20230315p.FleetsMember{},
-		&v20230315ps.FleetsMember{},
 		&v20250301.FleetsMember{},
-		&v20250301s.FleetsMember{}}
+		&storage.FleetsMember{}}
 }
