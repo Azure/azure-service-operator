251fdcc305580262f26c5b8253ae304ecbf4bcbe
Bevan Arps <bevan.arps@microsoft.com>
2026-01-13 15:34:31 +1300

    Create v4 of our wrapper

---
+++ v2/internal/testcommon/vcr/v4/replay_roundtripper_test.go
@@ -0,0 +1,240 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package v4
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/go-logr/logr"
+	"gopkg.in/dnaeon/go-vcr.v4/pkg/cassette"
+
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon/creds"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon/vcr"
+)
+
+func TestReplayRoundTripperRoundTrip_GivenSingleGET_ReturnsMultipleTimes(t *testing.T) {
+	t.Parallel()
+
+	// Arrange
+	req := &http.Request{
+		URL:    &url.URL{Path: "/foo"},
+		Method: http.MethodGet,
+		Body:   io.NopCloser(strings.NewReader("GET body goes here")),
+	}
+
+	resp := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader("GET response goes here")),
+	}
+
+	fake := vcr.NewFakeRoundTripper(cassette.ErrInteractionNotFound)
+	fake.AddResponse(req, resp)
+
+	redactor := vcr.NewRedactor(creds.DummyAzureIDs())
+
+	// Act
+	replayer := NewReplayRoundTripper(fake, logr.Discard(), redactor)
+
+	// Assert - first request works
+	assertExpectedResponse(t, replayer, req, 200, "GET response goes here")
+
+	// Assert - second request works by replaying the first
+	assertExpectedResponse(t, replayer, req, 200, "GET response goes here")
+
+	// Assert - third request works by replaying the first
+	assertExpectedResponse(t, replayer, req, 200, "GET response goes here")
+}
+
+func TestReplayRoundTripperRoundTrip_GivenSinglePut_ReturnsOnceExtra(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	// Arrange
+	req := &http.Request{
+		URL:    &url.URL{Path: "/foo"},
+		Method: http.MethodPut,
+		Body:   io.NopCloser(strings.NewReader("PUT body goes here")),
+	}
+
+	resp := &http.Response{
+		StatusCode: 200,
+		Body:       io.NopCloser(strings.NewReader("PUT response goes here")),
+	}
+
+	fake := vcr.NewFakeRoundTripper(cassette.ErrInteractionNotFound)
+	fake.AddResponse(req, resp)
+
+	redactor := vcr.NewRedactor(creds.DummyAzureIDs())
+
+	// Act
+	replayer := NewReplayRoundTripper(fake, logr.Discard(), redactor)
+
+	// Assert - first request works
+	assertExpectedResponse(t, replayer, req, 200, "PUT response goes here")
+
+	// Assert - second request works by replaying the first
+	assertExpectedResponse(t, replayer, req, 200, "PUT response goes here")
+
+	// Assert - third request fails because we've had our one replay
+	//nolint:bodyclose // response body is a string, no need to close
+	_, err := replayer.RoundTrip(req)
+	g.Expect(err).To(HaveOccurred())
+}
+
+func TestReplayRoundTripperRoundTrip_GivenMultiplePUTsToSameURL_ReturnsExpectedBodies(t *testing.T) {
+	t.Parallel()
+
+	// Arrange
+	//nolint:bodyclose // response body is a string, no need to close
+	alphaRequest, alphaResponse := createPutRequestAndResponse(
+		"/foo",
+		"Alpha goes here",
+		200)
+
+	//nolint:bodyclose // response body is a string, no need to close
+	betaRequest, betaResponse := createPutRequestAndResponse(
+		"/foo",
+		"Beta goes here",
+		203)
+
+	//nolint:bodyclose // response body is a string, no need to close
+	gammaRequest, gammaResponse := createPutRequestAndResponse(
+		"/foo",
+		"Gamma goes here",
+		200)
+
+	fake := vcr.NewFakeRoundTripper(cassette.ErrInteractionNotFound)
+	fake.AddResponse(alphaRequest, alphaResponse)
+	fake.AddResponse(betaRequest, betaResponse)
+	fake.AddResponse(gammaRequest, gammaResponse)
+
+	redactor := vcr.NewRedactor(creds.DummyAzureIDs())
+
+	// Act
+	replayer := NewReplayRoundTripper(fake, logr.Discard(), redactor)
+
+	// Assert - first alpha request works
+	assertExpectedResponse(t, replayer, alphaRequest, 200, "Alpha goes here")
+
+	// Assert - first beta request works
+	assertExpectedResponse(t, replayer, betaRequest, 203, "Beta goes here")
+
+	// Assert - first gamma request works
+	assertExpectedResponse(t, replayer, gammaRequest, 200, "Gamma goes here")
+
+	// Assert - second alpha request works by replaying the first
+	assertExpectedResponse(t, replayer, alphaRequest, 200, "Alpha goes here")
+
+	// Assert - second beta request works by replaying the first
+	assertExpectedResponse(t, replayer, betaRequest, 203, "Beta goes here")
+
+	// Assert - second gamma request works by replaying the first
+	assertExpectedResponse(t, replayer, gammaRequest, 200, "Gamma goes here")
+}
+
+func Test_ReplayRoundTripper_WhenCombinedWithTrackingRoundTripper_GivesDesiredResult(t *testing.T) {
+	t.Parallel()
+
+	// Arrange - Request and response to create the resource
+	//nolint:bodyclose // there's no actual body in this response to close
+	creationRequest, creationResponse := createPutRequestAndResponse(
+		"/sub/id/resource/A",
+		"create resource A",
+		200)
+
+	// Arrange - Request and response to update the resource
+	//nolint:bodyclose // there's no actual body in this response to close
+	updateRequest, updateResponse := createPutRequestAndResponse(
+		"/sub/id/resource/A",
+		"update resource A",
+		200)
+
+	// Arrange - set up fake replayer
+	fake := vcr.NewFakeRoundTripper(cassette.ErrInteractionNotFound)
+	fake.AddResponse(creationRequest, creationResponse)
+	fake.AddError(creationRequest, cassette.ErrInteractionNotFound)
+	fake.AddResponse(updateRequest, updateResponse)
+	fake.AddError(updateRequest, cassette.ErrInteractionNotFound)
+
+	redactor := vcr.NewRedactor(creds.DummyAzureIDs())
+
+	// Act
+	replayRountTripper := NewReplayRoundTripper(fake, logr.Discard(), redactor)
+	replayer := AddTrackingHeaders(replayRountTripper, redactor)
+
+	// Assert - first PUT to create the resource works
+	assertExpectedResponse(t, replayer, creationRequest, 200, "create resource A")
+
+	// Assert - second PUT to create the resource works because of replay
+	assertExpectedResponse(t, replayer, creationRequest, 200, "create resource A")
+
+	// Assert - first PUT to update the resource works
+	assertExpectedResponse(t, replayer, updateRequest, 200, "update resource A")
+
+	// Assert - second PUT to update the resource works due to replay
+	assertExpectedResponse(t, replayer, updateRequest, 200, "update resource A")
+}
+
+func createPutRequestAndResponse(
+	urlpath string,
+	body string,
+	statusCode int,
+) (*http.Request, *http.Response) {
+	req := &http.Request{
+		URL:    &url.URL{Path: urlpath},
+		Method: http.MethodPut,
+		Body: io.NopCloser(
+			bytes.NewBufferString(
+				fmt.Sprintf("PUT for %s", body),
+			)),
+	}
+
+	res := &http.Response{
+		StatusCode: statusCode,
+		Body: io.NopCloser(
+			bytes.NewBufferString(
+				fmt.Sprintf("PUT response for %s", body),
+			)),
+	}
+
+	return req, res
+}
+
+func assertExpectedResponse(
+	t *testing.T,
+	client http.RoundTripper,
+	request *http.Request,
+	expectedStatus int,
+	expectedBodyContent string,
+) {
+	t.Helper()
+	g := NewGomegaWithT(t)
+
+	// Get the response
+	//nolint:bodyclose // there's no actual body in this response to close
+	response, err := client.RoundTrip(request)
+	g.Expect(err).ToNot(HaveOccurred())
+
+	g.Expect(response.StatusCode).To(Equal(expectedStatus))
+
+	var body bytes.Buffer
+	if response.Body != nil {
+		_, err = body.ReadFrom(response.Body)
+		g.Expect(err).ToNot(HaveOccurred())
+		g.Expect(string(body.String())).To(ContainSubstring(expectedBodyContent))
+	}
+
+	// Reset the body so it can be read again
+	response.Body = io.NopCloser(&body)
+}
---
+++ v2/internal/testcommon/vcr/v4/test_recorder_test.go
@@ -0,0 +1,136 @@
+/*
+Copyright (c) Microsoft Corporation.
+Licensed under the MIT license.
+*/
+
+package v4
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	"github.com/go-logr/logr"
+
+	"github.com/Azure/azure-service-operator/v2/internal/config"
+	"github.com/Azure/azure-service-operator/v2/internal/testcommon/vcr"
+)
+
+func TestRecorder_WhenRecordingAndRecordingDoesNotExist_MakesRecording(t *testing.T) {
+	// NB: Can't run tests using Setenv() in parallel
+	t.Setenv("AZURE_SUBSCRIPTION_ID", "00000000-0000-0000-0000-000000000000")
+	t.Setenv("AZURE_TENANT_ID", "00000000-0000-0000-0000-000000000000")
+
+	g := NewGomegaWithT(t)
+
+	cfg := config.Values{}
+	cassetteName := "recordings/" + t.Name()
+
+	// Test prerequisite: The recording must not already exist
+	// We delete the recording at the end of the test, so it shouldn't ever be committed
+	// But if something goes awry, and we don't clean up, we need to flag the presence of
+	// this file as a failure.
+	// We're noisy about it (instead of just deleting the file proactively) in order to
+	// ensure the dev knows the file shouldn't be committed.
+	exists, err := vcr.CassetteFileExists(cassetteName)
+	g.Expect(err).To(BeNil())
+	g.Expect(exists).To(BeFalse())
+
+	// Ensure we clean up the cassette file at the end of the test
+	cassetteFile := vcr.CassetteFileName(cassetteName)
+	defer func() {
+		g.Expect(os.Remove(cassetteFile)).To(Succeed())
+	}()
+
+	// Create our TestRecorder and ensure it's recording
+	recorder, err := NewTestRecorder(cassetteName, cfg, logr.Discard())
+	g.Expect(err).To(BeNil())
+	g.Expect(recorder.IsReplaying()).To(BeFalse())
+
+	// Stand up a fake HTTP server:
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("Hello World"))
+	}))
+
+	client := recorder.CreateClient(t)
+
+	// Make sure we can get a response from the internet
+	//nolint:noctx
+	resp, err := client.Get(server.URL)
+	g.Expect(err).To(BeNil())
+	defer resp.Body.Close()
+
+	// Ensure the HTTP response is as expected
+	g.Expect(resp.StatusCode).To(Equal(http.StatusOK))
+
+	// Ensure the body is not empty
+	body, err := io.ReadAll(resp.Body)
+	g.Expect(err).To(BeNil())
+	g.Expect(body).NotTo(HaveLen(0))
+
+	// Stop the recorder
+	err = recorder.Stop()
+	g.Expect(err).To(BeNil())
+
+	// Verify we created a recording
+	exists, err = vcr.CassetteFileExists(cassetteName)
+	g.Expect(err).To(BeNil())
+	g.Expect(exists).To(BeTrue())
+}
+
+func TestRecorder_WhenRecordingAndRecordingExists_DoesPlayback(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	//
+	// Rerecording this test can be a challenge, due to
+	// (a) a prerequisite check ensuring the cassette exists
+	// (b) a similar check ensuring we're in replay mode, and
+	// (c) the requirement for selected environment variables to be present.
+	//
+	// The easiest way to rerecord this test is to:
+	//   (i) Delete the cassette file
+	//  (ii) Comment out the prerequisite checks
+	// (iii) Define the required environment variables
+	//  (iv) go test -run TestRecorderV3_WhenRecordingAndRecordingExists_DoesPlayback
+	//
+
+	cfg := config.Values{}
+	cassetteName := "recordings/" + t.Name()
+
+	// Test prerequisite: The recording must already exist
+	exists, err := vcr.CassetteFileExists(cassetteName)
+	g.Expect(err).To(BeNil())
+	g.Expect(exists).To(BeTrue()) // Comment out if rerecording, see (ii) above
+
+	// Create our TestRecorder and ensure it's in replay mode
+	recorder, err := NewTestRecorder(cassetteName, cfg, logr.Discard())
+	g.Expect(err).To(BeNil())
+	g.Expect(recorder.IsReplaying()).To(BeTrue()) // Comment out if rerecording, see (ii) above
+
+	url := "https://www.bing.com"
+	client := recorder.CreateClient(t)
+
+	// Make sure we can get a response from the internet
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
+	g.Expect(err).To(BeNil())
+
+	resp, err := client.Do(req)
+	g.Expect(err).To(BeNil())
+	defer resp.Body.Close()
+
+	// Ensure the body is not empty
+	body, err := io.ReadAll(resp.Body)
+	g.Expect(err).To(BeNil())
+	g.Expect(body).NotTo(HaveLen(0))
+
+	// Stop the recorder
+	err = recorder.Stop()
+	g.Expect(err).To(BeNil())
+}
