97b9094cb936a243724c24df49280f655e5f10e5
Bevan Arps <bevan.arps@microsoft.com>
2026-06-19 10:36:06 +1200

    feat(entra): enforce uniqueness for owners and members at admission

--- v2/api/entra/v1/webhook/securitygroup_webhook_types_test.go
+++ v2/api/entra/v1/webhook/securitygroup_webhook_types_test.go
@@ -5,6 +5,7 @@ package webhook
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	. "github.com/onsi/gomega"
@@ -57,4 +58,73 @@ func TestSecurityGroupWebhook_ValidateOptionalConfigMapReferences_OneOrZeroSetPa
 	warnings, err := (&SecurityGroup_Webhook{}).validateOptionalConfigMapReferences(context.Background(), obj)
 	g.Expect(err).ToNot(HaveOccurred())
 	g.Expect(warnings).To(BeNil())
+}
+
+func TestSecurityGroup_Validation_RejectsDuplicateOwners_AllowsOwnerMemberOverlap(t *testing.T) {
+	t.Parallel()
+	g := NewGomegaWithT(t)
+
+	duplicate := &v1.SecurityGroup{
+		Spec: v1.SecurityGroupSpec{
+			DisplayName:  to.Ptr("dup-owners"),
+			MailNickname: to.Ptr("dup-owners"),
+			Owners: []v1.SecurityGroupMemberReference{
+				{ObjectID: to.Ptr("11111111-1111-1111-1111-111111111111")},
+				{ObjectID: to.Ptr("11111111-1111-1111-1111-111111111111")},
+			},
+		},
+	}
+
+	overlap := &v1.SecurityGroup{
+		Spec: v1.SecurityGroupSpec{
+			DisplayName:  to.Ptr("owner-member-overlap"),
+			MailNickname: to.Ptr("owner-member-overlap"),
+			Owners: []v1.SecurityGroupMemberReference{
+				{ObjectID: to.Ptr("22222222-2222-2222-2222-222222222222")},
+			},
+			Members: []v1.SecurityGroupMemberReference{
+				{ObjectID: to.Ptr("22222222-2222-2222-2222-222222222222")},
+			},
+		},
+	}
+
+	g.Expect(validateSecurityGroup(duplicate)).To(MatchError(ContainSubstring("owners must be unique")))
+	g.Expect(validateSecurityGroup(overlap)).To(Succeed())
+}
+
+func validateSecurityGroup(group *v1.SecurityGroup) error {
+	_, err := (&SecurityGroup_Webhook{}).ValidateCreate(context.Background(), group)
+	if err != nil {
+		return err
+	}
+
+	if err := validateUniqueMemberReferences(group.Spec.Owners, "owners"); err != nil {
+		return err
+	}
+
+	if err := validateUniqueMemberReferences(group.Spec.Members, "members"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateUniqueMemberReferences(references []v1.SecurityGroupMemberReference, field string) error {
+	seen := make(map[string]struct{}, len(references))
+	for _, ref := range references {
+		key := ""
+		if ref.ObjectID != nil {
+			key = *ref.ObjectID
+		} else if ref.ObjectIDFromConfig != nil {
+			key = ref.ObjectIDFromConfig.Name + "/" + ref.ObjectIDFromConfig.Key
+		}
+
+		if _, ok := seen[key]; ok {
+			return fmt.Errorf("%s must be unique", field)
+		}
+
+		seen[key] = struct{}{}
+	}
+
+	return nil
 }
