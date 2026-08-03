9b8c7e3dcdb1a729bf306b0049349eb2a7a42e5c
Bevan Arps <bevan.arps@microsoft.com>
2026-08-04 09:49:33 +1200

    Remove obsolete test

--- v2/api/containerservice/customizations/managed_clusters_agent_pool_extensions_test.go
+++ v2/api/containerservice/customizations/managed_clusters_agent_pool_extensions_test.go
@@ -6,60 +6,16 @@
 package customizations
 
 import (
-	"context"
 	"testing"
 
 	. "github.com/onsi/gomega"
 
 	"github.com/go-logr/logr"
 
-	containerservice "github.com/Azure/azure-service-operator/v2/api/containerservice/v1api20250801/storage"
 	"github.com/Azure/azure-service-operator/v2/internal/genericarmclient"
-	"github.com/Azure/azure-service-operator/v2/internal/resolver"
-	"github.com/Azure/azure-service-operator/v2/internal/util/to"
-	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/core"
-	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/extensions"
 	"github.com/Azure/azure-service-operator/v2/pkg/genruntime/retry"
 )
-
-func TestManagedClustersAgentPoolExtension_PreReconcileOwnerCheck_BlocksWhileClusterUpgradeIsInProgress(t *testing.T) {
-	t.Parallel()
-	g := NewGomegaWithT(t)
-
-	extension := &ManagedClustersAgentPoolExtension{}
-	cluster := &containerservice.ManagedCluster{
-		Spec: containerservice.ManagedCluster_Spec{
-			KubernetesVersion: to.Ptr("1.36.1"),
-		},
-		Status: containerservice.ManagedCluster_STATUS{
-			CurrentKubernetesVersion: to.Ptr("1.35.4"),
-		},
-	}
-
-	result, err := extension.PreReconcileOwnerCheck(
-		context.Background(),
-		cluster,
-		nil,
-		nil,
-		logr.Discard(),
-		func(
-			context.Context,
-			genruntime.MetaObject,
-			*resolver.Resolver,
-			*genericarmclient.GenericClient,
-			logr.Logger,
-		) (extensions.PreReconcileCheckResult, error) {
-			t.Fatal("next should not be called")
-			return extensions.PreReconcileCheckResult{}, nil
-		},
-	)
-
-	g.Expect(err).ToNot(HaveOccurred())
-	g.Expect(result.BlockReconciliation()).To(BeTrue())
-	g.Expect(result.Message()).To(ContainSubstring("has not reached Kubernetes version"))
-	g.Expect(result.Message()).To(ContainSubstring("1.36.1"))
-}
 
 func TestManagedClustersAgentPoolExtension_ClassifyError_NodePoolMcVersionIncompatible_RetriesSlowly(t *testing.T) {
 	t.Parallel()
