a4af9edc7983113c4904e4043fa8e07808ff378f
Bevan Arps <bevan.arps@microsoft.com>
2026-09-08 11:15:03 +1200

    Prevent nolintlint from complaining

--- v2/test/perf/perf_helpers_test.go
+++ v2/test/perf/perf_helpers_test.go
@@ -194,7 +194,7 @@ type resourceSet struct {
 // 3. Hold for the configured duration
 // 4. Delete all resource sets
 //
-//nolint:unparam // Keep the error result consistent with the other performance test patterns.
+//nolint:unparam,nolintlint // Keep the error result consistent with the other performance test patterns.
 func runStaticPattern(
 	t *testing.T,
 	tc *testcommon.KubePerTestContext,
